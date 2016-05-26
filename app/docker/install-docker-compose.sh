8460fe58b7f326f5ece3e7e1f6d3d72c92cbd859
Jon Jagger <jon@jaggersoft.com>
2016-05-26 14:50:41 +0100

    deleted dead files

--- app/docker/install-docker-compose.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-echo 'installing docker-compose 1.7.0'
-curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
-sudo mv docker-compose /usr/local/bin/docker-compose
