f869a1cd4782f9145d537523bfb4042a70807cc4
Jon Jagger <jon@jaggersoft.com>
2016-06-06 15:31:46 +0100

    moved app/data/caches to app/caches so there is no need for an app/data folder

--- app/models/cache_properties.rb
+++ app/models/cache_properties.rb
@@ -2,7 +2,7 @@
 module CacheProperties # mix-in
 
   def cache_path
-    File.expand_path('..', File.dirname(__FILE__)) + '/data/caches'
+    File.expand_path('..', File.dirname(__FILE__)) + '/caches'
   end
 
   def cache_filename
