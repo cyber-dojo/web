aeb5d9480294a50fea0798cfeac1f727170301aa
Jon Jagger <jon@jaggersoft.com>
2016-06-09 09:59:09 +0100

    updated .gitignore to ignore files temporarily placed in root folder when web docker image is being built

--- test/app_controllers/app_controller_test_base.rb
+++ test/app_controllers/app_controller_test_base.rb
@@ -15,7 +15,7 @@ class AppControllerTestBase < ActionDispatch::IntegrationTest
     @dojo = Dojo.new
     super
     set_runner_class('StubRunner')
-    `rm -f /usr/src/cyber-dojo/app/caches/exercises.json`
+    `rm -f /usr/src/cyber-dojo/app/caches/*.json`
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
