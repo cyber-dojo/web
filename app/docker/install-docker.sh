0e4ea60a5decaae46c1ddb0be3a6abb0f6fb24dd
Jon Jagger <jon@jaggersoft.com>
2016-06-11 22:00:22 +0100

    bump versions in install-docker.sh script to match those in travis.yml

--- app/docker/install-docker.sh
+++ app/docker/install-docker.sh
@@ -8,12 +8,12 @@ set -e
 echo 'installing docker'
 curl -sSL https://get.docker.com/ | sh
 
-echo 'installing docker-machine 0.6.0'
+echo 'installing docker-machine 0.7.0'
 curl -L https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > docker-machine
 mv docker-machine /usr/local/bin/docker-machine
 chmod +x /usr/local/bin/docker-machine
 
-echo 'installing docker-compose 1.7.0'
+echo 'installing docker-compose 1.7.1'
 curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 mv docker-compose /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
