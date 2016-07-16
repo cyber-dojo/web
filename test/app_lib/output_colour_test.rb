689be7a9153a6f5f8fd0b69d8ce7f202b5d4085c
Jon Jagger <jon@jaggersoft.com>
2016-07-16 07:39:34 +0100

    added app_lib test that languages.red_amber_green lambda correctly colours saved test-run outputs

--- test/app_lib/output_colour_test.rb
+++ test/app_lib/output_colour_test.rb
@@ -5,18 +5,18 @@ require_relative './app_lib_test_base'
 class OutputColourTest < AppLibTestBase
 
   test '9DBD7D',
-  'all saved Runner outputs are correctly coloured red/amber/green' do
-    root = output_path
-    disk[root].each_dir do |unit_test_framework|
+  'all saved Runner outputs are correctly coloured red/amber/green by OutputColour.of' do
+    disk[output_path].each_dir do |unit_test_framework|
       ['red', 'amber', 'green'].each do |expected|
-        path = "#{root}/#{unit_test_framework}/#{expected}"
+        path = "#{output_path}/#{unit_test_framework}/#{expected}"
         dir = disk[path]
+        assert dir.exists?, "#{path} does not exist"
         dir.each_file do |filename|
           output = dir.read(filename)
           actual = OutputColour::of(unit_test_framework, output)
           diagnostic = '' +
             "OutputColour::of(output)\n" +
-            "  output read from: #{root}/#{unit_test_framework}/#{expected}/#{filename}\n"
+            "  output read from: #{path}/#{filename}\n"
           assert_equal expected, actual, diagnostic
         end
       end
@@ -25,27 +25,12 @@ class OutputColourTest < AppLibTestBase
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '04F165',
-  'all saved TestRunner outputs have red cases & amber cases & green cases' do
-    root = output_path
-    disk[root].each_dir do |unit_test_framework|
-      ['red','amber','green'].each do |colour|
-        path = "#{root}/#{unit_test_framework}/#{colour}"
-        diagnostic = "#{path}/ does not exist"
-        assert disk[path].exists?, diagnostic
-      end
-    end
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
   test '9005E7',
-  'all dojo.languages have corresponding test_output/unit_test_framework' do
-    root = output_path
+  'all dojo.languages have corresponding test/app_lib/output/unit_test_framework' do
     count = 0
     dojo.languages.each do |language|
       count += 1
-      path = root + '/' + language.unit_test_framework
+      path = output_path + '/' + language.unit_test_framework
       diagnostic = '' +
         "language: #{language.name}\n" +
         "unit_test_framework: #{language.unit_test_framework}\n" +
@@ -53,6 +38,42 @@ class OutputColourTest < AppLibTestBase
       assert disk[path].exists?, diagnostic
     end
     assert count > 0, 'no languages'
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '1F2763',
+  "all dojo.languages red_amber_green lambda's correctly colour saved Runner outputs" do
+    lambda_yes_count = 0
+    lambda_no_count = 0
+    dojo.languages.each do |language|
+      src = language.red_amber_green
+      if src.nil?
+        lambda_no_count += 1
+        next
+      else
+        lambda_yes_count += 1
+      end
+      ['red', 'amber', 'green'].each do |expected|
+        path = "#{output_path}/#{language.unit_test_framework}/#{expected}"
+        dir = disk[path]
+        assert dir.exists?, "#{path} does not exist"
+        dir.each_file do |filename|
+          output = dir.read(filename)
+          red_amber_green = eval(src.join("\n"))
+          actual = red_amber_green.call(output).to_s
+          diagnostic = '' +
+            "#{language.name}\n" +
+            "#{language.unit_test_framework}\n" +
+            "output read from: #{path}/#{filename}\n" +
+            "red_amber_green.call()"
+          assert_equal expected, actual, diagnostic
+        end
+      end
+    end
+    assert lambda_yes_count > 0, 'no languages'
+    p "#{lambda_yes_count} languages have working red_amber_green lambdas"
+    p "#{lambda_no_count} languages dont"
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
