fc0b0b778e0064ef191c6ff23407c7cd87218abc
Jon Jagger <jon@jaggersoft.com>
2016-07-29 11:33:12 +0100

    upgrade to docker-engine 1.12.0

--- cli/install-docker.sh
+++ cli/install-docker.sh
@@ -5,12 +5,9 @@ set -e
 # This script is part of the server installation instructions
 # described at http://blog.cyber-dojo.org/2016/07/running-your-own-cyber-dojo-web-server.html
 
-echo 'installing docker 1.11.2'
-curl -L https://get.docker.com/builds/Linux/x86_64/docker-1.11.2.tgz > docker-1.11.2.tgz
-tar -xvzf docker-1.11.2.tgz
-rm docker-1.11.2.tgz
-mv docker/* /usr/bin/
-rmdir docker
+echo 'installing docker'
+# This currently installs docker 1.12.0
+curl -sSL https://get.docker.com/ | sh
 
 echo 'installing docker-machine 0.7.0'
 curl -L https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > docker-machine
@@ -21,5 +18,3 @@ echo 'installing docker-compose 1.7.1'
 curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 mv docker-compose /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
-sudo docker daemon &
