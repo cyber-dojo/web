0bb1b7bb7ad9a2f592d2858f95026bcdee06ec4f
Jon Jagger <jon@jaggersoft.com>
2016-07-29 12:39:57 +0100

    install-docker scripts now installs docker-machine 0.8.0 and docker-compose 1.8.0

--- cli/install-docker.sh
+++ cli/install-docker.sh
@@ -9,12 +9,12 @@ echo 'installing docker'
 # This currently installs docker 1.12.0
 curl -sSL https://get.docker.com/ | sh
 
-echo 'installing docker-machine 0.7.0'
-curl -L https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > docker-machine
+echo 'installing docker-machine 0.8.0'
+curl -L https://github.com/docker/machine/releases/download/v0.8.0/docker-machine-`uname -s`-`uname -m` > docker-machine
 mv docker-machine /usr/local/bin/docker-machine
 chmod +x /usr/local/bin/docker-machine
 
-echo 'installing docker-compose 1.7.1'
-curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+echo 'installing docker-compose 1.8.0'
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 mv docker-compose /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
