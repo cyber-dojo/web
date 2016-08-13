9e1ec562b3d68b0ce1738b13682524c4f6cedb1a
Jon Jagger <jon@jaggersoft.com>
2016-08-13 15:02:16 +0530

    delete dead line in coverage controller

--- test/test_coverage.rb
+++ test/test_coverage.rb
@@ -6,7 +6,6 @@ SimpleCov.start do
   add_group 'app/models',      'app/models'
   add_group 'app/helpers',     'app/helpers'
   add_group 'app/lib',         'app/lib'
-  #add_group('lib') { |src| src.filename.include?('cyber-dojo/lib') }
   add_group('lib') { |src|
     src.filename.include?('web/lib') ||       # running tests locally
     src.filename.include?('cyber-dojo/lib')   # running tests inside web container
