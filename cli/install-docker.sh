23530ed1169ec8560db73dba2ecb24b034ceeb35
Jon Jagger <jon@jaggersoft.com>
2016-09-13 17:09:10 +0100

    dont fake an install of anything; instead cyber-dojo script only checks for docker-machine existing when not on Linux box

--- cli/install-docker.sh
+++ cli/install-docker.sh
@@ -19,10 +19,3 @@ echo 'installing docker-compose 1.8.0'
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod +x docker-compose
 mv docker-compose /usr/local/bin
-
-# fake an install of docker-machine
-# the cyberdojo script checks its existence (via hash)
-# as its needed on a Docker-Toolbox install
-touch docker-machine
-chmod +x docker-machine
-mv docker-machine /usr/local/bin
