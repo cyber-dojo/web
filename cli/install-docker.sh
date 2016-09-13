9019a867a327ce74cef6bdfe3d235f3c65e44c23
Jon Jagger <jon@jaggersoft.com>
2016-09-13 15:33:22 +0100

    still trying to find whats happening to docker-machine-install in travis script

--- cli/install-docker.sh
+++ cli/install-docker.sh
@@ -5,16 +5,22 @@ set -e
 # This script is part of the server installation instructions
 # described at http://blog.cyber-dojo.org/2016/07/running-your-own-cyber-dojo-web-server.html
 
+ME=`whoami`
+if [ "${ME}" != 'root' ]; then
+  echo 'this must be run as root'
+  exit
+fi
+
 echo 'installing docker'
 # This currently installs docker 1.12.1
 curl -sSL https://get.docker.com/ | sh
 
 echo 'installing docker-machine 0.8.1'
 curl -L https://github.com/docker/machine/releases/download/v0.8.1/docker-machine-`uname -s`-`uname -m` > docker-machine
-mv docker-machine /usr/local/bin/docker-machine
-chmod +x /usr/local/bin/docker-machine
+chmod +x docker-machine
+mv docker-machine /usr/local/bin
 
 echo 'installing docker-compose 1.8.0'
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
-mv docker-compose /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+chmod +x docker-compose
+mv docker-compose /usr/local/bin
