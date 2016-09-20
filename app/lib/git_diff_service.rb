558fd14a2da72718fa24fcd871d22608cbb22004
Jon Jagger <jon@jaggersoft.com>
2016-09-20 08:30:25 +0100

    switched git-diff-service to do get-request with json payload

--- app/lib/git_diff_service.rb
+++ app/lib/git_diff_service.rb
@@ -4,13 +4,15 @@ require 'net/http'
 module GitDiffService # mix-in
 
   def avatar_git_diff(avatar, was_tag, now_tag)
-    diff_params = {
-      :was_files => avatar.tags[was_tag].visible_files.to_json,
-      :now_files => avatar.tags[now_tag].visible_files.to_json
-    }
-    uri = URI.parse(ENV['DIFFER_PORT'].sub('tcp', 'http') + '/diff')
-    uri.query = URI.encode_www_form(diff_params)
-    response = Net::HTTP.get_response(uri)
+    uri = URI.parse('http://differ:4567/diff')
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request.content_type = 'application/json'
+    request.body = {
+      :was_files => avatar.tags[was_tag].visible_files,
+      :now_files => avatar.tags[now_tag].visible_files
+    }.to_json
+    response = http.request(request)
     JSON.parse(response.body)
   end
 
