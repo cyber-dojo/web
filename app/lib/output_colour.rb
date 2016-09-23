606fb0cf68102a6732bcef6ef3f8e745cdd0d4b0
Jon Jagger <jon@jaggersoft.com>
2016-09-23 11:21:54 +0100

    fix red/amber/green output regexs for upgraded Ruby minitest and testunit

--- app/lib/output_colour.rb
+++ app/lib/output_colour.rb
@@ -188,7 +188,7 @@ module OutputColour # mix-in
   end
 
   def self.parse_ruby_mini_test(output)
-    ruby_pattern = Regexp.new('^(\d*) tests, (\d*) assertions, (\d*) failures, (\d*) errors')
+    ruby_pattern = Regexp.new('^(\d*) runs, (\d*) assertions, (\d*) failures, (\d*) errors')
     if match = ruby_pattern.match(output)
       return :amber if match[4] != '0'
       return :red   if match[3] != '0'
@@ -199,7 +199,14 @@ module OutputColour # mix-in
   end
 
   def self.parse_ruby_test_unit(output)
-    self.parse_ruby_mini_test(output)
+    ruby_pattern = Regexp.new('^(\d*) tests, (\d*) assertions, (\d*) failures, (\d*) errors')
+    if match = ruby_pattern.match(output)
+      return :amber if match[4] != '0'
+      return :red   if match[3] != '0'
+      return :green
+    else
+      return :amber
+    end
   end
 
   def self.parse_hunit(output)
