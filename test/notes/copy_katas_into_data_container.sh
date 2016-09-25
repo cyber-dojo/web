e302090bd8b7b301c8b31f639cf939b5acf96c22
Jon Jagger <jon@jaggersoft.com>
2016-09-25 10:51:52 +0100

    [ci skip] added script to copy saved katas from host-disk to katas-data-container

--- test/notes/copy_katas_into_data_container.sh
+++ test/notes/copy_katas_into_data_container.sh
@@ -3,11 +3,12 @@
 # Script to copy saved local katas into cyber-dojo-web data-container.
 # Useful for
 # 1. cyber-dojo.org so all katas are inside the data-container again
-# 2. a stand-alone script to be used inside of the complexity inside cli/cyber-dojo.sh
-# 3. to copy katas saved from custom servers.
+# 2. a stand-alone script to be used instead of the complexity inside cli/cyber-dojo.sh
+# 3. to copy katas saved from short-lived servers into long-lived servers.
 
-# TODO: put a pause after each hex transfer
-# TODO: delete from src after transfer (otherwise may run out of disk space)
+# Note. this script does not delete the dir after transferring it.
+# If you are transferring a large amount of data you need a large
+# amount of free disk space.
 
 src_path=/home/jrbjagger/katas
 dst_path=/usr/src/cyber-dojo/katas
@@ -15,6 +16,7 @@ dst_path=/usr/src/cyber-dojo/katas
 for i in {0..255}
 do
   hex=`printf '%02X' ${i}`
-  echo ${hex}
+  echo ${hex} # eg 3F
   tar -c -f - -C ${src_path} ${hex} | sudo docker exec -i cyber-dojo-web tar -x -f - -C ${dst_path}
+  sleep 5s
 done
