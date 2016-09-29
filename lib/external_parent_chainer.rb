5633977c8b3c56a60559bd9d4169e259e56b87e1
Jon Jagger <jon@jaggersoft.com>
2016-09-29 04:49:42 +0100

    refactoring; improve comments for external-parent-chainer

--- lib/external_parent_chainer.rb
+++ lib/external_parent_chainer.rb
@@ -12,30 +12,30 @@ module ExternalParentChainer # mix-in
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Provides transparent access to...
-# 1. The external objects held in the root dojo object:
+# Hijack *method*_missing to provide transparent access to external *objects*.
+# Works by assuming the object (which included this module) has a parent
+# property and repeatedly chains back parent to parent to parent
+# till it gets to an object without a parent property which it assumes
+# is the root object holding the external objects, which it delegates to.
+#
+# For example, a call such as shell.exec(...) will start with a
+#    method_missing(:shell,[])
+# which then searches for the root object and may end up as
+#    parent.parent.parent.shell.exec(...)
+#
 # See app/models/dojo.rb
 #
-#  runner   - performs the actual test run, using docker in some way.
-#  katas    - access to cyber-dojo's sessions, by ID
-#  shell    - executes shell commands, eg mkdir,ls,git
-#  disk     - access to the file-system, directories, file read/write
-#  log      - memory/stdout based logging.
-#  git      - all required git commands. Forwards to shell.
-#
-# 2. The objects representing the main cyber-dojo folders:
-#
-# languages - as per setup page.
-# exercises - as per setup page.
+#   runner   - performs the actual test run, using docker
+#   katas    - access to cyber-dojo's sessions, by ID
+#   shell    - executes shell commands, eg mkdir,ls,git
+#   disk     - file-system directories and file read/write
+#   log      - memory/stdout based logging
+#   git      - all required git commands. Forwards to shell
 #
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Also allows the lib/ classes representing external objects to easily access
+# Allows the lib/ classes representing external objects to easily access
 # each other by chaining back to the root dojo object. For example:
 #
 #     HostGit   -> shell -> dojo.shell -> HostShell
 #     HostShell -> log   -> dojo.log   -> HostLog
 #
-# Works by assuming the object (which included this module) has a parent
-# property and repeatedly chains back parent to parent to parent
-# till it gets to an object without a parent property which it assumes
-# is the root dojo object, which it delegates to.
