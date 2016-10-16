92578fe2e9e5c43459dd32e9a03977b06d2071d9
Jon Jagger <jon@jaggersoft.com>
2016-10-16 09:13:14 +0100

    delete dead files; temporarily reduce min coverage of app/lib

--- test/build_up_test.sh
+++ test/build_up_test.sh
@@ -2,15 +2,14 @@
 set -e
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-cd ${my_dir}/../docker/web
-./build-image.sh
+${my_dir}/../build-image.sh
+${my_dir}/../../commander/cyber-dojo up
 
-cd ${my_dir}/../cli
-echo 'Bringing cyber-dojo up'
-./cyber-dojo up
 echo 'Shelling into web container and running tests'
 cid=`docker ps --all --quiet --filter "name=cyber-dojo-web"`
 docker exec ${cid} sh -c "cd test && ./run.sh ${*}"
 # TODO copy coverage out
 done=$?
+${my_dir}/../../commander/cyber-dojo up
+
 exit $done
