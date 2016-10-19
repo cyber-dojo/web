0adce0e6ec7cd9bb3a7036a6bd20ba1818db1941
Jon Jagger <jon@jaggersoft.com>
2016-10-19 07:49:24 +0100

    refactoring; use lower-case for non exported shell variables

--- build-image.sh
+++ build-image.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 set -e
 
-# A docker-client binary is installed *inside* the web image
-# This creates a dependency on the docker-version installed
-# on the host. Thus, the web Dockerfile accepts the docker-version
-# to install as a parameter, and the built web image is tagged with
-# this version number.
-DOCKER_VERSION=${1:-1.12.2}
-
-# the 'home' directory inside the web image. I don't expect
-# this to change, it's parameterized to avoid duplication.
-CYBER_DOJO_HOME=${2:-/usr/src/cyber-dojo}
-
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
+docker_version=${1:-1.12.2}
+cyber_dojo_home=${2:-/usr/src/cyber-dojo}
 
 context_dir=${my_dir}
 
 docker build \
-  --build-arg=CYBER_DOJO_HOME=${CYBER_DOJO_HOME} \
-  --build-arg=DOCKER_VERSION=${DOCKER_VERSION} \
-  --tag=cyberdojo/web:${DOCKER_VERSION} \
+  --build-arg=CYBER_DOJO_HOME=${cyber_dojo_home} \
+  --build-arg=DOCKER_VERSION=${docker_version} \
+  --tag=cyberdojo/web:${docker_version} \
   --file=${context_dir}/Dockerfile \
   ${context_dir}
