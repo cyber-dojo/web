8db2e5e4c87b8354ef0193ccb2596292c113dcbb
Jon Jagger <jon@jaggersoft.com>
2016-11-08 14:50:58 +0100

    remove docker-version-number from image name (tag)

--- build-image.sh
+++ build-image.sh
@@ -2,14 +2,12 @@
 set -e
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-docker_version=${1:-1.12.2}
 cyber_dojo_home=${2:-/usr/src/cyber-dojo}
 
 context_dir=${my_dir}
 
 docker build \
   --build-arg=CYBER_DOJO_HOME=${cyber_dojo_home} \
-  --build-arg=DOCKER_VERSION=${docker_version} \
-  --tag=cyberdojo/web:${docker_version} \
+  --tag=cyberdojo/web \
   --file=${context_dir}/Dockerfile \
   ${context_dir}
