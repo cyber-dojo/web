d2cf0960665579afc0ed60998ee1a0195b312ffc
Jon Jagger <jon@jaggersoft.com>
2016-11-10 20:21:39 +0000

    comment update on what tests are failing

--- test/run.sh
+++ test/run.sh
@@ -6,7 +6,7 @@ rm -rf /tmp/cyber-dojo-*
 # run tests for each module
 modules=( app_helpers app_lib app_models lib app_controllers )
 #modules=( app_helpers ) does not leak volumes
-modules=( app_lib ) # does not leak volumes
+#modules=( app_lib ) # does not leak volumes. Needs RunnerService tests to get 100% coverage
 #modules=( app_models ) does not leak volumes
 #modules=( lib ) does not leak volumes
 #modules=( app_controllers )  # ImagePullerTests fail, Leaks volumes
