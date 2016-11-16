cac0ef745677bf38da10f9e711917932cb9125b3
Jon Jagger <jon@jaggersoft.com>
2016-11-16 11:33:55 +0000

    [ci skip] added notes on script used to copy katas/ from hard-disk-dir to data-container

--- notes/copy_katas_into_data_container.sh
+++ notes/copy_katas_into_data_container.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Script to copy saved local katas into cyber-dojo-web data-container.
+# Simple script to copy saved local katas (on hard disk)
+# into the cyber-dojo-web data-container (on the same server)
 # Useful for
 # 1. cyber-dojo.org so all katas are inside the data-container again
-# 2. a stand-alone script to be used instead of the complexity inside cli/cyber-dojo.sh
-# 3. to copy katas saved from short-lived servers into long-lived servers.
+# 2. copying katas saved from short-lived servers into long-lived servers.
 
-# Note. this script does not delete the dir after transferring it.
-# If you are transferring a large amount of data you need a large
-# amount of free disk space.
-# Note. this script does not ensure the copied files inside the
-# cyber-dojo-web data container have the correct rights. You may need to
-# add a [chown -R cyber-dojo] docker command
+# Notes.
+# o) make sure you change src_path to katas location on your hard disk
+#    eg /var/www/cyber-dojo/katas
+# o) does not delete the dir after transferring it.
+#    If you are transferring a large amount of data you'll need a large
+#    amount of free disk space.
+# o) does not ensure the copied files inside the
+#    cyber-dojo-web data container have the correct rights. You may need to
+#    add a [chown -R cyber-dojo] docker command
+# o) assumes a cyber-dojo server is running and hence there
+#    is an alive cyber-dojo-web container.
+# o) sleeps each time round the loop so as not to hog the cpu if
+#    you are running this on a live cyber-dojo server.
 
 src_path=/home/jrbjagger/katas
 dst_path=/usr/src/cyber-dojo/katas
