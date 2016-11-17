8f88d04382b8cc982fc952a9a21d7dcc274656fe
Jon Jagger <jon@jaggersoft.com>
2016-11-17 13:20:30 +0000

    check docker-compose is installed at top of build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+hash docker 2> /dev/null
+if [ $? != 0 ]; then
+  echo
+  echo "docker is not installed"
+  exit 1
+fi
+
+hash docker-compose 2> /dev/null
+if [ $? != 0 ]; then
+  echo
+  echo "docker-compose is not installed"
+  exit 1
+fi
+
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 docker-compose -f ${my_dir}/docker-compose.yml build
