4558f88940bfbc812273d207fc19b72bbc2df463
Jon Jagger <jon@jaggersoft.com>
2016-12-01 10:44:53 +0000

    in travis script install docker from master script in cyber-dojo/ruby repo

--- build.sh
+++ build.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-hash docker 2> /dev/null
-if [ $? != 0 ]; then
-  echo
-  echo "docker is not installed"
-  exit 1
-fi
-
-hash docker-compose 2> /dev/null
-if [ $? != 0 ]; then
-  echo
-  echo "docker-compose is not installed"
-  exit 1
-fi
-
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-
 docker-compose --file ${my_dir}/docker-compose.yml build
