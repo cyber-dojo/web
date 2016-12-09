6877bc38d2bec19b90977a15df64387f5c0b579a
Jon Jagger <jon@jaggersoft.com>
2016-12-09 17:45:30 +0000

    deleted dead coverage-setup code

--- test/test_coverage.rb
+++ test/test_coverage.rb
@@ -1,15 +1,12 @@
 require 'simplecov'
 
 SimpleCov.start do
-  filters.clear
+  #filters.clear
   add_group 'app/controllers', 'app/controllers'
   add_group 'app/models',      'app/models'
   add_group 'app/helpers',     'app/helpers'
   add_group 'app/lib',         'app/lib'
-  add_group('lib') { |src|
-    src.filename.include?('web/lib') ||       # running tests locally
-    src.filename.include?('cyber-dojo/lib')   # running tests inside web container
-  }
+  add_group('lib') { |src| src.filename.include?('cyber-dojo/lib') }
 end
 
 cov_root = File.expand_path('..', File.dirname(__FILE__))
