6a15c8e290f8c50c824612e84883ea15c7b8bd9c
Jon Jagger <jon@jaggersoft.com>
2016-12-10 12:40:48 +0000

    updated up.sh so that it creates the katas-data-container if one does not already exist; this is needed on the travis run

--- up.sh
+++ up.sh
@@ -14,5 +14,42 @@ export CYBER_DOJO_START_POINT_LANGUAGES=languages
 export CYBER_DOJO_START_POINT_EXERCISES=exercises
 export CYBER_DOJO_START_POINT_CUSTOM=custom
 
+one_time_creation_of_katas_data_volume()
+{
+  # The katas data-volume is not created as a named volume because
+  # it predates that docker feature.
+  docker ps --all | grep -s ${CYBER_DOJO_KATAS_DATA_CONTAINER} > /dev/null
+  if [ $? != 0 ]; then
+
+    local CONTEXT_DIR=/tmp/build-katas-data-container
+    mkdir -p ${CONTEXT_DIR}
+    echo '*' > ${CONTEXT_DIR}/.dockerignore
+
+    echo 'FROM alpine:3.4' > ${CONTEXT_DIR}/Dockerfile
+    echo 'RUN adduser -D -H -u 19661 cyber-dojo' >> ${CONTEXT_DIR}/Dockerfile
+    echo 'ARG CYBER_DOJO_KATAS_ROOT' >> ${CONTEXT_DIR}/Dockerfile
+    echo 'USER root' >> ${CONTEXT_DIR}/Dockerfile
+    echo 'RUN  mkdir -p ${CYBER_DOJO_KATAS_ROOT}' >> ${CONTEXT_DIR}/Dockerfile
+    echo 'RUN  chown -R cyber-dojo ${CYBER_DOJO_KATAS_ROOT}' >> ${CONTEXT_DIR}/Dockerfile
+    echo 'VOLUME [ "${CYBER_DOJO_KATAS_ROOT}" ]' >> ${CONTEXT_DIR}/Dockerfile
+    echo 'CMD [ "katas-data-container" ]' >> ${CONTEXT_DIR}/Dockerfile
+
+    local TAG=cyberdojo/katas
+    # create a katas volume - it is mounted into the web container
+    # using a volumes_from in docker-compose.yml
+    docker build \
+              --build-arg=CYBER_DOJO_KATAS_ROOT=${CYBER_DOJO_ROOT}/katas \
+              --tag=${TAG} \
+              --file=Dockerfile \
+              ${CONTEXT_DIR} > /dev/null
+    docker create \
+              --name ${CYBER_DOJO_KATAS_DATA_CONTAINER} \
+              ${TAG} \
+              echo 'cdfKatasDC' > /dev/null
+  fi
+}
+
+one_time_creation_of_katas_data_volume
+
 docker-compose --file ${my_dir}/docker-compose.yml up -d
 
