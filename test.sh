4cf6a91fdc3b9ddc76190a6f0ee951abc1caff59
Jon Jagger <jon@jaggersoft.com>
2016-12-10 14:36:26 +0000

    debugging .travis.yml ... katas-data-container has to be created before build

--- test.sh
+++ test.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -e
 
-my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-
 cid=`docker ps --all --quiet --filter "name=cyber-dojo-web"`
 docker exec ${cid} sh -c "cd test && ./run.sh ${*}"
 
 # copy coverage stats out of container
+my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 mkdir -p ${my_dir}/coverage
 modules=( app_helpers app_lib app_models lib app_controllers )
 for module in ${modules[*]}
 do
-  # to copy the *contents* of a dir docker cp requires a trailing dot
+  # copying the *contents* of a dir [docker cp] requires a trailing dot
   src=${cid}:/usr/src/cyber-dojo/test/${module}/coverage/.
   dst=${my_dir}/coverage/${module}
   docker cp ${src} ${dst}
--- up.sh
+++ up.sh
@@ -6,50 +6,13 @@ set -e
 #cyber-dojo up
 #sleep 2
 
-my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-
 export CYBER_DOJO_ROOT=/usr/src/cyber-dojo
 export CYBER_DOJO_KATAS_DATA_CONTAINER=cyber-dojo-katas-DATA-CONTAINER
 export CYBER_DOJO_START_POINT_LANGUAGES=languages
 export CYBER_DOJO_START_POINT_EXERCISES=exercises
 export CYBER_DOJO_START_POINT_CUSTOM=custom
 
-one_time_creation_of_katas_data_volume()
-{
-  # The katas data-volume is not created as a named volume
-  # because it predates that docker feature.
-  #local KDC=$(docker ps --all | grep -s ${CYBER_DOJO_KATAS_DATA_CONTAINER})
-  #if [ "${KDC}" == "" ]; then
-    echo "Making katas data-container"
-    local CONTEXT_DIR=/tmp/build-katas-data-container
-    mkdir -p ${CONTEXT_DIR}
-    echo '*' > ${CONTEXT_DIR}/.dockerignore
-    echo 'FROM alpine:3.4' > ${CONTEXT_DIR}/Dockerfile
-    echo 'RUN adduser -D -H -u 19661 cyber-dojo' >> ${CONTEXT_DIR}/Dockerfile
-    echo 'ARG CYBER_DOJO_KATAS_ROOT' >> ${CONTEXT_DIR}/Dockerfile
-    echo 'USER root' >> ${CONTEXT_DIR}/Dockerfile
-    echo 'RUN  mkdir -p ${CYBER_DOJO_KATAS_ROOT}' >> ${CONTEXT_DIR}/Dockerfile
-    echo 'RUN  chown -R cyber-dojo ${CYBER_DOJO_KATAS_ROOT}' >> ${CONTEXT_DIR}/Dockerfile
-    echo 'VOLUME [ "${CYBER_DOJO_KATAS_ROOT}" ]' >> ${CONTEXT_DIR}/Dockerfile
-    echo 'CMD [ "katas-data-container" ]' >> ${CONTEXT_DIR}/Dockerfile
-    local TAG=cyberdojo/katas
-    # create a katas volume - it is mounted into the web container
-    # using a volumes_from in docker-compose.yml
-    docker build \
-              --build-arg=CYBER_DOJO_KATAS_ROOT=${CYBER_DOJO_ROOT}/katas \
-              --tag=${TAG} \
-              --file=${CONTEXT_DIR}/Dockerfile \
-              ${CONTEXT_DIR} > /dev/null
-    docker create \
-              --name ${CYBER_DOJO_KATAS_DATA_CONTAINER} \
-              ${TAG} \
-              echo 'cdfKatasDC' > /dev/null
-    #fi
-}
-
-set +e
-one_time_creation_of_katas_data_volume
-set -e
+my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
 docker-compose --file ${my_dir}/docker-compose.yml up -d
 
