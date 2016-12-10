d4f13e7abf45ba94f0cb17a4951958c1cf1d024d
Jon Jagger <jon@jaggersoft.com>
2016-12-10 07:28:23 +0000

    updated test.sh script to copy coverage stats out of container

--- test.sh
+++ test.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -e
 
+my_dir="$( cd "$( dirname "${0}" )" && pwd )"
+
 cid=`docker ps --all --quiet --filter "name=cyber-dojo-web"`
 docker exec ${cid} sh -c "cd test && ./run.sh ${*}"
+
+# copy coverage stats out of container
+modules=( app_helpers app_lib app_models lib app_controllers )
+for module in ${modules[*]}
+do
+  # to copy the *contents* of a dir docker cp requires a trailing dot
+  src=${cid}:/usr/src/cyber-dojo/test/${module}/coverage/.
+  dst=${my_dir}/coverage/${module}
+  docker cp ${src} ${dst}
+done
--- test/run.sh
+++ test/run.sh
@@ -5,13 +5,12 @@ rm -rf /tmp/cyber-dojo-*
 
 # run tests for each module
 modules=( app_helpers app_lib app_models lib app_controllers )
-
 for module in ${modules[*]}
 do
     echo
-    echo "======$module======"
-    cd $module
-    ./run.sh $*
+    echo "======${module}======"
+    cd ${module}
+    ./run.sh ${*}
     cd ..
 done
 
