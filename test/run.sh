53439741fef3603daf46fdd17f206a0337b2e533
Jon Jagger <jon@jaggersoft.com>
2016-12-10 12:39:57 +0000

    fixed test/run.sh which was not clearing out /tmp properly

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# ensure Mocks saving to Dir.tmpdir have clean start
-rm -rf /tmp/cyber-dojo-*
+# Mocks save to Dir.tmpdir
+# Also test/test_wrapper.sh sets CYBER_DOJO_KATAS_ROOT
+# to /tmp/cyber-dojo/katas
+# Ensure both have a clean start
+
+rm -rf /tmp/cyber-dojo
 
 modules=( app_helpers app_lib app_models lib app_controllers )
 for module in ${modules[*]}
