134fe7541d8c44973c2f523e0eecfe42f5d03039
Jon Jagger <jon@jaggersoft.com>
2016-12-10 15:24:21 +0000

    tidied up .travis.yml after trail of debugging

--- up.sh
+++ up.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# You must do a down so the up brings up a new web container
-#cyber-dojo down
-#cyber-dojo up
-#sleep 2
-
 export CYBER_DOJO_ROOT=/usr/src/cyber-dojo
 export CYBER_DOJO_KATAS_DATA_CONTAINER=cyber-dojo-katas-DATA-CONTAINER
 export CYBER_DOJO_START_POINT_LANGUAGES=languages
@@ -18,6 +13,7 @@ one_time_creation_of_start_point_volumes()
   chmod +x /tmp/cyber-dojo
   local GIT_URL=https://github.com/cyber-dojo/start-points-
   set +e
+  # These all fail (and do nothing) if the start-point already exists
   /tmp/cyber-dojo start-point create ${CYBER_DOJO_START_POINT_LANGUAGES} \
       --git=${GIT_URL}languages.git
   /tmp/cyber-dojo start-point create ${CYBER_DOJO_START_POINT_EXERCISES} \
@@ -25,6 +21,7 @@ one_time_creation_of_start_point_volumes()
   /tmp/cyber-dojo start-point create ${CYBER_DOJO_START_POINT_CUSTOM} \
       --git=${GIT_URL}custom.git
   set -e
+  rm /tmp/cyber-dojo
 }
 
 one_time_creation_of_start_point_volumes
