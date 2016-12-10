e21f36dac9a8d4137c7b7bc515ba4a46bafd70a0
Jon Jagger <jon@jaggersoft.com>
2016-12-10 07:34:17 +0000

    deleted unneeded rm call; new container gets clean /tmp

--- test/run.sh
+++ test/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# ensure Mocks saving to Dir.tmpdir have clean start
-rm -rf /tmp/cyber-dojo-*
-
-# run tests for each module
 modules=( app_helpers app_lib app_models lib app_controllers )
 for module in ${modules[*]}
 do
