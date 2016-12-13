9140ad1e4f00559e51cb34574dd83b7c4c5bb7cc
Jon Jagger <jon@jaggersoft.com>
2016-12-13 11:49:36 +0000

    deleted dead script comments

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 # Mocks save to Dir.tmpdir
-# Also test/test_wrapper.sh sets CYBER_DOJO_KATAS_ROOT
-# to /tmp/cyber-dojo/katas
-# Ensure both have a clean start
-
 rm -rf /tmp/cyber-dojo
 
 modules=( app_helpers app_lib app_models lib app_controllers )
