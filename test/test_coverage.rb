41f70760158737e0b1c6384cc6dd676cc3f19fac
Jon Jagger <jon@jaggersoft.com>
2016-12-13 16:11:21 +0000

    refactored test-coverage duplication

--- test/test_coverage.rb
+++ test/test_coverage.rb
@@ -1,34 +1,16 @@
 require 'simplecov'
 
-modyule = ARGV[0]
-
 SimpleCov.start do
-  #filters.clear
-  #add_group('debug') { |src| puts "coverage:#{src.filename}"; false }
-
-  if modyule == 'app_helpers'
-    add_group('test/app_helpers') { |src| src.filename.start_with?('/app/test/app_helpers/') }
-    add_group('app/helpers')      { |src| src.filename.start_with?('/app/app/helpers/') }
-  end
-  if modyule == 'app_lib'
-    add_group('test/app_lib') { |src| src.filename.start_with?('/app/test/app_lib/') }
-    add_group('app/lib')      { |src| src.filename.start_with?('/app/app/lib/') }
-  end
-  if modyule == 'app_models'
-    add_group('test/app_models') { |src| src.filename.start_with?('/app/test/app_models/') }
-    add_group('app/models')      { |src| src.filename.start_with?('/app/app/models/') }
-  end
-  if modyule == 'app_controllers'
-    add_group('test/app_controllers') { |src| src.filename.start_with?('/app/test/app_controllers/') }
-    add_group('app/controllers')      { |src| src.filename.start_with?('/app/app/controllers/') }
-  end
-  if modyule == 'lib'
-    add_group('test/lib') { |src| src.filename.start_with?('/app/test/lib/') }
-    add_group('lib')      { |src| src.filename.start_with?('/app/lib/') }
-  end
-
+  modyule = ARGV[0]
+  slashed_modyule = modyule.sub('_','/')
+  add_group('test/'+modyule) { |src| src.filename.start_with?('/app/test/'+modyule+'/') }
+  add_group(slashed_modyule) { |src| src.filename.start_with?('/app/'+slashed_modyule+'/') }
 end
 
 cov_root = File.expand_path('..', File.dirname(__FILE__))
 SimpleCov.root cov_root
 SimpleCov.coverage_dir ENV['COVERAGE_DIR']
+
+#- - - - - - - - - - - - - - - - - - - - - - -
+#filters.clear
+#add_group('debug') { |src| puts "coverage:#{src.filename}"; false }
