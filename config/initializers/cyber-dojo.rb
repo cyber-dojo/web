a5011fe882765d9f81fd7e6fb44b3f278ebca660
Jon Jagger <jon@jaggersoft.com>
2016-12-13 10:25:58 +0000

    removed unneeded katas/ and downloads/ dirs. katas are now stored in separate service. download tgz files are created off /tmp

--- config/initializers/cyber-dojo.rb
+++ config/initializers/cyber-dojo.rb
@@ -1,6 +1,6 @@
 
 root = File.expand_path("../..", File.dirname(__FILE__))
-%w( caches katas downloads ).each do |dir|
+%w( caches ).each do |dir|
   pathed_dir = root + '/' + dir
   Dir.mkdir(pathed_dir) unless File.exists?(pathed_dir)
 end
