a8dab586ce58e617e959750831c9a3ef9276e9e0
Jon Jagger <jon@jaggersoft.com>
2016-12-20 07:40:53 +0000

    added comments to id-splitter

--- app/lib/id_splitter.rb
+++ app/lib/id_splitter.rb
@@ -12,3 +12,12 @@ module IdSplitter # mix-in
   end
 
 end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
+# cyber-dojo uses a file-system instead of a database.
+# Each kata has its own 10 digit hex-id, eg '356CDE70DB'
+# which corresponds to a dir such as
+# ..../katas/35/6CDE70DB/
+# where the hex-id is split into a 2/8 dir structure like git.
+# This means the katas dir has 256 2-digit dirs.
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
