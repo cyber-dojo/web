4713f7a1715699abaa89aea4bc61a69883140fb0
Jon Jagger <jon@jaggersoft.com>
2016-12-21 21:45:12 +0000

    folded test_wrapper.sh into test/run.sh

--- test/run.sh
+++ test/run.sh
@@ -15,7 +15,20 @@ do
     echo
     echo "======${module}======"
     cd ${module}
-    ../test_wrapper.sh *_test.rb ${*}
+    testFiles=(*_test.rb)
+    # don't log to stdout
+    export CYBER_DOJO_LOG_CLASS=MemoryLog
+    # clear out old coverage stats
+    coverage_dir=/tmp/cyber-dojo/coverage/${module}
+    mkdir -p ${coverage_dir}
+    rm -rf ${coverage_dir}/.resultset.json
+    test_log="${coverage_dir}/test.log"
+    # run-the-tests!
+    export COVERAGE_DIR=${coverage_dir}
+    ruby -e "%w( ${testFiles[*]} ).shuffle.map{ |file| require './'+file }" \
+      ${module} \
+      ${*} 2>&1 | tee ${test_log}
+    ruby ../print_coverage_percent.rb ${module} | tee -a ${test_log}
     cd ..
 done
 
