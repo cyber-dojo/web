bd60e191c00dcfc1bf85de17bfecd51cc402c028
Jon Jagger <jon@jaggersoft.com>
2016-12-25 10:36:05 +0000

    [ci skip] make quick_test.sh quicker by not rebuilding image, instead volume mount code and test dirs

--- quick_test.sh
+++ quick_test.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 set -e
 
-# NB: Missing links to external services - non unit-tests will fail
+# NB: This does not have links to external services
+#     non unit-tests will fail
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
 CYBER_DOJO_HOME=/app
-
-docker build \
-  --build-arg CYBER_DOJO_HOME=${CYBER_DOJO_HOME} \
-  --file ${my_dir}/Dockerfile \
-  --tag cyberdojo/web \
-  ${my_dir}
-
-CYBER_DOJO_LANGUAGES_ROOT=${CYBER_DOJO_HOME}/start-points/languages
-CYBER_DOJO_EXERCISES_ROOT=${CYBER_DOJO_HOME}/start-points/exercises
-CYBER_DOJO_CUSTOM_ROOT=${CYBER_DOJO_HOME}/start-points/custom
+CYBER_DOJO_START_POINT_ROOT=${CYBER_DOJO_HOME}/start-points
+CYBER_DOJO_LANGUAGES_ROOT=${CYBER_DOJO_START_POINTS_ROOT}/languages
+CYBER_DOJO_EXERCISES_ROOT=${CYBER_DOJO_START_POINTS_ROOT}/exercises
+CYBER_DOJO_CUSTOM_ROOT=${CYBER_DOJO_START_POINTS_ROOT}/custom
 
 docker run \
   --rm \
   --tty \
-  --volume languages:${CYBER_DOJO_HOME}/start-points/languages:ro \
-  --volume exercises:${CYBER_DOJO_HOME}/start-points/exercises:ro \
-  --volume custom:${CYBER_DOJO_HOME}/start-points/custom:ro \
+  --user cyber-dojo \
+  --volume ${my_dir}/app:${CYBER_DOJO_HOME}/app:ro \
+  --volume ${my_dir}/lib:${CYBER_DOJO_HOME}/lib:ro \
+  --volume ${my_dir}/test:${CYBER_DOJO_HOME}/test:ro \
+  --volume languages:${CYBER_DOJO_START_POINTS_ROOT}/languages:ro \
+  --volume exercises:${CYBER_DOJO_START_POINTS_ROOT}/exercises:ro \
+  --volume custom:${CYBER_DOJO_START_POINTS_ROOT}/custom:ro \
+  --env CYBER_DOJO_HOME=${CYBER_DOJO_HOME} \
   --env CYBER_DOJO_LANGUAGES_ROOT=${CYBER_DOJO_LANGUAGES_ROOT} \
   --env CYBER_DOJO_EXERCISES_ROOT=${CYBER_DOJO_EXERCISES_ROOT} \
   --env CYBER_DOJO_CUSTOM_ROOT=${CYBER_DOJO_CUSTOM_ROOT} \
