ae3fbfcc3f9bd0caa8bb051f438379b56ab98d01
Jon Jagger <jon@jaggersoft.com>
2016-12-26 08:23:27 +0000

    deleted commented code I was mulling on

--- test/test_coverage.rb
+++ test/test_coverage.rb
@@ -1,9 +1,4 @@
 require 'simplecov'
-#require 'simplecov-json'
-#SimpleCov.formatters = [
-#  SimpleCov::Formatter::HTMLFormatter,
-#  SimpleCov::Formatter::JSONFormatter,
-#]
 
 SimpleCov.start do
 
