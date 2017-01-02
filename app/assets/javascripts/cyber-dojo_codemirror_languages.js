021e1fe09326ae509b22771b25ff80a90997faa4
Byran Wills-Heath <byran@adgico.co.uk>
2017-01-02 16:33:24 +0000

    Added syntax highlighting for more languages

--- app/assets/javascripts/cyber-dojo_codemirror_languages.js
+++ app/assets/javascripts/cyber-dojo_codemirror_languages.js
@@ -28,6 +28,22 @@ var cyberDojo = (function(cd, $) {
       case '.c':
       case '.h':
         return 'text/x-c++src';
+      case '.d':
+        return 'text/x-d';
+      case '.feature':
+        return 'text/x-feature';
+      case '.js':
+        return 'text/javascript';
+      case '.php':
+        return 'text/x-php';
+      case '.py':
+        return 'text/x-python';
+      case '.rb':
+        return 'text/x-ruby';
+      case '.vb':
+        return 'text/x-vb';
+      case '.vhdl':
+        return 'text/x-vhdl';
     }
     return '';
   };
