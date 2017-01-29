57ca791555f73c8ef5dd39e35f98b55ca587504e
Jon Jagger <jon@jaggersoft.com>
2017-01-29 13:56:34 +0000

    reinstate remaining image-puller controller tests

--- test/app_controllers/image_puller_test.rb
+++ test/app_controllers/image_puller_test.rb
@@ -1,174 +1,218 @@
 require_relative './app_controller_test_base'
 
-class ImagePullerTest < AppControllerTestBase
+class MockRunner
 
-  def setup_mock_shell
-    set_shell_class('MockHostShell')
+  @@pulled = []
+  @@pull = []
+  @@new_kata = []
+
+  def initialize(_parent)
   end
 
-  # Note: test_hex_id_helpers.setup_id(hex) sets StubRunner
-  # which assumes the current state of [docker images] to be
-  #    cyberdojofoundation/nasm_assert
-  #    cyberdojofoundation/gcc_assert
-  #    cyberdojofoundation/csharp_nunit
-  #    cyberdojofoundation/gpp_cpputest
+  def teardown
+    error "@@pulled != []:#{@@pulled}" unless @@pulled == []
+    error "@@pull != []:#{@@pull}" unless @@pull == []
+    error "@@new_kata != []:#{@@new_kata}" unless @@new_kata == []
+  end
+
+  def mock_pulled?(image_name, result)
+    @@pulled << [image_name, result]
+  end
+
+  def pulled?(image_name)
+    error "no mock for pulled?(#{image_name})" if @@pulled == []
+    mock = @@pulled.shift
+    error "expecting #{mock[0]}, actual:#{image_name}" unless mock[0] == image_name
+    mock[1]
+  end
+
+  # - - - - - - - - - - - - - - - - - - - -
+
+  def mock_pull(image_name, result)
+    @@pull << [image_name, result]
+  end
+
+  def pull(image_name)
+    error "no mock for pull(#{image_name})" if @@pull == []
+    mock = @@pull.shift
+    error "expected #{mock[0]}:actual #{image_name}" unless mock[0] == image_name
+    mock[1]
+  end
+
+  # - - - - - - - - - - - - - - - - - - - -
+
+  def mock_new_kata(image_name)
+    @@new_kata << image_name
+  end
+  def new_kata(image_name, _kata_id)
+    error "no mock for new_kata(#{image_name},kata_id)" if @@new_kata == []
+    mock = @@new_kata.shift
+    error "expected #{mock}:actual #{image_name}" unless mock == image_name
+  end
+
+  private
+
+  def error(message)
+    fail "MockRunner:#{message}"
+  end
+
+end
+
+# = = = = = = = = = = = = = = = = = = = = = = =
+
+class ImagePullerTest < AppControllerTestBase
 
   # - - - - - - - - - - - - - - - - - - - - - -
   # from Language+Test setup page
   # - - - - - - - - - - - - - - - - - - - - - -
 
   test '406596',
-  'language pull.needed is true when docker image is not pulled' do
+  'language pull_needed==true' do
+    set_runner_class('MockRunner')
+    runner.mock_pulled?("#{cdf}/csharp_moq", false)
     do_get 'pull_needed', major_minor_js('language', 'C#', 'Moq')
     assert json['needed']
+    runner.teardown
   end
 
   # - - - - - - - - - - - - - - - - - - - - - -
 
   test '406A3D',
-  'language pull.needed is false when docker image is pulled' do
+  'language pull_needed==false' do
+    set_runner_class('MockRunner')
+    runner.mock_pulled?("#{cdf}/csharp_nunit", true)
     do_get 'pull_needed', major_minor_js('language', 'C#', 'NUnit')
     refute json['needed']
+    runner.teardown
   end
 
   # - - - - - - - - - - - - - - - - - - - - - -
-=begin
+
   test '406080',
-  'language pull issues docker-pull image_name command',
-  'and returns succeeded=true when pull succeeds' do
-    setup_mock_shell
-    mock_docker_pull_success('csharp_nunit')
+  'language pull=true' do
+    set_runner_class('MockRunner')
+    runner.mock_pull("#{cdf}/csharp_nunit", true)
     do_get 'pull', major_minor_js('language', 'C#', 'NUnit')
     assert json['succeeded']
-    shell.teardown
+    runner.teardown
   end
 
   # - - - - - - - - - - - - - - - - - - - - - -
 
   test '4063FD',
-  'language pull issues docker-pull image_name command',
-  'and returns succeeded=false when pull fails' do
-    setup_mock_shell
-    mock_docker_pull_failure('csharp_nunit')
+  'language pull==false' do
+    set_runner_class('MockRunner')
+    runner.mock_pull("#{cdf}/csharp_nunit", false)
     do_get 'pull', major_minor_js('language', 'C#', 'NUnit')
     refute json['succeeded']
-    shell.teardown
+    runner.teardown
   end
-=end
 
   # - - - - - - - - - - - - - - - - - - - - - -
   # from Custom setup page
   # - - - - - - - - - - - - - - - - - - - - - -
 
   test '406C10',
-  'custom pull.needed is true when docker image is not pulled' do
+  'custom pull_needed==false' do
+    set_runner_class('MockRunner')
+    runner.mock_pulled?("#{cdf}/python_unittest", false)
     do_get 'pull_needed', major_minor_js('custom', 'Tennis refactoring', 'Python unitttest')
     assert json['needed']
+    runner.teardown
   end
 
   # - - - - - - - - - - - - - - - - - - - - - -
 
   test '406E9A',
-  'custom pull_needed is false when docker image is pulled' do
+  'custom pull_needed==true' do
+    set_runner_class('MockRunner')
+    runner.mock_pulled?("#{cdf}/csharp_nunit", true)
     do_get 'pull_needed', major_minor_js('custom', 'Tennis refactoring', 'C# NUnit')
     refute json['needed']
+    runner.teardown
   end
 
   # - - - - - - - - - - - - - - - - - - - - - -
-=begin
+
   test '4064A0',
-  'custom pull issues docker-pull image_name command',
-  'and returns succeeded=true when pull succeeds' do
-    setup_mock_shell
-    mock_docker_pull_success('python_unittest')
+  'custom pull==true' do
+    set_runner_class('MockRunner')
+    runner.mock_pull("#{cdf}/python_unittest", true)
     do_get 'pull', major_minor_js('custom', 'Tennis refactoring', 'Python unitttest')
     assert json['succeeded']
-    shell.teardown
+    runner.teardown
   end
 
   # - - - - - - - - - - - - - - - - - - - - - -
 
   test '4065E7',
-  'custom pull issues docker-pull image_name command',
-  'and returns succeeded=false when pull fails' do
-    setup_mock_shell
-    mock_docker_pull_failure('python_unittest')
+  'custom pull==false' do
+    set_runner_class('MockRunner')
+    runner.mock_pull("#{cdf}/python_unittest", false)
     do_get 'pull', major_minor_js('custom', 'Tennis refactoring', 'Python unitttest')
     refute json['succeeded']
-    shell.teardown
+    runner.teardown
   end
-=end
 
   # - - - - - - - - - - - - - - - - - - - - - -
   # from Fork on review page/dialog
   # - - - - - - - - - - - - - - - - - - - - - -
 
   test '406269',
-  'kata pull.needed is false when image (from post start-point re-architecture)',
-  'kata.id has already been pulled' do
+  'kata pull_needed==false (from post start-point re-architecture)' do
+    set_runner_class('MockRunner')
+    set_storer_class('FakeStorer')
+    runner.mock_pulled?("#{cdf}/csharp_nunit", true)
+    runner.mock_new_kata("#{cdf}/csharp_nunit")
     create_kata('C#, NUnit')
     do_get 'pull_needed', id_js
     refute json['needed']
+    runner.teardown
   end
 
   # - - - - - - - - - - - - - - - - - - - - - -
 
   test '406A97',
-  'kata pull.needed is true when image (from post start-point re-architecture)',
-  'kata.id has not been pulled' do
+  'kata pull_needed==true (from post start-point re-architecture)' do
+    set_runner_class('MockRunner')
+    set_storer_class('FakeStorer')
+    runner.mock_pulled?("#{cdf}/csharp_moq", false)
+    runner.mock_new_kata("#{cdf}/csharp_moq")
     create_kata('C#, Moq')
     do_get 'pull_needed', id_js
     assert json['needed']
+    runner.teardown
   end
 
   # - - - - - - - - - - - - - - - - - - - - - -
-=begin
+
   test '406E66',
-  'kata pull issues docker-pull image_name command',
-  'and returns succeeded=true when pull succeeds' do
+  'kata pull==true' do
+    set_runner_class('MockRunner')
+    set_storer_class('FakeStorer')
+    runner.mock_pull("#{cdf}/csharp_moq", true)
+    runner.mock_new_kata("#{cdf}/csharp_moq")
     create_kata('C#, Moq')
-    mock_docker_pull_success('csharp_moq')
     do_get 'pull', id_js
     assert json['succeeded']
-    shell.teardown
+    runner.teardown
   end
 
   # - - - - - - - - - - - - - - - - - - - - - -
 
   test '406B07',
-  'kata pull issues docker-pull image_name command',
-  'and returns succeeded=false when pull fails' do
+  'kata pull==false' do
+    set_runner_class('MockRunner')
+    set_storer_class('FakeStorer')
+    runner.mock_pull("#{cdf}/csharp_moq", false)
+    runner.mock_new_kata("#{cdf}/csharp_moq")
     create_kata('C#, Moq')
-    mock_docker_pull_failure('csharp_moq')
     do_get 'pull', id_js
     refute json['succeeded']
-    shell.teardown
+    runner.teardown
   end
-=end
 
   private
-
-  def mock_docker_pull_failure(image_name)
-    setup_mock_shell
-    shell.mock_exec(
-      [sudo + "docker pull cyberdojofoundation/#{image_name}"],
-      any_output='456ersfdg',
-      exit_failure=34
-    )
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-  def mock_docker_pull_success(image_name)
-    setup_mock_shell
-    shell.mock_exec(
-      [sudo + "docker pull cyberdojofoundation/#{image_name}"],
-      docker_pull_output,
-      exit_success
-    )
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
 
   def do_get(route, params = {})
     controller = 'image_puller'
@@ -199,8 +243,8 @@ class ImagePullerTest < AppControllerTestBase
 
   # - - - - - - - - - - - - - - - - - - - - - -
 
-  def sudo
-    'sudo -u docker-runner sudo '
+  def cdf
+    'cyberdojofoundation'
   end
 
 end
