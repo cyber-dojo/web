e71c48ad5dc83eadb806f6a92aab538b289c0c0e
Jon Jagger <jon@jaggersoft.com>
2017-02-03 14:29:30 +0200

    updated puller-service tests now that puller validates the image_name arguments

--- test/app_lib/puller_service_test.rb
+++ test/app_lib/puller_service_test.rb
@@ -4,32 +4,53 @@ class PullerServiceTest < AppLibTestBase
 
   # These will fail if there is no network connectivity.
 
-  def setup
-    super
-    set_puller_class('PullerService')
+  test 'D4F808',
+  'smoke test puller' do
+    refute puller.pulled? 'cyberdojo/non_existant'
+    image_name = 'cyberdojofoundation/gcc_assert'
+    assert puller.pull image_name
+    assert puller.pulled? image_name
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test 'D4FCD3',
-  'smoke test puller.pull(nil) raises' do
-    error = assert_raises { puller.pull(nil) }
-    assert error.message.start_with? 'PullerService:pull:docker pull'
+  'smoke test pulled?() raises for invalid image_name' do
+    invalid_image_names.each do |invalid_image_name|
+      assert_raises_invalid_image('pulled?') {
+        puller.pulled?(invalid_image_name)
+      }
+    end
   end
 
   test 'D4FCD4',
-  'smoke test puller.pulled?(nil) returns false' do
-    refute puller.pulled?(nil)
+  'smoke test pull() raises for invalid image_name' do
+    invalid_image_names.each do |invalid_image_name|
+      assert_raises_invalid_image('pull') {
+        puller.pull(invalid_image_name)
+      }
+    end
+  end
+
+  private
+
+  def assert_raises_invalid_image(method)
+    error = assert_raises { yield }
+    expected = "PullerService:#{method}:image_name:invalid"
+    assert error.message.start_with?(expected), error.message
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'D4F808',
-  'smoke test puller-service' do
-    refute puller.pulled? 'cyberdojo/non_existant'
-    image_name = 'cyberdojofoundation/gcc_assert'
-    assert puller.pull image_name
-    assert puller.pulled? image_name
+  def invalid_image_names
+    [
+      '',             # nothing!
+      '_',            # cannot start with separator
+      'name_',        # cannot end with separator
+      'ALPHA/name',   # no uppercase
+      'alpha/name_',  # cannot end in separator
+      'alpha/_name',  # cannot begin with separator
+    ]
   end
 
 end
