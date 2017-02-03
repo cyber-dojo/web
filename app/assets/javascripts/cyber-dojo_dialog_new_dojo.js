76a35634c99540c733a8add4da01a8760be0b862
Jon Jagger <jon@jaggersoft.com>
2017-02-03 09:39:47 +0200

    refactoring

--- app/assets/javascripts/cyber-dojo_dialog_new_dojo.js
+++ app/assets/javascripts/cyber-dojo_dialog_new_dojo.js
@@ -3,9 +3,95 @@
 var cyberDojo = (function(cd, $) {
   "use strict";
 
-  cd.newDojoDialog = function(params, from) {
-    cd.dialog_pullImageIfNeededThen(params, from, showNewDojoDialog);
+  cd.newDojoDialog = function(dojoParams, from) {
+    $.getJSON('/image_puller/pulled', dojoParams, function(pulled) {
+      if (!pulled.result) {
+        dialog_pullImage(dojoParams, from);
+      } else {
+        showNewDojoDialog(dojoParams, from);
+      }
+    });
   };
+
+  //- - - - - - - - - - - - - - - - - - - - - - - -
+
+  var dialog_pullImage = function(dojoParams, from) {
+    var title = 'setting up a new practice session...';
+    var pullingDialog = makePullingDialog(title, dojoParams);
+    pullingDialog.dialog('open');
+    $.getJSON('/image_puller/pull', dojoParams, function(pull) {
+      pullingDialog.dialog('close');
+      if (pull.result) {
+        showNewDojoDialog(dojoParams, from);
+      } else {
+        makePullFailedDialog(title, dojoParams).dialog('open');
+      }
+    });
+  };
+
+  //- - - - - - - - - - - - - - - - - - - - - - - -
+
+  var makePullingDialog = function(title, dojoParams) {
+    var html = '' +
+      '<div class="selection">' + dojoParams.selection + '</div><br/>' +
+      'First time for this selection!<br/>' +
+      "We're now doing a one-time only setup.<br/>" +
+      'It can take a minute or two.<br/>' +
+      'Please wait...';
+    return $('<div id="setup-pull-dialog">')
+      .html(avatarGridTable(html))
+      .dialog({
+        title: cd.dialogTitle(title),
+        closeOnEscape: true,
+        close: function() { $(this).remove(); },
+        autoOpen: false,
+        width: 620,
+        height: 230,
+        modal: true,
+      });
+  };
+
+  //- - - - - - - - - - - - - - - - - - - - - - - -
+
+  var makePullFailedDialog = function(title, dojoParams) {
+    var html = '' +
+      "We're very sorry...<br/>" +
+      'The setup failed.<br/>';
+    return $('<div id="setup-pull-dialog">')
+      .html(avatarGridTable(html))
+      .dialog({
+        title: cd.dialogTitle(title),
+        closeOnEscape: true,
+        close: function() { $(this).remove(); },
+        autoOpen: false,
+        buttons: {
+          'close': function() {
+            $(this).remove();
+          }
+        },
+        width: 620,
+        height: 310,
+        modal: true,
+      });
+  };
+
+  //- - - - - - - - - - - - - - - - - - - - - - - -
+
+  var avatarGridTable = function(rhs) {
+    return '' +
+      '<table>' +
+      '<tr>' +
+      '<td>' +
+      '<img src="/images/avatars/all_avatars_background.png" class="avatars-grid">' +
+      '</td>' +
+      '<td>' +
+      rhs +
+      '</td>' +
+      '</tr>' +
+      '</table>';
+  };
+
+  //- - - - - - - - - - - - - - - - - - - - - - - -
 
   var showNewDojoDialog = function(params, from) {
     var id = params['id'];
