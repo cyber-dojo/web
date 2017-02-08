782b08c8ec08e5bb97b48b1d2b2ad400c8ce4cc4
Jon Jagger <jon@jaggersoft.com>
2017-02-08 11:13:53 +0000

    [ci skip] added second URL reference for need to remove pid file when restarting rails server

--- run_rails_server.sh
+++ run_rails_server.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # See https://github.com/docker/compose/issues/1393
+# See http://stackoverflow.com/questions/35022428
 rm -f /app/tmp/pids/server.pid
 
 rails server --environment=production
