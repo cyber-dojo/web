4a5f80bf8d848b83cb28b3ec91771f7185ed265a
Jon Jagger <jon@jaggersoft.com>
2017-02-22 07:46:46 +0000

    refactor downloader so it depends only on storer (and not on katas)

--- app/controllers/downloader_controller.rb
+++ app/controllers/downloader_controller.rb
@@ -8,18 +8,18 @@ class DownloaderController < ApplicationController
     kata_dir = disk[kata_path]
     kata_dir.make
     kata_dir.write_json('manifest.json', storer.kata_manifest(id))
-    katas[id].avatars.each do |avatar|
-      avatar_path = "#{kata_path}/#{avatar.name}"
+    storer.started_avatars(id).each do |avatar_name|
+      avatar_path = "#{kata_path}/#{avatar_name}"
       avatar_dir = disk[avatar_path]
       avatar_dir.make
-      rags = storer.avatar_increments(id, avatar.name)
+      rags = storer.avatar_increments(id, avatar_name)
       rags.shift # tag0
       avatar_dir.write_json('increments.json', rags)
       (1..rags.size).each do |tag|
         tag_path = "#{avatar_path}/#{tag}"
         tag_dir = disk[tag_path]
         tag_dir.make
-        visible_files = storer.tag_visible_files(id, avatar.name, tag)
+        visible_files = storer.tag_visible_files(id, avatar_name, tag)
         tag_dir.write_json('manifest.json', visible_files)
       end
     end
