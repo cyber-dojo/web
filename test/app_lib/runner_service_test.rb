b1da88c7f864d22f3e17b6b1f6742579f9efc76a
Jon Jagger <jon@jaggersoft.com>
2017-04-15 19:15:43 +0100

    moved all app_lib smake tests using real external-services into their own file

--- test/app_lib/runner_service_test.rb
+++ test/app_lib/runner_service_test.rb
@@ -6,59 +6,8 @@ class RunnerServiceTest < AppLibTestBase
 
   def setup
     super
-    set_storer_class('FakeStorer')
+    #set_storer_class('StorerThrower')
     set_runner_class('RunnerService')
-  end
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  test '2BD23CD3',
-  'smoke test runner-service raising' do
-    assert_raises { runner.image_pulled?(nil, nil) }
-    assert_raises { runner.image_pull(nil, nil) }
-    assert_raises { runner.kata_new(nil, nil) }
-  end
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  test '2BDF808102',
-  'smoke test pulling' do
-    kata_id = '2BDF808102'
-    refute runner.image_pulled? 'cyberdojo/non_existant', kata_id
-    image_name = 'cyberdojofoundation/gcc_assert'
-    assert runner.image_pull image_name, kata_id
-    assert runner.image_pulled? image_name, kata_id
-  end
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  test '2BDAD80812',
-  'smoke test runner-service colour is red-amber-green traffic-light' do
-    image_name = 'cyberdojofoundation/gcc_assert'
-    kata_id = '2BDAD80812'
-    runner.kata_new(image_name, kata_id)
-    runner.avatar_new(image_name, kata_id, lion, starting_files)
-    args = []
-    args << image_name
-    args << kata_id
-    args << lion
-    args << (max_seconds = 10)
-    args << (delta = {
-      :deleted => [],
-      :new     => [],
-      :changed => starting_files.keys
-    })
-    args << starting_files
-    begin
-      stdout,stderr,status,colour = runner.run(*args)
-      assert stdout.start_with? "makefile:4: recipe for target 'test.output' failed"
-      assert stderr.start_with? 'Assertion failed: answer() == 42'
-      assert_equal 2, status
-      assert_equal 'red', colour
-    ensure
-      runner.avatar_old(image_name, kata_id, lion)
-      runner.kata_old(image_name, kata_id)
-    end
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - -
--- test/app_lib/smoke_test.rb
+++ test/app_lib/smoke_test.rb
@@ -1,12 +1,71 @@
 require_relative 'app_lib_test_base'
 
-class StorerServiceTest < AppLibTestBase
+class SmokeTest < AppLibTestBase
+
+  # These will fail if there is no network connectivity.
 
   def setup
     super
     set_storer_class('StorerService')
+    set_runner_class('RunnerService')
   end
 
+#=begin
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+  # runner
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '2BD23CD3',
+  'smoke test runner-service raising' do
+    assert_raises { runner.image_pulled?(nil, nil) }
+    assert_raises { runner.image_pull(nil, nil) }
+    assert_raises { runner.kata_new(nil, nil) }
+  end
+
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '2BDF808102',
+  'smoke test pulling' do
+    kata_id = '2BDF808102'
+    refute runner.image_pulled? 'cyberdojo/non_existant', kata_id
+    image_name = 'cyberdojofoundation/gcc_assert'
+    assert runner.image_pull image_name, kata_id
+    assert runner.image_pulled? image_name, kata_id
+  end
+
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '2BDAD80812',
+  'smoke test runner-service colour is red-amber-green traffic-light' do
+    image_name = 'cyberdojofoundation/gcc_assert'
+    kata_id = '2BDAD80812'
+    runner.kata_new(image_name, kata_id)
+    runner.avatar_new(image_name, kata_id, lion, starting_files)
+    args = []
+    args << image_name
+    args << kata_id
+    args << lion
+    args << (max_seconds = 10)
+    args << (delta = {
+      :deleted => [],
+      :new     => [],
+      :changed => starting_files.keys
+    })
+    args << starting_files
+    begin
+      stdout,stderr,status,colour = runner.run(*args)
+      assert stdout.start_with? "makefile:4: recipe for target 'test.output' failed"
+      assert stderr.start_with? 'Assertion failed: answer() == 42'
+      assert_equal 2, status
+      assert_equal 'red', colour
+    ensure
+      runner.avatar_old(image_name, kata_id, lion)
+      runner.kata_old(image_name, kata_id)
+    end
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # storer
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
   # In docker-compose.yml the storer service is setup with
   #     environment: [ CYBER_DOJO_KATAS_ROOT=/tmp/cyber-dojo/katas ]
@@ -76,5 +135,22 @@ class StorerServiceTest < AppLibTestBase
     assert_equal files0, hash['was_tag']
     assert_equal files1, hash['now_tag']
   end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # zipper
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test 'D66EBF',
+  'smoke test zipper.zip' do
+    error = assert_raises { zipper.zip(kata_id='') }
+    assert error.message.end_with?('invalid kata_id'), error.message
+  end
+
+  test 'D66959',
+  'smoke test zipper.zip_tag' do
+    error = assert_raises { zipper.zip_tag(kata_id='', 'lion', 0) }
+    assert error.message.end_with?('invalid kata_id'), error.message
+  end
+#=end
 
 end
