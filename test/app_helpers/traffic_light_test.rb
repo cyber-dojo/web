3971b6cab164e8cf4e8b0a68faa7d6e3f3a7fbd5
Jon Jagger <jon@jaggersoft.com>
2017-05-06 07:10:18 +0100

    add missing test for app_helpers

--- test/app_helpers/traffic_light_test.rb
+++ test/app_helpers/traffic_light_test.rb
@@ -96,11 +96,19 @@ class TrafficLightTest < AppHelpersTestBase
     diff_traffic_light_func({'outcome' => 'red'})
   end
 
-  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  #- - - - - - - - - - - - - - - -
+
+  test 'BF0443',
+  'diff_traffic_light2' do
+    diff_traffic_light_func2('red')
+    diff_traffic_light_func2('green')
+  end
+
+  #- - - - - - - - - - - - - - - -
 
   def diff_traffic_light_func(light)
     kata = Object.new
-    def kata.id; 'ABCD1234'; end
+    def kata.id; 'ABCDE12345'; end
     avatar = Avatar.new(kata, 'hippo')
     light = Tag.new(avatar, {
       'number' => (tag = 3),
@@ -110,7 +118,7 @@ class TrafficLightTest < AppHelpersTestBase
       '<div' +
       " class='diff-traffic-light'" +
       " data-tip='ajax:traffic_light'" +
-      " data-id='ABCD1234'" +
+      " data-id='ABCDE12345'" +
       " data-avatar-name='hippo'" +
       " data-colour='#{colour}'" +
       " data-was-tag='#{tag - 1}'" +
@@ -119,6 +127,28 @@ class TrafficLightTest < AppHelpersTestBase
           " alt='#{colour} traffic-light'/>" +
       '</div>'
     actual = diff_traffic_light(light)
+    assert_equal expected, actual
+  end
+
+  #- - - - - - - - - - - - - - - -
+
+  def diff_traffic_light_func2(colour)
+    id = 'ABCDE12345'
+    avatar_name = 'hippo'
+    tag = 3
+    expected = '' +
+      '<div' +
+      " class='diff-traffic-light'" +
+      " data-tip='ajax:traffic_light'" +
+      " data-id='#{id}'" +
+      " data-avatar-name='#{avatar_name}'" +
+      " data-colour='#{colour}'" +
+      " data-was-tag='#{tag - 1}'" +
+      " data-now-tag='#{tag}'>" +
+      "<img src='/images/bulb_#{colour}.png'" +
+          " alt='#{colour} traffic-light'/>" +
+      '</div>'
+    actual = diff_traffic_light2(id, avatar_name, colour, tag)
     assert_equal expected, actual
   end
 
