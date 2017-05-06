a765ef3131caa82c13c62ea025815d13170016b7
Jon Jagger <jon@jaggersoft.com>
2017-05-06 08:58:49 +0100

    added comment to dashboard worker on what is making calls to storer

--- app/controllers/dashboard_worker.rb
+++ app/controllers/dashboard_worker.rb
@@ -3,6 +3,58 @@ module DashboardWorker # mixin
 
   module_function
 
+  def gather
+    # Showing the dashboard uses 2 calls to the storer.
+    # One explicit one for storer.kata_increments()
+    # and one hidden one to storer.kata_manifest()
+    # for the kata's progress_regexs in the
+    # progress button's partial.
+    # The kata manifest is cached so @kata.created
+    # uses the cache and does not cause a second call
+    # to storer.kata_manifest()
+    @kata = kata
+    @minute_columns = bool('minute_columns')
+    @auto_refresh = bool('auto_refresh')
+
+    @all_lights = {}
+    storer.kata_increments(kata.id).each do |name, lights|
+      lights.shift # 0==created-event
+      unless lights.empty?
+        @all_lights[name] = lights
+      end
+    end
+
+    max_seconds_uncollapsed = seconds_per_column * 5
+    args = []
+    args << @kata.created
+    args << seconds_per_column
+    args << max_seconds_uncollapsed
+    gapper = DashboardTdGapper.new(*args)
+
+    @gapped = gapper.fully_gapped(@all_lights, time_now)
+    @time_ticks = gapper.time_ticks(@gapped)
+  end
+
+  include TimeNow
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def bool(attribute)
+    tf = params[attribute]
+    tf == 'false' ? tf : 'true'
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def seconds_per_column
+    flag = params['minute_columns']
+    # default is that time-gaps are on
+    return 60 if flag.nil? || flag == 'true'
+    return 60*60*24*365*1000
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
   def animals_progress
     animals = {}
     avatars.active.each do |avatar|
@@ -14,43 +66,7 @@ module DashboardWorker # mixin
     animals
   end
 
-  def gather
-    @kata = kata
-    @minute_columns = bool('minute_columns')
-    @auto_refresh = bool('auto_refresh')
-
-    @all_lights = {}
-    storer.kata_increments(kata.id).each do |name, lights|
-      created = Time.mktime(*lights.shift['time'])
-      unless lights.empty?
-        @all_lights[name] = lights
-      end
-    end
-
-    created ||= kata.created
-    max_seconds_uncollapsed = seconds_per_column * 5
-    args = []
-    args << created
-    args << seconds_per_column
-    args << max_seconds_uncollapsed
-    gapper = DashboardTdGapper.new(*args)
-
-    @gapped = gapper.fully_gapped(@all_lights, time_now)
-    @time_ticks = gapper.time_ticks(@gapped)
-    @progress = @kata.progress_regexs != [ ]
-  end
-
-  def bool(attribute)
-    tf = params[attribute]
-    tf == 'false' ? tf : 'true'
-  end
-
-  def seconds_per_column
-    flag = params['minute_columns']
-    # default is that time-gaps are on
-    return 60 if flag.nil? || flag == 'true'
-    return 60*60*24*365*1000
-  end
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   def most_recent_progress(avatar)
     regexs = avatar.kata.progress_regexs
@@ -66,7 +82,5 @@ module DashboardWorker # mixin
       colour: (matches[0] != nil ? 'red' : 'green')
     }
   end
-
-  include TimeNow
 
 end
