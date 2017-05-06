89aacbdcb162685a0a64c971ebf9729a780e9d88
Jon Jagger <jon@jaggersoft.com>
2017-05-06 09:27:48 +0100

    fix pie-charts on reimplemented dashboard view

--- app/helpers/pie_chart_helper.rb
+++ app/helpers/pie_chart_helper.rb
@@ -3,31 +3,48 @@ module PieChartHelper # mix-in
 
   module_function
 
-  def pie_chart(lights, key)
-     # key is the traffic-light's avatar's name
-     pie_chart_from_counts({
-              red: count(lights, :red),
-            amber: count(lights, :amber),
-            green: count(lights, :green),
-        timed_out: count(lights, :timed_out)
-     }, 34, key)
+  def pie_chart(lights, avatar_name)
+    pie_chart_from_counts({
+            red: count(lights, :red),
+          amber: count(lights, :amber),
+          green: count(lights, :green),
+      timed_out: count(lights, :timed_out)
+    }, 34, avatar_name)
   end
 
-  def pie_chart_from_counts(counts, size, key)
+  def pie_chart2(lights, avatar_name)
+    # used in dashboard's view
+    pie_chart_from_counts({
+            red: count2(lights, :red),
+          amber: count2(lights, :amber),
+          green: count2(lights, :green),
+      timed_out: count2(lights, :timed_out)
+    }, 34, avatar_name)
+  end
+
+  def pie_chart_from_counts(counts, size, avatar_name)
      '<canvas' +
         " class='pie'" +
         " data-red-count='#{counts[:red]}'" +
         " data-amber-count='#{counts[:amber]}'" +
         " data-green-count='#{counts[:green]}'" +
         " data-timed-out-count='#{counts[:timed_out]}'" +
-        " data-key='#{key}'" +
+        " data-key='#{avatar_name}'" +
         " width='#{size}'" +
         " height='#{size}'>" +
       '</canvas>'
   end
 
   def count(lights, colour)
-    lights.entries.count { |light| light.colour == colour }
+    lights.entries.count { |light|
+      light.colour == colour
+    }
+  end
+
+  def count2(lights, colour)
+    lights.entries.count { |light|
+      light['colour'] == colour.to_s
+    }
   end
 
 end
--- test/app_helpers/pie_chart_test.rb
+++ test/app_helpers/pie_chart_test.rb
@@ -36,6 +36,40 @@ class PieChartTest < AppHelpersTestBase
 
   #- - - - - - - - - - - - - - - - - - - -
 
+  test '10E59F',
+  'pie-chart from kata_increments() lights used in dashboard view' do
+    kata = make_kata
+    lion = kata.start_avatar(['lion'])
+
+    maker = DeltaMaker.new(lion)
+    runner.stub_run_colour('red')
+    maker.run_test
+    runner.stub_run_colour('green')
+    maker.run_test
+
+    lights = storer.kata_increments(kata.id)['lion']
+    lights.shift
+
+    size = 34
+    expected = '' +
+      '<canvas' +
+      " class='pie'" +
+      " data-red-count='1'" +
+      " data-amber-count='0'" +
+      " data-green-count='1'" +
+      " data-timed-out-count='0'" +
+      " data-key='lion'" +
+      " width='#{size}'" +
+      " height='#{size}'>" +
+      '</canvas>'
+
+    actual = pie_chart2(lights, 'lion')
+
+    assert_equal expected, actual
+  end
+
+  #- - - - - - - - - - - - - - - - - - - -
+
   test '3AF953',
   'pie-chart from lights' do
     lights = [
