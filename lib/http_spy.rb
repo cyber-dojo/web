a12177aaddfd76098726349a4b45f87a9e71c209
Jon Jagger <jon@jaggersoft.com>
2017-07-30 09:46:24 +0530

    added tests for runner-service ready to refactor against

--- lib/http_spy.rb
+++ lib/http_spy.rb
@@ -2,23 +2,32 @@
 class HttpSpy
 
   def initialize(_parent)
+    clear
   end
 
-  attr_reader :hostname, :port, :method, :named_args
+  attr_reader :spied, :hostname, :port, :method, :named_args
+
+  def clear
+    @spied = []
+  end
 
   def get(hostname, port, method, named_args)
-    @hostname = hostname
-    @port = port
-    @method = method.to_s
-    @named_args = named_args
+    @spied << [
+      @hostname = hostname,
+      @port = port,
+      @method = method.to_s,
+      @named_args = named_args
+    ]
     { @method => { 'stdout' => '', 'stderr' => '', 'status' => 0, 'colour' => 'amber' } }
   end
 
   def post(hostname, port, method, named_args)
-    @hostname = hostname
-    @port = port
-    @method = method.to_s
-    @named_args = named_args
+    @spied << [
+      @hostname = hostname,
+      @port = port,
+      @method = method.to_s,
+      @named_args = named_args
+    ]
     { @method => { 'stdout' => '', 'stderr' => '', 'status' => 0, 'colour' => 'amber' } }
   end
 
--- test/app_lib/runner_service_test.rb
+++ test/app_lib/runner_service_test.rb
@@ -9,6 +9,128 @@ class RunnerServiceTest < AppLibTestBase
     set_runner_class('RunnerService')
   end
 
+  def make_kata_stateful
+    kata = make_kata({ 'language' => 'C (gcc)-assert' })
+    assert_equal 'stateful', kata.runner_choice
+    kata
+  end
+
+  def make_kata_stateless
+    kata = make_kata({ 'language' => 'Python-unittest' })
+    assert_equal 'stateless', kata.runner_choice
+    kata
+  end
+
+  def assert_spied_stateful(index, method_name, args)
+    assert_equal [
+      stateful_runner_name = 'runner',
+      stateful_runner_port = 4557,
+      method_name,
+      args
+    ], @http.spied[index]
+  end
+
+  def assert_spied_stateless(index, method_name, args)
+    assert_equal [
+      stateless_runner_name = 'runner_stateless',
+      stateless_runner_port = 4597,
+      method_name,
+      args
+    ], @http.spied[index]
+  end
+
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+  # image_pulled?
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '2BDF808C84',
+  'image_pulled? request forwards to stateful runner ' +
+  'for kata whose runner_choice is stateful' do
+    @http = HttpSpy.new(nil)
+    kata = make_kata_stateful
+    @http.clear
+    runner.image_pulled? kata.image_name, kata.id
+    assert_spied_stateful(0, 'image_pulled?', {:image_name=>kata.image_name, :kata_id => kata.id })
+  end
+
+  test '2BDF808C85',
+  'image_pulled? request forwards to stateless runner ' +
+  'for kata whose runner_choice is stateless' do
+    @http = HttpSpy.new(nil)
+    kata = make_kata_stateless
+    @http.clear
+    runner.image_pulled? kata.image_name, kata.id
+    assert_spied_stateless(0, 'image_pulled?', {:image_name=>kata.image_name, :kata_id => kata.id })
+  end
+
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+  # image_pull
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '2BDF808204',
+  'image_pull request forwards to stateful runner ' +
+  'for kata whose runner_choice is stateful' do
+    @http = HttpSpy.new(nil)
+    kata = make_kata_stateful
+    @http.clear
+    runner.image_pull kata.image_name, kata.id
+    assert_spied_stateful(0, 'image_pull', {:image_name=>kata.image_name, :kata_id => kata.id })
+  end
+
+  test '2BDF808205',
+  'image_pull request forwards to stateless runner ' +
+  'for kata whose runner_choice is stateless' do
+    @http = HttpSpy.new(nil)
+    kata = make_kata_stateless
+    @http.clear
+    runner.image_pull kata.image_name, kata.id
+    assert_spied_stateless(0, 'image_pull', {:image_name=>kata.image_name, :kata_id => kata.id })
+  end
+
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+  # kata_new
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '2BDF808351',
+  'kata_new request forwards to stateful runner ' +
+  'for kata whose runner_choice is stateful' do
+    @http = HttpSpy.new(nil)
+    kata = make_kata_stateful
+    assert_spied_stateful(0, 'kata_new', {:image_name=>kata.image_name, :kata_id => kata.id })
+  end
+
+  test '2BDF808352',
+  'kata_new request does not forward to stateless runner ' +
+  'for kata whose runner_choice is stateless' do
+    @http = HttpSpy.new(nil)
+    kata = make_kata_stateless
+    assert_nil @http.spied[0]
+  end
+
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+  # kata_old
+  #- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '2BDF808767',
+  'kata_old request forwards to stateful runner ' +
+  'for kata whose runner_choice is stateful' do
+    @http = HttpSpy.new(nil)
+    kata = make_kata_stateful
+    @http.clear
+    runner.kata_old(kata.image_name, kata.id)
+    assert_spied_stateful(0, 'kata_old', {:image_name=>kata.image_name, :kata_id => kata.id })
+  end
+
+  test '2BDF808768',
+  'kata_old request does not forward to stateless runner ' +
+  'for kata whose runner_choice is stateless' do
+    @http = HttpSpy.new(nil)
+    kata = make_kata_stateless
+    @http.clear
+    runner.kata_old(kata.image_name, kata.id)
+    assert_nil @http.spied[0]
+  end
+
   #- - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test '2BDF8082E5',
@@ -46,6 +168,15 @@ class RunnerServiceTest < AppLibTestBase
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - -
 
+  test '2BDF808601',
+  'stateless run() delegates to stateless runner',
+  'args do not include deleted_filenames or changed_files',
+  'but do include visible_files' do
+    assert_stateless_runner('2BDAD80601', 'cyberdojofoundation/gcc_assert_stateless')
+    assert_stateless_runner('2BDAD80602', 'quay.io:8080/cyberdojofoundation/gcc_assert_stateless')
+    assert_stateless_runner('2BDAD80603', 'localhost/cyberdojofoundation/gcc_assert_stateless:1.2')
+  end
+
   def assert_stateless_runner(kata_id, image_name)
     @http = HttpSpy.new(nil)
     args = []
@@ -62,15 +193,6 @@ class RunnerServiceTest < AppLibTestBase
     refute @http.spied_named_arg? :deleted_filenames
     refute @http.spied_named_arg? :changed_files
     assert @http.spied_named_arg? :visible_files
-  end
-
-  test '2BDF808601',
-  'stateless run() delegates to stateless runner',
-  'args do not include deleted_filenames or changed_files',
-  'but do include visible_files' do
-    assert_stateless_runner('2BDAD80601', 'cyberdojofoundation/gcc_assert_stateless')
-    assert_stateless_runner('2BDAD80602', 'quay.io:8080/cyberdojofoundation/gcc_assert_stateless')
-    assert_stateless_runner('2BDAD80603', 'localhost/cyberdojofoundation/gcc_assert_stateless:1.2')
   end
 
 end
