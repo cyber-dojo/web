d27f69c0774e88d4a3c849d90ea39e7d911bfd01
Jon Jagger <jon@jaggersoft.com>
2017-08-10 08:06:01 +0530

    don't try to curl a file to /tmp/cyber-dojo because that is often a dir

--- up.sh
+++ up.sh
@@ -6,19 +6,20 @@ my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
 one_time_creation_of_start_point_volumes()
 {
-  curl https://raw.githubusercontent.com/cyber-dojo/commander/master/cyber-dojo > /tmp/cyber-dojo
-  chmod +x /tmp/cyber-dojo
+  local NAME=/tmp/cyber-dojo-one-time
+  curl https://raw.githubusercontent.com/cyber-dojo/commander/master/cyber-dojo > ${NAME}
+  chmod +x ${NAME}
   local GIT_URL=https://github.com/cyber-dojo/start-points-
   set +e
   # These all fail (and do nothing) if the start-point already exists
-  /tmp/cyber-dojo start-point create ${CYBER_DOJO_START_POINT_LANGUAGES} \
+  ${NAME} start-point create ${CYBER_DOJO_START_POINT_LANGUAGES} \
       --list=https://raw.githubusercontent.com/cyber-dojo/start-points-languages/master/languages_list_travis 2> /dev/null
-  /tmp/cyber-dojo start-point create ${CYBER_DOJO_START_POINT_EXERCISES} \
+  ${NAME} start-point create ${CYBER_DOJO_START_POINT_EXERCISES} \
       --git=${GIT_URL}exercises.git 2> /dev/null
-  /tmp/cyber-dojo start-point create ${CYBER_DOJO_START_POINT_CUSTOM} \
+  ${NAME} start-point create ${CYBER_DOJO_START_POINT_CUSTOM} \
       --git=${GIT_URL}custom.git 2> /dev/null
   set -e
-  rm /tmp/cyber-dojo
+  rm ${NAME}
 }
 
 one_time_creation_of_start_point_volumes
