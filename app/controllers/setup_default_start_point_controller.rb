956641777e6251694210e3a134ea06bdafa1c7c4
Jon Jagger <jon@jaggersoft.com>
2017-12-18 13:58:43 +0000

    refactoring; used major/minor names consistently in setup

--- app/controllers/setup_default_start_point_controller.rb
+++ app/controllers/setup_default_start_point_controller.rb
@@ -6,9 +6,9 @@ class SetupDefaultStartPointController < ApplicationController
   # step 2. exercise                                (eg Fizz_Buzz)
 
   def show_languages
-    @id = id
     current_display_name = storer.kata_exists?(id) ? katas[id].display_name : nil
     choices = starter.languages_choices(current_display_name)
+    @id = id
     @major_names   = choices['major_names']
     @major_index   = choices['major_index']
     @minor_names   = choices['minor_names']
@@ -16,12 +16,10 @@ class SetupDefaultStartPointController < ApplicationController
   end
 
   def show_exercises
-    @language = params['language']
-    @test = params['test']
-    #@major = params['major] # TODO
-    #@minor = params['minor] # TODO
     current_exercise_name = storer.kata_exists?(id) ? katas[id].exercise : nil
     choices = starter.exercises_choices(current_exercise_name)
+    @major = params['major']
+    @minor = params['minor']
     @exercises_names = choices['names']
     @exercises       = choices['contents']
     @initial_index   = choices['index']
@@ -30,8 +28,8 @@ class SetupDefaultStartPointController < ApplicationController
   def save
     major = params['major']
     minor = params['minor']
-    exercise_name = params['exercise']
-    manifest = starter.language_manifest(major, minor, exercise_name)
+    exercise = params['exercise']
+    manifest = starter.language_manifest(major, minor, exercise)
     kata = katas.create_kata(manifest)
     render json: {
       image_name: kata.image_name,
