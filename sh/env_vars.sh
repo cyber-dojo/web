25791f1d2993e1bb8b3dae2107fdcfb6dfb300f1
Jon Jagger <jon@jaggersoft.com>
2017-12-18 08:23:45 +0000

    added starter service to docker-compose.yml

--- sh/env_vars.sh
+++ sh/env_vars.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export CYBER_DOJO_HOME=/app
+
+export CYBER_DOJO_START_POINTS_ROOT=/app/start_points
 export CYBER_DOJO_START_POINT_LANGUAGES=languages_small
 export CYBER_DOJO_START_POINT_EXERCISES=exercises
 export CYBER_DOJO_START_POINT_CUSTOM=custom
