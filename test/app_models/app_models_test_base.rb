3586e7a42a0b42de516f13747834fc5319e83672
Jon Jagger <jon@jaggersoft.com>
2017-12-21 08:10:11 +0000

    refactoring; deleted dead test code

--- test/app_models/app_models_test_base.rb
+++ test/app_models/app_models_test_base.rb
@@ -2,11 +2,4 @@ require_relative '../all'
 
 class AppModelsTestBase < TestBase
 
-  def correct_path_format?(object)
-    path = object.path
-    ends_in_slash = path.end_with?('/')
-    has_doubled_separator = path.scan('/' * 2).length != 0
-    !ends_in_slash && !has_doubled_separator
-  end
-
 end
