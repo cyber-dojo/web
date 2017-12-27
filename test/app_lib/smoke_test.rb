47e2c27803536c893e785caadf37a8171be96f2a
Jon Jagger <jon@jaggersoft.com>
2017-12-27 15:20:39 +0000

    refactoring; added missing starter-service smoke tests

--- test/app_lib/smoke_test.rb
+++ test/app_lib/smoke_test.rb
@@ -9,6 +9,58 @@ class SmokeTest < AppLibTestBase
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # starter
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  smoke_test '9823AA',
+  'smoke test starter-service' do
+    json = starter.custom_choices
+    assert_equal [ 'Yahtzee refactoring' ], json['major_names']
+    assert_equal [
+      'C# NUnit',
+      'C++ (g++) assert',
+      'Java JUnit',
+      'Python unitttest'
+    ], json['minor_names']
+    assert_equal [[0,1,2,3]], json['minor_indexes']
+
+    json = starter.languages_choices
+    assert_equal [
+      'C (gcc)',
+      'C#',
+      'C++ (g++)',
+      'Python',
+      'Ruby'
+    ], json['major_names']
+    assert_equal [
+      'NUnit',
+      'RSpec',
+      'assert',
+      'behave',
+      'py.test',
+      'unittest'
+    ], json['minor_names']
+    assert_equal [[2],[0],[2],[3,4,5],[1]], json['minor_indexes']
+
+    json = starter.exercises_choices
+    assert_equal [
+      'Bowling_Game',
+      'Fizz_Buzz',
+      'Leap_Years',
+      'Tiny_Maze'
+    ], json['names']
+
+    manifest = starter.custom_manifest('Yahtzee refactoring', 'C# NUnit')
+    assert_equal 'Yahtzee refactoring, C# NUnit', manifest['display_name']
+
+    manifest = starter.language_manifest('C#', 'NUnit', 'Fizz_Buzz')
+    assert_equal 'C#, NUnit', manifest['display_name']
+
+    manifest = starter.manifest('C')
+    assert_equal 'C (gcc), assert', manifest['display_name']
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
   # differ
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
