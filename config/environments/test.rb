64f54292c89b8ce219712593da164975e6224a58
Jon Jagger <jon@jaggersoft.com>
2017-12-28 17:51:29 +0000

    refactoring; update config files to remove deprecation warnings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,7 +8,7 @@ CyberDojo::Application.configure do
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
 
   # Show full error reports and disable caching
@@ -31,6 +31,9 @@ CyberDojo::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Opt into Rails 5 behaviour
+  config.active_support.test_order = :sorted
 
   config.exceptions_app = lambda do |env|
     ExceptionController.action(:render_error).call(env)
