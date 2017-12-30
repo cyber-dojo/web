78e3e321fdfd232abbe91ae71a6026f550a3ee37
Jon Jagger <jon@jaggersoft.com>
2017-12-30 17:13:02 +0000

    [ci skip] update word_count script

--- script/stats/word_count.sh
+++ script/stats/word_count.sh
@@ -3,8 +3,17 @@ sed 's/test/ /g' | \
 sed 's/assert_equal/ /g' | \
 sed 's/end/ /g' | \
 sed 's/def/ /g' | \
+sed 's/do/ /g' | \
 tr '0' ' ' | \
 tr '1' ' ' | \
+tr '2' ' ' | \
+tr '3' ' ' | \
+tr '4' ' ' | \
+tr '5' ' ' | \
+tr '6' ' ' | \
+tr '7' ' ' | \
+tr '8' ' ' | \
+tr '9' ' ' | \
 tr '.' ' ' | \
 tr '(' ' ' | \
 tr ')' ' ' | \
