6885d7ea408f4a4503110220e684415552413101
Jon Jagger <jon@jaggersoft.com>
2017-12-31 09:26:56 +0000

    refactoring; reorder requires

--- test/all.rb
+++ test/all.rb
@@ -2,7 +2,6 @@
 # This line must come first, before any required/loaded files to be covered.
 require_relative './test_coverage'
 
-require 'json'
 require_relative '../lib/all'
 require_relative '../app/helpers/all'
 require_relative '../app/lib/all'
@@ -10,3 +9,5 @@ require_relative '../app/models/all'
 require_relative '../app/services/all'
 
 require_relative './test_base'
+
+require 'json'
