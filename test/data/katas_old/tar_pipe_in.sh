7880a1e7770d1ae75e4397a15000a52b4f3a694c
Jon Jagger <jon@jaggersoft.com>
2017-12-31 16:20:10 +0000

    refactoring; practise with an S is a verb, licence with a C is a noun

--- test/data/katas_old/tar_pipe_in.sh
+++ test/data/katas_old/tar_pipe_in.sh
@@ -5,26 +5,32 @@ set -e
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly KATA_IDS=(5A0F824303 420B05BA0A 420F2A2979 421F303E80 420BD5D5BE 421AFD7EC5)
-readonly CONTAINER='test_web_cyber-dojo-storer'
+readonly STORER_CONTAINER='test_web_cyber-dojo-storer'
 
 . ${MY_DIR}/../../../.env
 
+# - - - - - - - - - - - - - - - - - - - - - - - -
 # make sure ${CYBER_DOJO_KATAS_ROOT} dir exists
+
 docker exec \
-  ${CONTAINER} \
+  ${STORER_CONTAINER} \
     sh -c "mkdir -p ${CYBER_DOJO_KATAS_ROOT}"
 
-# tar-pipe test data into storer's katas data-container
+# - - - - - - - - - - - - - - - - - - - - - - - -
+# tar-pipe test data into storer-container
+
 for KATA_ID in "${KATA_IDS[@]}"
 do
   cat ${MY_DIR}/${KATA_ID}.tgz \
     | docker exec \
         --interactive \
-          ${CONTAINER} \
+          ${STORER_CONTAINER} \
             sh -c "tar -zxf - -C ${CYBER_DOJO_KATAS_ROOT}"
 done
 
-# set ownership of test-data in storer's katas data-container
+# - - - - - - - - - - - - - - - - - - - - - - - -
+# set ownership of test-data in storer-container
+
 docker exec \
-    ${CONTAINER} \
+    ${STORER_CONTAINER} \
       sh -c "chown -R storer:storer ${CYBER_DOJO_KATAS_ROOT}"
