c392aa2093adf267cf20fa2e2e7d6724b283f581
Jon Jagger <jon@jaggersoft.com>
2018-01-01 07:17:59 +0000

    refactoring; bring down containers if all tests pass

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
-readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-${SH_DIR}/build_docker_image.sh
-${SH_DIR}/docker_containers_up.sh
-${SH_DIR}/../test/data/katas_old/tar_pipe_in.sh
-${SH_DIR}/run_tests_in_container.sh ${*}
-#${SH_DIR}/docker_containers_down.sh
+${MY_DIR}/sh/docker_containers_down.sh
+${MY_DIR}/sh/build_docker_image.sh
+${MY_DIR}/sh/docker_containers_up.sh
+${MY_DIR}/test/data/katas_old/tar_pipe_in.sh
+${MY_DIR}/sh/run_tests_in_container.sh ${*}
+if [ $? -eq 0 ]; then
+  ${MY_DIR}/sh/docker_containers_down.sh
+fi
