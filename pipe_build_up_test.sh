b6e7e1bc3829d52bdaf7b81412d31bb5728aeec4
Jon Jagger <jon@jaggersoft.com>
2018-01-02 21:09:37 +0000

    refactoring; renamed dirs to make it clear whats test related

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -6,7 +6,7 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 ${MY_DIR}/sh/docker_containers_down.sh
 ${MY_DIR}/sh/build_docker_image.sh
 ${MY_DIR}/sh/docker_containers_up.sh
-${MY_DIR}/katas_old/tar_pipe_into_storer.sh
+${MY_DIR}/test_katas_old/tar_pipe_into_storer.sh
 ${MY_DIR}/sh/run_tests_in_container.sh ${*}
 if [ $? -eq 0 ]; then
   ${MY_DIR}/sh/docker_containers_down.sh
