79dbd6e55f6eaba665a8350ee9a5df9d1eaa15f6
Jon Jagger <jon@jaggersoft.com>
2018-01-05 08:54:03 +0000

    refactoring; test improvements

--- test/app_services/differ_service_test.rb
+++ test/app_services/differ_service_test.rb
@@ -14,27 +14,28 @@ class DifferServiceTest < AppServicesTestBase
 
   test '3AB',
   'smoke test' do
-    kata = make_language_kata({
-      'display_name' => default_language_name('stateful')
-    })
-    kata.start_avatar(['lion'])
+    kata = make_language_kata({ 'display_name' => 'C (gcc), assert' })
+    avatar = kata.start_avatar
     begin
       args = []
       args << kata.id
-      args << 'lion'
-      args << (files1 = starting_files)
-      args << (now1 = [2016,12,8, 8,3,23])
+      args << avatar.name
+      args << avatar.visible_files
+      args << (now = [2016,12,8, 8,3,23])
       args << (output = 'Assert failed: answer() == 42')
       args << (colour = 'red')
       storer.avatar_ran_tests(*args)
-      actual = differ.diff(kata.id, 'lion', was_tag=0, now_tag=1)
+
+      actual = differ.diff(kata.id, avatar.name, was_tag=0, now_tag=1)
 
       refute_nil actual['hiker.c']
       assert_equal({
-        "type"=>"same", "line"=>"#include \"hiker.h\"", "number"=>1
+        'type'   => 'same',
+        'line'   => '#include "hiker.h"',
+        'number' => 1
       }, actual['hiker.c'][0])
     ensure
-      runner.avatar_old(kata.image_name, kata.id, 'lion')
+      runner.avatar_old(kata.image_name, kata.id, avatar.name)
       runner.kata_old(kata.image_name, kata.id)
     end
   end
