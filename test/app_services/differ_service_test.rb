ab0318343835b618f4326604f7599f236cd0bec3
Jon Jagger <jon@jaggersoft.com>
2018-01-06 09:21:12 +0000

    refactoring; use RunnerStub in differ_service test

--- test/app_services/differ_service_test.rb
+++ test/app_services/differ_service_test.rb
@@ -6,38 +6,31 @@ class DifferServiceTest < AppServicesTestBase
     '702922'
   end
 
-  def hex_setup
-    set_differ_class('DifferService')
-  end
-
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test '3AB',
   'smoke test' do
-    kata = make_language_kata({ 'display_name' => 'C (gcc), assert' })
-    avatar = kata.start_avatar
-    begin
-      args = []
-      args << kata.id
-      args << avatar.name
-      args << avatar.visible_files
-      args << (now = [2016,12,8, 8,3,23])
-      args << (output = 'Assert failed: answer() == 42')
-      args << (colour = 'red')
-      storer.avatar_ran_tests(*args)
+    in_kata(:stateful) {
+      as(:wolf) {
+        args = []
+        args << kata.id
+        args << wolf.name
+        args << wolf.visible_files
+        args << (now = [2016,12,8, 8,3,23])
+        args << (output = 'Assert failed: answer() == 42')
+        args << (colour = 'red')
+        storer.avatar_ran_tests(*args)
 
-      actual = differ.diff(kata.id, avatar.name, was_tag=0, now_tag=1)
+        actual = differ.diff(kata.id, wolf.name, was_tag=0, now_tag=1)
 
-      refute_nil actual['hiker.c']
-      assert_equal({
-        'type'   => 'same',
-        'line'   => '#include "hiker.h"',
-        'number' => 1
-      }, actual['hiker.c'][0])
-    ensure
-      runner.avatar_old(kata.image_name, kata.id, avatar.name)
-      runner.kata_old(kata.image_name, kata.id)
-    end
+        refute_nil actual['hiker.c']
+        assert_equal({
+          'type'   => 'same',
+          'line'   => '#include "hiker.h"',
+          'number' => 1
+        }, actual['hiker.c'][0])
+      }
+    }
   end
 
 end
--- test/test_domain_helpers.rb
+++ test/test_domain_helpers.rb
@@ -1,6 +1,45 @@
 
 module TestDomainHelpers # mix-in
 
+  def in_kata(runner_choice = :stateless, &block)
+    display_name = {
+       stateless: 'Python, unittest',
+        stateful: 'C (gcc), assert',
+      processful: 'Python, py.test'
+    }[runner_choice]
+    refute_nil display_name, runner_choice
+    make_language_kata({ 'display_name' => display_name })
+    begin
+      block.call
+    ensure
+      runner.kata_old(kata.image_name, kata.id)
+    end
+  end
+
+  # - - - - - - - - - - - - - - - -
+
+  def as(name = :wolf, &block)
+    avatar = kata.start_avatar([name.to_s])
+    begin
+      block.call
+    ensure
+      runner.avatar_old(kata.image_name, kata.id, avatar.name)
+    end
+  end
+
+  # - - - - - - - - - - - - - - - -
+
+  def wolf
+    # Idea: wrap the avatar object inside a DeltaMaker
+    # and return that as a 'proxy'. This would allow
+    # lines such as
+    #    maker = DeltaMaker.new(wolf)
+    # to disappear from test code
+    kata.avatars['wolf']
+  end
+
+  # - - - - - - - - - - - - - - - -
+
   def make_language_kata(options = {})
     display_name = options['display_name'] || default_language_name
     exercise_name = options['exercise'] || default_exercise_name
