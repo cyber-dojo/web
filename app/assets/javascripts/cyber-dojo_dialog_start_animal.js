121570a49886858cbd0a5a77481560d08c7af801
Jon Jagger <jon@jaggersoft.com>
2018-01-31 22:16:57 +0000

    fixed reset to home-page when new avatar is started

--- app/assets/javascripts/cyber-dojo_dialog_start_animal.js
+++ app/assets/javascripts/cyber-dojo_dialog_start_animal.js
@@ -3,12 +3,12 @@
 var cyberDojo = (function(cd, $) {
   "use strict";
 
-  const title = ''; // none
+  const noTitle = '';
 
   cd.startAnimal = function(id, from) {
     $.getJSON('/enter/start', { id: id }, function(dojo) {
       if (dojo.full) {
-        cd.dialog(dojo.full_dialog_html, title, 'ok').dialog('open');
+        cd.dialog(dojo.full_dialog_html, noTitle, 'ok').dialog('open');
       } else {
         startDialog(id, dojo.avatar_name, dojo.start_dialog_html, from);
       }
@@ -18,15 +18,13 @@ var cyberDojo = (function(cd, $) {
   var startDialog = function(id, avatarName, dialogHtml, from) {
     var url = '/kata/edit/' + id + '?avatar=' + avatarName;
     var okOrCancel = function() {
-      if (from == 'from_setup') {
-        window.location = cd.homePageUrl(id);
-      }
+      window.location.href = cd.homePageUrl(id);
       window.open(url);
     };
     $('<div class="dialog">')
       .html(dialogHtml)
       .dialog({
-        title: cd.dialogTitle(title),
+        title: cd.dialogTitle(noTitle),
         autoOpen: true,
         width: 400,
         modal: true,
