597831eeb381baf3fda96f2043290ab7043145d1
Jon Jagger <jon@jaggersoft.com>
2018-02-15 20:51:35 +0000

    default setup page now shows info on lhs when [language,testFramework] or [exercise] is selected from list

--- app/controllers/setup_default_start_point_controller.rb
+++ app/controllers/setup_default_start_point_controller.rb
@@ -4,15 +4,19 @@ class SetupDefaultStartPointController < ApplicationController
   def show
     sp = starter.languages_exercises_start_points
     @id = id
-    @languages_names = sp['languages'].sort
-    @language_index = 0 # TODO
+    @languages_names = sp['languages']
+    @language_index = 1 # TODO
     #current_display_name = kata.exists? ? kata.display_name : nil
     #display_name_index(choices, current_display_name)
 
     @exercises_names = sp['exercises'].keys.sort
-    @exercise_index = 0 # TODO
+    @exercise_index = 1 # TODO
     #current_exercise_name = kata.exists? ? kata.exercise : nil
     #exercise_index(choices, current_exercise_name)
+    @instructions = []
+    @exercises_names.each do |name|
+      @instructions << sp['exercises'][name]
+    end
 
     @from = params['from']
   end
