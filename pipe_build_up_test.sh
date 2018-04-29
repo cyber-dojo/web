24d1208726c1a0496afd3fd22b7005e0092c7bb4
Jon Jagger <jon@jaggersoft.com>
2018-04-29 16:17:37 +0100

    refactoring; bash quote style

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -5,9 +5,9 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 export SHA=$(cd "${MY_DIR}" && git rev-parse HEAD)
 
-"${MY_DIR}"/sh/docker_containers_down.sh
-"${MY_DIR}"/sh/build_docker_images.sh
-"${MY_DIR}"/sh/docker_containers_up.sh
-if "${MY_DIR}"/sh/run_tests_in_container.sh "$@"; then
-  "${MY_DIR}"/sh/docker_containers_down.sh
+"${MY_DIR}/sh/docker_containers_down.sh"
+"${MY_DIR}/sh/build_docker_images.sh"
+"${MY_DIR}/sh/docker_containers_up.sh"
+if "${MY_DIR}/sh/run_tests_in_container.sh" "$@"; then
+  "${MY_DIR}/sh/docker_containers_down.sh"
 fi
