ee81735c161501711c329c7664bbd7bd642ee145
Jon Jagger <jon@jaggersoft.com>
2018-05-21 08:11:47 +0100

    refactoring; export sha in lower-level .sh file so image has sha.txt when working in fast local loop

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -6,6 +6,8 @@ readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
 . "${ROOT_DIR}/.env"
 
+export SHA=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
+
 docker-compose \
   --file "${ROOT_DIR}/docker-compose.yml" \
   build
