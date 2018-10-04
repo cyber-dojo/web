5802221ab6135871a28e95c173b9263d7cc83fb2
Jon Jagger <jon@jaggersoft.com>
2018-10-04 15:45:59 +0100

    added back sha/index entries

--- app/controllers/sha_controller.rb
+++ app/controllers/sha_controller.rb
@@ -3,14 +3,14 @@ class ShaController < ApplicationController
 
   def index
     @shas = {
-      #'differ' => differ.sha,
-      #'starter' => starter.sha,
-      #'grouper' => grouper.sha,
-      #'singler' => singler.sha,
+      'differ' => differ.sha,
+      'starter' => starter.sha,
+      'grouper' => grouper.sha,
+      'singler' => singler.sha,
       'web' => web_sha,
-      #'zipper' => zipper.sha,
-      #'runner-stateless' => runner.sha('stateless'),
-      #'runner-stateful' => runner.sha('stateful'),
+      'zipper' => zipper.sha,
+      'runner-stateless' => runner.sha('stateless'),
+      'runner-stateful' => runner.sha('stateful'),
     }
   end
 
