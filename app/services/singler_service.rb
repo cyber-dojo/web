d0686498d5187598e9ae8e734443e807ea7040bc
Jon Jagger <jon@jaggersoft.com>
2018-10-06 15:47:28 +0100

    update singler/grouper adapters

--- app/services/singler_service.rb
+++ app/services/singler_service.rb
@@ -14,26 +14,16 @@ class SinglerService
 
   # - - - - - - - - - - - -
 
+  def exists?(id)
+    http_get(__method__, id)
+  end
+
   def create(manifest, files)
     http_post(__method__, manifest, files)
   end
 
   def manifest(id)
     http_get(__method__, id)
-  end
-
-  # - - - - - - - - - - - -
-
-  def id?(id)
-    http_get(__method__, id)
-  end
-
-  def id_completed(partial_id)
-    http_get(__method__, partial_id)
-  end
-
-  def id_completions(outer_id)
-    http_get(__method__, outer_id)
   end
 
   # - - - - - - - - - - - -
