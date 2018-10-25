eda99e89a38062cad82d09f5cece57f4d6d741bc
Jon Jagger <jon@jaggersoft.com>
2018-10-25 13:24:56 +0100

    start removing avatar-name from review URL

--- app/assets/javascripts/cyber-dojo_show_review_url.js
+++ app/assets/javascripts/cyber-dojo_show_review_url.js
@@ -3,10 +3,9 @@
 var cyberDojo = ((cd, $) => {
   'use strict';
 
-  cd.showReviewUrl = (id, avatarName, wasTag, nowTag, filename) => {
+  cd.showReviewUrl = (id, _avatarName, wasTag, nowTag, filename) => {
     return '/review/show/' + id +
-      '?avatar=' + avatarName +
-      '&was_tag=' + wasTag +
+      '?was_tag=' + wasTag +
       '&now_tag=' + nowTag +
       '&filename=' + filename;
   };
