5e09ecac244729c2e4868631dd7cb7c3bf8f2b48
Jon Jagger <jon@jaggersoft.com>
2018-11-01 07:16:59 +0000

    refactoring; use simpler define_method in app_models/manifest

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -9,17 +9,17 @@ class Manifest
 
   def self.required(*names)
     names.each do |name|
-      define_method name, &lambda {
+      define_method name do
         @manifest[name.to_s]
-      }
+      end
     end
   end
 
   def self.optional(fields)
     fields.each do |name,default|
-      define_method name, &lambda {
+      define_method name do
         @manifest[name.to_s] || default
-      }
+      end
     end
   end
 
@@ -29,7 +29,7 @@ class Manifest
            :group_index, # eg 45 (salmon), nil if !group-practice-session
            :created,            # eg [2018,10,14, 9,50,23]
            :display_name,       # eg 'Java, JUnit'
-           :filename_extension, # eg [ ".java" ]
+           :filename_extension, # eg [ '.java' ]
            :id,                 # eg '260za8'
            :image_name,         # eg 'cyberdojofoundation/java_junit'
            :runner_choice       # eg 'stateless'
