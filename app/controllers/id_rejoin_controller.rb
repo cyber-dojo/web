6062e176009893328dbece08b355eafb66a23c65
Jon Jagger <jon@jaggersoft.com>
2018-11-02 10:57:52 +0000

    start work on individual rejoin

--- app/controllers/id_rejoin_controller.rb
+++ app/controllers/id_rejoin_controller.rb
@@ -1,18 +1,53 @@
 
 class IdRejoinController < ApplicationController
 
+  def show
+    @from = from
+  end
+
   def drop_down
-    # TODO: individual-rejoin
+    if from == 'individual'
+      json = individual_drop_down
+    end
+    if from == 'group'
+      json = group_drop_down
+    end
+    render json:json
+  end
+
+  private
+
+  def from
+    params[:from]
+  end
+
+  def individual_drop_down
+    # id could be old id for a group
+    #    if it has a single avatar
+    #      go straight to the avatar using its kata-id
+    #    if it has several avatars
+    #      ???
+    
+
+    # id could be new id for a kata
+    #    go straight to the kata
+    kata = katas[id]
+    json = { exists:kata.exists? }
+    if json[:exists]
+      json[:kataId] = id
+    end
+    json
+  end
+
+  def group_drop_down
     group = groups[porter.port(id)]
     json = { exists:group.exists? }
     if json[:exists]
       json[:empty] = group.empty?
       json[:avatarPickerHtml] = avatar_picker_html(group.katas)
     end
-    render json:json
+    json
   end
-
-  private
 
   def avatar_picker_html(katas)
     @avatar_names = Avatars.names
