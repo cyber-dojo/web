dfa46f938b6c9c94b1ca7af5985f4a63b20e621e
Jon Jagger <jon@jaggersoft.com>
2018-11-02 12:46:22 +0000

    refactoring

--- app/controllers/id_join_controller.rb
+++ app/controllers/id_join_controller.rb
@@ -1,28 +1,23 @@
 
 class IdJoinController < ApplicationController
 
-  # Only for a group practice-session
+  def show
+    @avatar_names = Avatars.names
+  end
+
   def drop_down
     group = groups[porter.port(id)]
     json = { exists:group.exists? }
     if json[:exists]
       kata = group.join
-      json[:full] = kata.nil?
-      if json[:full]
-        json[:fullHtml] = full_html
-      else
+      if kata
         json[:id] = kata.id
         json[:avatarName] = kata.avatar_name
+      else
+        json[:full] = true
       end
     end
     render json:json
-  end
-
-  private
-
-  def full_html
-    @avatar_names = Avatars.names
-    bind('/app/views/id_join/full.html.erb')
   end
 
 end
