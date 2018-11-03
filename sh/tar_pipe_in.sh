59c768bdfae0121ebbbe51d57c26279a054c7221
Jon Jagger <jon@jaggersoft.com>
2018-11-03 09:31:55 +0000

    update script to use new inserter container

--- sh/tar_pipe_in.sh
+++ sh/tar_pipe_in.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+set -e
 
-readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
+readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly STORER_CONTAINER=cyber-dojo-storer
 
 echo "clearing out old ${STORER_CONTAINER}"
 docker exec -it ${STORER_CONTAINER} sh -c 'rm -rf /usr/src/cyber-dojo/katas/*'
 
 echo "filling old ${STORER_CONTAINER} will test data"
-${ROOT_DIR}/../porter/test/storer_katas/old/tar_pipe_in.sh ${STORER_CONTAINER}
-${ROOT_DIR}/../porter/test/storer_katas/dup/tar_pipe_in.sh ${STORER_CONTAINER}
+${SH_DIR}/insert_katas_test_data.sh ${STORER_CONTAINER}
 
 echo "clearing out new saver"
 docker-machine ssh default 'cd /tmp/id-map && sudo rm -rf *'
