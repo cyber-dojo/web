5a06c20d4a952471821f5fc628149dcbcddbef48
Jon Jagger <jon@jaggersoft.com>
2018-11-09 11:57:38 +0000

    refactoring; delete dead code

--- app/controllers/dojo_controller.rb
+++ app/controllers/dojo_controller.rb
@@ -5,7 +5,6 @@ class DojoController < ApplicationController
     @title = 'home'
     @id = id
     @message = ENV['MESSAGE']
-    @shuffled_avatar_names = Avatars.names.shuffle
     @pies_filename = %w( Coimbatore Bray Mullingar ).shuffle[0]
   end
 
