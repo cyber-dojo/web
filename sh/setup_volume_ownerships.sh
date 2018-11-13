fac4d3d3ab46987a5987d40e7f0749efce217430
Jon Jagger <jon@jaggersoft.com>
2018-11-13 09:09:28 +0000

    added porter-service tests

--- sh/setup_volume_ownerships.sh
+++ sh/setup_volume_ownerships.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "setting ownerships in porter and saver"
-docker-machine ssh default 'cd /tmp/id-map && sudo rm -rf * && sudo chown -R 19664 .'
-docker-machine ssh default 'cd /tmp/groups && sudo rm -rf * && sudo chown -R 19663 .'
-docker-machine ssh default 'cd /tmp/katas  && sudo rm -rf * && sudo chown -R 19663 .'
+# TODO: the commands below are specific to DockerToolbox...
+# if not on DockerToolbox then drop the docker-machine ssh
+# How can you tell?
+
+echo "setting ownership in porter"
+docker-machine ssh default \
+  'cd /tmp/id-map && sudo rm -rf * && sudo chown -R 19664 .'
+
+echo "setting ownership in saver"
+docker-machine ssh default \
+  'cd /tmp/groups && sudo rm -rf * && sudo chown -R 19663 .'
+docker-machine ssh default \
+  'cd /tmp/katas  && sudo rm -rf * && sudo chown -R 19663 .'
