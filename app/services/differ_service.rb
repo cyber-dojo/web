8f9075ba8af510992a4f4ae021aa9bc5ca008be7
Jon Jagger <jon@jaggersoft.com>
2018-11-23 08:56:56 +0000

    refactoring; drop comment lines

--- app/services/differ_service.rb
+++ app/services/differ_service.rb
@@ -6,13 +6,9 @@ class DifferService
     @http = HttpHelper.new(externals, self, 'differ', 4567)
   end
 
-  # - - - - - - - - - - - - -
-
   def sha
     http.get
   end
-
-  # - - - - - - - - - - - - -
 
   def diff(was_files, now_files)
     http.get(was_files, now_files)
--- app/services/runner_service.rb
+++ app/services/runner_service.rb
@@ -6,13 +6,9 @@ class RunnerService
     @http = HttpHelper.new(externals, self, 'runner', 4597)
   end
 
-  # - - - - - - - - - - - - - - - - - - - - - - - -
-
   def sha
     http.get
   end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - -
 
   def run_cyber_dojo_sh(image_name, id, files, max_seconds)
     http.post(image_name, id, files, max_seconds)
