dfd59ab5458acc28579e227affe641ba945f153d
Jon Jagger <jon@jaggersoft.com>
2018-11-23 12:53:58 +0000

    refactoring

--- lib/cleaner.rb
+++ lib/cleaner.rb
@@ -4,15 +4,9 @@ module Cleaner # mix-in
   module_function
 
   def cleaned_files(files)
-    # If files is a Rails ActionController::Parameters then
-    # you can't do a .map or .to_h on it, so using .each
-    cleaned = {}
-    files.each do |filename, content|
-      content = cleaned_string(content)
-      content = content.gsub(/\r\n/, "\n")
-      cleaned[filename] = content
+    files.transform_values! do |content|
+      cleaned_string(content).gsub(/\r\n/, "\n")
     end
-    cleaned
   end
 
   def cleaned_string(s)
