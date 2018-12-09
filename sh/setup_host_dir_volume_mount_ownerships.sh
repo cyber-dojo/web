21e38e00828e3b46803655bb3de599eefc405c88
Jon Jagger <jon@jaggersoft.com>
2018-12-09 14:07:10 +0000

    refactoring; delete dead code

--- sh/setup_host_dir_volume_mount_ownerships.sh
+++ sh/setup_host_dir_volume_mount_ownerships.sh
@@ -5,18 +5,11 @@ chown_saver_dir()
 {
   # The dirs have already been created via the
   # docker_containers_up.sh command.
-  # See the volume-mounts in docker-compose.yml
+  # See saver's volume-mounts in docker-compose.yml
   local dir_name=$1
   local gid=19663
   local command="cd ${dir_name} && rm -rf * && chown -R ${gid}:${gid} ."
   docker exec --user root test-web-saver sh -c "${command}"
-  #if [[ ! -z ${DOCKER_MACHINE_NAME} && -z ${TRAVIS} ]]; then
-  #  echo "...running: docker-machine ssh default '${command}'"
-  #  docker-machine ssh default "${command}"
-  #else
-  #  echo "...running: ${command}"
-  #  ${command}
-  #fi
 }
 
 echo "clearing out /groups and setting its ownership to saver"
