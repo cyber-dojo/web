9c3a70c62a1dc57157e3e6fc747a0fc1a11c4cf5
Jon Jagger <jon@jaggersoft.com>
2018-12-09 14:04:31 +0000

    try simpler approach to setting ownership of /groups and /katas dirs as needed by saver

--- sh/setup_host_dir_volume_mount_ownerships.sh
+++ sh/setup_host_dir_volume_mount_ownerships.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 set -e
 
-chown_dir()
+chown_saver_dir()
 {
   # The dirs have already been created via the
   # docker_containers_up.sh command.
   # See the volume-mounts in docker-compose.yml
   local dir_name=$1
-  local gid=$2
-  local command="cd ${dir_name} && sudo rm -rf * && sudo chown -R ${gid}:${gid} ."
-  if [[ ! -z ${DOCKER_MACHINE_NAME} && -z ${TRAVIS} ]]; then
-    echo "...running: docker-machine ssh default '${command}'"
-    docker-machine ssh default "${command}"
-  else
-    echo "...running: ${command}"
-    ${command}
-  fi
+  local gid=19663
+  local command="cd ${dir_name} && rm -rf * && chown -R ${gid}:${gid} ."
+  docker exec --user root test-web-saver sh -c "${command}"
+  #if [[ ! -z ${DOCKER_MACHINE_NAME} && -z ${TRAVIS} ]]; then
+  #  echo "...running: docker-machine ssh default '${command}'"
+  #  docker-machine ssh default "${command}"
+  #else
+  #  echo "...running: ${command}"
+  #  ${command}
+  #fi
 }
 
-echo "clearing out /tmp/groups and setting its ownership to saver"
-chown_dir '/tmp/groups' 19663
+echo "clearing out /groups and setting its ownership to saver"
+chown_saver_dir '/groups'
 
 echo "clearing out /tmp/katas and setting its ownership to saver"
-chown_dir '/tmp/katas'  19663
+chown_saver_dir '/katas'
