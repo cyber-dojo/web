f16ab0eefba124e8177eb825e02578532e25e205
Jon Jagger <jon@jaggersoft.com>
2018-12-10 14:41:44 +0100

    simplify saver so it has a single volume-mount

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -7,7 +7,6 @@ readonly SH_DIR="${ROOT_DIR}/sh"
 "${SH_DIR}/docker_containers_down.sh"
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
-"${SH_DIR}/setup_host_dir_volume_mount_ownerships.sh"
 if "${SH_DIR}/run_tests_in_container.sh" "$@"; then
   "${SH_DIR}/docker_containers_down.sh"
 else
