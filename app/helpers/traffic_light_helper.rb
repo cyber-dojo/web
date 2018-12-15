3b15ea6a87dc85b4731c3f225dd2fabb1abf277c
Jon Jagger <jon@jaggersoft.com>
2018-12-15 16:41:42 +0000

    right-align traffic-light-count on dashboard

--- app/helpers/traffic_light_helper.rb
+++ app/helpers/traffic_light_helper.rb
@@ -29,13 +29,15 @@ module TrafficLightHelper # mix-in
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   def traffic_light_count(lights)
-    "<div class='traffic-light-count #{lights[-1].colour}'" +
-        " data-tip='traffic_light_count'" +
-        " data-red-count='#{colour_count(lights, :red)}'" +
-        " data-amber-count='#{colour_count(lights, :amber)}'" +
-        " data-green-count='#{colour_count(lights, :green)}'" +
-        " data-timed-out-count='#{colour_count(lights, :timed_out)}'>" +
-      lights.count.to_s +
+    "<div class='traffic-light-count-wrapper'>" +
+      "<div class='traffic-light-count #{lights[-1].colour}'" +
+          " data-tip='traffic_light_count'" +
+          " data-red-count='#{colour_count(lights, :red)}'" +
+          " data-amber-count='#{colour_count(lights, :amber)}'" +
+          " data-green-count='#{colour_count(lights, :green)}'" +
+          " data-timed-out-count='#{colour_count(lights, :timed_out)}'>" +
+        lights.count.to_s +
+      '</div>' +
     '</div>'
   end
 
