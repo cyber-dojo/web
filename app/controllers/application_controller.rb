2d8142e7bff95351ec2d980930067534650b4046
Jon Jagger <jon@jaggersoft.com>
2019-01-17 12:29:42 +0000

    refactoring; move mapped_id to bottom of controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,30 +10,6 @@ class ApplicationController < ActionController::Base
 
   # - - - - - - - - - - - - - - - - - - - - - - - -
 
-  def mapped_id
-    # See comment below
-    if id.size == 10
-      id6 = mapper.mapped_id(id)
-      url = request.url
-      if m = /#{id}\?avatar=([a-z]*)&?/.match(url)
-        kata = groups[id6].katas.find{ |k| k.avatar_name == m[1] }
-        url6 = url.sub(m.to_s, kata.id+'?')
-      else
-        url6 = url.sub(id, id6)
-      end
-      url6 = url6.sub('was_tag', 'was_index')
-      url6 = url6.sub('now_tag', 'now_index')
-      if url6.end_with?('?')
-        url6 = url6[0..-2]
-      end
-      redirect_to url6
-    else
-      yield
-    end
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - -
-
   def groups
     Groups.new(self)
   end
@@ -50,6 +26,12 @@ class ApplicationController < ActionController::Base
 
   def kata
     @kata ||= katas[id]
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def id
+    params[:id]
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - -
@@ -76,10 +58,6 @@ class ApplicationController < ActionController::Base
     params[:index].to_i
   end
 
-  def id
-    params[:id]
-  end
-
   def files_for(index)
     kata.events[index]
         .files(:with_output)
@@ -87,24 +65,49 @@ class ApplicationController < ActionController::Base
         .to_h
   end
 
+  # - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def mapped_id
+    # See comment below
+    if id.size == 10
+      id6 = mapper.mapped_id(id)
+      url = request.url
+      if m = /#{id}\?avatar=([a-z]*)&?/.match(url)
+        kata = groups[id6].katas.find{ |k| k.avatar_name == m[1] }
+        url6 = url.sub(m.to_s, kata.id+'?')
+      else
+        url6 = url.sub(id, id6)
+      end
+      url6 = url6.sub('was_tag', 'was_index')
+      url6 = url6.sub('now_tag', 'now_index')
+      if url6.end_with?('?')
+        url6 = url6[0..-2]
+      end
+      redirect_to url6
+    else
+      yield
+    end
+  end
+
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
-# mapped()
+# mapped_id()
 # - - - - - - - - - - - - - - - - - - - - - - - -
-# cyber-dojo was originally designed for group practice-sessions.
-# At that time, an individual practice-session was just a group
+# cyber-dojo was originally designed for group practice-sessions
+# and individual practice sessions were not properly supported.
+# Viz, an individual practice-session was just a group
 # practice-session with a single avatar.
-# So originally, kata/edit/ URLs always looked like this...
+# So originally, kata/edit/ URLs _always_ looked like this...
 #    http://cyber-dojo.org/kata/edit/hVU93Kj8rq?avatar=tiger
 # There was a 10-digit id (hVU93Kj8rq) and an avatar name (tiger).
 #
 # cyber-dojo now properly supports both group practice-sessions
-# and individual practice sessions, but the URLs always look like this
+# and individual practice sessions. Now the URLs always look like this
 #    http://cyber-dojo.org/kata/edit/mFL6se
-# and use 6-digit ids (mFL6se) and never contain an avatar name.
-# If mFL6se is in a group practice-session an avatar will be visible.
-# If mFL6se is an individual practice-session an avatar won't be visible.
+# and use 6-digit ids (mFL6se) and _never_ contain an avatar name.
+# If mFL6se is in a group practice-session id an avatar will be visible.
+# If mFL6se is an individual practice-session id an avatar won't be visible.
 #
 # The job of mapped_id() is to redirect old 10-digit ids to new 6-digit ids.
 # For example
@@ -118,10 +121,10 @@ end
 # Examples of mapped_id() URL redirections...
 #
 #     dashboard/show/1F00C1BFC8
-# --> dashboard/show/2M0Ry7?
+# --> dashboard/show/2M0Ry7
 #
 #     kata/edit/1F00C1BFC8?avatar=turtle
-# --> kata/edit/2M0Ry7?
+# --> kata/edit/2M0Ry7
 #
 #     review/show/1F00C1BFC8?avatar=turtle&was_tag=2&now_tag=3
 # --> review/show/2M0Ry7?was_tag=2&now_tag=3
