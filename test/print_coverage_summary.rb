a1a6f4d9f74a0201871dceb324ae99106b3407af
Jon Jagger <jon@jaggersoft.com>
2019-01-17 12:31:12 +0000

    app-controllers coverage up to >75% now

--- test/print_coverage_summary.rb
+++ test/print_coverage_summary.rb
@@ -169,7 +169,7 @@ end
 #- - - - - - - - - - - - - - - - - - - - -
 
 def coverage(stats, name)
-  min = (name != 'app_controllers') ? 100 : 60
+  min = (name != 'app_controllers') ? 100 : 75
   percent = stats[name][:coverage]
   [ "#{name} coverage >= #{min}", percent.to_f >= min ]
 end
