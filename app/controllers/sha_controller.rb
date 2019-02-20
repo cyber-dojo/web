5e258e5bc4319bf05ef9a41979a924ca59e713d1
Jon Jagger <jon@jaggersoft.com>
2019-02-20 11:23:26 +0000

    fix sha/index runner hyperlink

--- app/controllers/sha_controller.rb
+++ app/controllers/sha_controller.rb
@@ -4,12 +4,12 @@ class ShaController < ApplicationController
   def index
     @shas = {
       'web'     => web_sha,
-      'starter' => starter.sha,
+      # 'starter' => starter.sha, # different. Hmmm....
       'saver'   => saver.sha,
       'mapper'  => mapper.sha,
-      'runner'  => runner.sha,
+      'runner-stateless' => runner.sha,
       'differ'  => differ.sha,
-      #'zipper'  => zipper.sha,
+      #'zipper'  => zipper.sha, # Offline
     }
   end
 
