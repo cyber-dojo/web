079c007342d226ab74ee3e8c830c921784ce8c6b
Jon Jagger <jon@jaggersoft.com>
2019-03-01 20:53:22 +0100

    added starter+sha to sha/index page

--- app/controllers/sha_controller.rb
+++ app/controllers/sha_controller.rb
@@ -2,14 +2,14 @@
 class ShaController < ApplicationController
 
   def index
-    @shas = {
-      'web'     => web_sha,
-      # 'starter' => starter.sha, # different. Hmmm....
-      'saver'   => saver.sha,
-      'mapper'  => mapper.sha,
-      'runner-stateless' => runner.sha,
-      'differ'  => differ.sha,
-      #'zipper'  => zipper.sha, # Offline
+    @services = {
+      'differ'  => triple('differ', differ.sha),
+      'mapper'  => triple('mapper', mapper.sha),
+      'runner'  => triple('runner', runner.sha, github_url('runner-stateless', runner.sha)),
+      'saver'   => triple('saver', saver.sha),
+      'starter' => triple('starter', starter.sha, github_url('start-points-base', starter.sha)),
+      'web'     => triple('web', web_sha),
+      # 'zipper'  => triple('zipper', zipper.sha)
     }
   end
 
@@ -17,6 +17,14 @@ class ShaController < ApplicationController
 
   def web_sha
     ENV['SHA']
+  end
+
+  def triple(name, sha, url = github_url(name, sha))
+    { 'name' => name, 'sha' => sha, 'github_url' => url }
+  end
+
+  def github_url(repo_name, sha)
+    "https://github.com/cyber-dojo/#{repo_name}/tree/#{sha}"
   end
 
 end
