846b94899dcb2f88b5fbf015ce53f7921bf1a36d
Jon Jagger <jon@jaggersoft.com>
2019-03-03 08:27:50 +0100

    add dockerhub url to sha/index page

--- app/controllers/sha_controller.rb
+++ app/controllers/sha_controller.rb
@@ -3,14 +3,14 @@ class ShaController < ApplicationController
 
   def index
     @services = {
-      'differ'  => triple('differ', differ.sha),
-      'mapper'  => triple('mapper', mapper.sha),
-      'nginx'   => triple('nginx', nginx_sha),
-      'runner'  => triple('runner', runner.sha, github_url('runner-stateless', runner.sha)),
-      'saver'   => triple('saver', saver.sha),
-      'starter' => triple('starter', starter.sha, github_url('start-points-base', starter.sha)),
-      'web'     => triple('web', web_sha),
-      # 'zipper'  => triple('zipper', zipper.sha)
+      'differ'  => info('differ', differ.sha),
+      'mapper'  => info('mapper', mapper.sha),
+      'nginx'   => info('nginx', nginx_sha),
+      'runner'  => info('runner', runner.sha, 'runner-stateless'),
+      'saver'   => info('saver', saver.sha),
+      'starter' => info('starter', starter.sha, 'start-points-base'),
+      'web'     => info('web', web_sha),
+      # 'zipper'  => info('zipper', zipper.sha)
     }
   end
 
@@ -21,15 +21,24 @@ class ShaController < ApplicationController
   end
 
   def nginx_sha
+    # This does not work in prod or beta
     `wget --quiet -O - 'http://nginx:80/sha.txt'`
   end
 
-  def triple(name, sha, url = github_url(name, sha))
-    { 'name' => name, 'sha' => sha, 'github_url' => url }
+  def info(name, sha, repo_name = name)
+    { 'name' => name,
+      'sha' => sha,
+      'github_url' => github_url(repo_name, sha),
+      'dockerhub_url' => dockerhub_url(repo_name)
+    }
   end
 
   def github_url(repo_name, sha)
     "https://github.com/cyber-dojo/#{repo_name}/tree/#{sha}"
+  end
+
+  def dockerhub_url(repo_name)
+    "https://hub.docker.com/r/cyberdojo/#{repo_name}/tags"
   end
 
 end
