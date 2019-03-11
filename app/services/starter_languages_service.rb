0919ca9510e8bafd2ec1908f019870aa41648c33
Jon Jagger <jon@jaggersoft.com>
2019-03-11 19:42:07 +0000

    add custom-exercises and custom-languages and use them to get display-names (and remember to add files)

---
+++ app/services/starter_languages_service.rb
@@ -0,0 +1,29 @@
+require_relative 'http_helper'
+
+class StarterLanguagesService
+
+  def initialize(externals)
+    @http = HttpHelper.new(externals, self, 'languages', 4527)
+  end
+
+  # - - - - - - - - - - - -
+
+  def sha
+    http.get
+  end
+
+  # - - - - - - - - - - - -
+
+  def start_points
+    http.get
+  end
+
+  def manifest(display_name)
+    http.get(display_name)
+  end
+
+  private
+
+  attr_reader :http
+
+end
