91324eb400795ea3585081f1646dc63874e34663
Jon Jagger <jon@jaggersoft.com>
2019-03-18 15:17:38 +0000

    restores app_models tests

--- test/app_models/externals_test.rb
+++ test/app_models/externals_test.rb
@@ -16,9 +16,11 @@ class ExternalsTest < AppModelsTestBase
   test '880',
   'setting an external class to the name of an existing class succeeds' do
     exists = 'ExternalDouble'
-    set_differ_class(exists)  && assert_equal(exists, differ.class.name)
-    set_runner_class(exists)  && assert_equal(exists, runner.class.name)
-    set_starter_class(exists) && assert_equal(exists, starter.class.name)
+    set_differ_class(exists) && assert_equal(exists, differ.class.name)
+    set_runner_class(exists) && assert_equal(exists, runner.class.name)
+    set_custom_class(exists) && assert_equal(exists, custom.class.name)
+    set_exercises_class(exists) && assert_equal(exists, exercises.class.name)
+    set_languages_class(exists) && assert_equal(exists, languages.class.name)
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - -
@@ -27,9 +29,11 @@ class ExternalsTest < AppModelsTestBase
   'setting an external class to the name of a non-existent class raises StandardError' do
     error = StandardError
     does_not_exist = 'DoesNotExist'
-    set_differ_class(does_not_exist)  && assert_raises(error) { differ.class  }
-    set_runner_class(does_not_exist)  && assert_raises(error) { runner.class  }
-    set_starter_class(does_not_exist) && assert_raises(error) { starter.class }
+    set_differ_class(does_not_exist) && assert_raises(error) { differ.class }
+    set_runner_class(does_not_exist) && assert_raises(error) { runner.class }
+    set_custom_class(does_not_exist) && assert_raises(error) { custom.class }
+    set_exercises_class(does_not_exist) && assert_raises(error) { exercises.class }
+    set_languages_class(does_not_exist) && assert_raises(error) { languages.class }
   end
 
 end
--- test/test_external_helpers.rb
+++ test/test_external_helpers.rb
@@ -7,25 +7,41 @@ module TestExternalHelpers # mix-in
 
   def setup
     @config = {
-      'DIFFER'  => ENV['CYBER_DOJO_DIFFER_CLASS'],
-      'RUNNER'  => ENV['CYBER_DOJO_RUNNER_CLASS'],
-      'STARTER' => ENV['CYBER_DOJO_STARTER_CLASS'],
-      'SAVER'   => ENV['CYBER_DOJO_SAVER_CLASS'],
-      'ZIPPER'  => ENV['CYBER_DOJO_ZIPPER_CLASS'],
-      'HTTP'    => ENV['CYBER_DOJO_HTTP_CLASS']
+      'CUSTOM'    => ENV['CYBER_DOJO_CUSTOM_CLASS'],
+      'EXERCISES' => ENV['CYBER_DOJO_EXERCISES_CLASS'],
+      'LANGUAGES' => ENV['CYBER_DOJO_LANGUAGES_CLASS'],
+      'DIFFER'    => ENV['CYBER_DOJO_DIFFER_CLASS'],
+      'RUNNER'    => ENV['CYBER_DOJO_RUNNER_CLASS'],
+      'SAVER'     => ENV['CYBER_DOJO_SAVER_CLASS'],
+      'ZIPPER'    => ENV['CYBER_DOJO_ZIPPER_CLASS'],
+      'HTTP'      => ENV['CYBER_DOJO_HTTP_CLASS']
     }
   end
 
   def teardown
-    ENV['CYBER_DOJO_DIFFER_CLASS']  = @config['DIFFER']
-    ENV['CYBER_DOJO_RUNNER_CLASS']  = @config['RUNNER']
-    ENV['CYBER_DOJO_STARTER_CLASS'] = @config['STARTER']
-    ENV['CYBER_DOJO_SAVER_CLASS']   = @config['SAVER']
-    ENV['CYBER_DOJO_ZIPPER_CLASS']  = @config['ZIPPER']
-    ENV['CYBER_DOJO_HTTP_CLASS']  =   @config['HTTP']
+    ENV['CYBER_DOJO_CUSTOM_CLASS']    = @config['CUSTOM']
+    ENV['CYBER_DOJO_EXERCISES_CLASS'] = @config['EXERCISES']
+    ENV['CYBER_DOJO_LANGUAGES_CLASS'] = @config['LANGUAGES']
+    ENV['CYBER_DOJO_DIFFER_CLASS']    = @config['DIFFER']
+    ENV['CYBER_DOJO_RUNNER_CLASS']    = @config['RUNNER']
+    ENV['CYBER_DOJO_SAVER_CLASS']     = @config['SAVER']
+    ENV['CYBER_DOJO_ZIPPER_CLASS']    = @config['ZIPPER']
+    ENV['CYBER_DOJO_HTTP_CLASS']      = @config['HTTP']
   end
 
   # - - - - - - - - - - - - - - - - - - -
+
+  def set_custom_class(value)
+    set_class('custom', value)
+  end
+
+  def set_exercises_class(value)
+    set_class('exercises', value)
+  end
+
+  def set_languages_class(value)
+    set_class('languages', value)
+  end
 
   def set_differ_class(value)
     set_class('differ', value)
@@ -33,10 +49,6 @@ module TestExternalHelpers # mix-in
 
   def set_runner_class(value)
     set_class('runner', value)
-  end
-
-  def set_starter_class(value)
-    set_class('starter', value)
   end
 
   def set_saver_class(value)
