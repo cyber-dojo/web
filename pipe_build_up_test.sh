0a07461f79513ebd43d809c1e4105fac9ca614d9
Jon Jagger <jon@jaggersoft.com>
2019-06-29 12:26:16 +0200

    set docker-compose's TAG environment-variables at start of pipe_build_up_test script so they are set for initial down/build steps and you don't get warnings

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -4,6 +4,19 @@ set -e
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly SH_DIR="${ROOT_DIR}/sh"
 
+docker run --rm cyberdojo/versioner:latest sh -c 'cat /app/.env' > /tmp/versioner.web.env
+set -a
+. /tmp/versioner.web.env
+set +a
+export CYBER_DOJO_LANGUAGES=cyberdojo/languages-all:d996783
+export CYBER_DOJO_DIFFER_TAG=${CYBER_DOJO_DIFFER_SHA:0:7}
+export CYBER_DOJO_MAPPER_TAG=${CYBER_DOJO_MAPPER_SHA:0:7}
+export CYBER_DOJO_RAGGER_TAG=${CYBER_DOJO_RAGGER_SHA:0:7}
+export CYBER_DOJO_RUNNER_TAG=${CYBER_DOJO_RUNNER_SHA:0:7}
+export CYBER_DOJO_SAVER_TAG=${CYBER_DOJO_SAVER_SHA:0:7}
+export CYBER_DOJO_ZIPPER_TAG=${CYBER_DOJO_ZIPPER_SHA:0:7}
+export CYBER_DOJO_VERSIONER_TAG=${CYBER_DOJO_VERSIONER_TAG:-latest}
+
 "${SH_DIR}/docker_containers_down.sh"
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
@@ -11,5 +24,5 @@ readonly SH_DIR="${ROOT_DIR}/sh"
 if "${SH_DIR}/run_tests_in_container.sh" "$@"; then
   "${SH_DIR}/docker_containers_down.sh"
 else
-  exit 1
+  exit 3
 fi
--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -74,19 +74,6 @@ wait_until_running()
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
-docker run --rm cyberdojo/versioner:latest sh -c 'cat /app/.env' > /tmp/versioner.web.env
-set -a
-. /tmp/versioner.web.env
-set +a
-export CYBER_DOJO_LANGUAGES=cyberdojo/languages-all:d996783
-export CYBER_DOJO_DIFFER_TAG=${CYBER_DOJO_DIFFER_SHA:0:7}
-export CYBER_DOJO_MAPPER_TAG=${CYBER_DOJO_MAPPER_SHA:0:7}
-export CYBER_DOJO_RAGGER_TAG=${CYBER_DOJO_RAGGER_SHA:0:7}
-export CYBER_DOJO_RUNNER_TAG=${CYBER_DOJO_RUNNER_SHA:0:7}
-export CYBER_DOJO_SAVER_TAG=${CYBER_DOJO_SAVER_SHA:0:7}
-export CYBER_DOJO_ZIPPER_TAG=${CYBER_DOJO_ZIPPER_SHA:0:7}
-export CYBER_DOJO_VERSIONER_TAG=${CYBER_DOJO_VERSIONER_TAG:-latest}
-
 echo
 docker-compose \
   --file "${ROOT_DIR}/docker-compose.yml" \
