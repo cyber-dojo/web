277658b0a3be5bb8f43dae9b0b34944d570ccbad
JonJagger <jon@jaggersoft.com>
2019-07-29 08:48:08 +0100

    reduce web's k8s memory limit to 256M

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -4,17 +4,19 @@ set -e
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 export SHA=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
 
-echo
-docker-compose \
-  --file "${ROOT_DIR}/docker-compose.yml" \
-  build
+build_service_image()
+{
+  echo
+  docker-compose \
+    --file "${ROOT_DIR}/docker-compose.yml" \
+      build \
+        "${1}"
+}
+
+build_service_image web
 
 # Assuming we do not have any new web commits, web's latest commit
 # sha will match the image tag inside versioner's .env file.
 # This means we can tag to it and a [cyber-dojo up] call
-# will use the tagged images.
-readonly SHA=$(docker run --rm cyberdojo/web:latest sh -c 'echo -n ${SHA}')
-readonly TAG=${SHA:0:7}
-docker tag cyberdojo/web:latest cyberdojo/web:${TAG}
-
-docker system prune --force > /dev/null 2>&1
+# will use the tagged image.
+docker tag cyberdojo/web:latest cyberdojo/web:${SHA:0:7}
