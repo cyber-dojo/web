ef8c25ea1e182af4bbc245bc77660b6895d60c3e
JonJagger <jon@jaggersoft.com>
2019-10-15 18:56:58 +0100

    Allow script to copy test-data into live local server for manual testing

--- sh/copy_in_saver_test_data.sh
+++ sh/copy_in_saver_test_data.sh
@@ -5,7 +5,7 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 # You cannot docker cp to a tmpfs, so tar-piping instead...
 
-readonly CONTAINER=test-web-saver
+readonly CONTAINER=${1:-test-web-saver}
 readonly SRC_PATH=${MY_DIR}/../test/data/cyber-dojo
 readonly DEST_PATH=/cyber-dojo
 
