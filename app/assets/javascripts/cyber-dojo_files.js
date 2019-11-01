9deee5b1746b33635dcd19b9a47a44a3a55583f0
JonJagger <jon@jaggersoft.com>
2019-11-01 11:31:04 +0100

    Add back in highlightSorter now cd.highlightFilenames() is available in kata/edit and review/show pages

--- app/assets/javascripts/cyber-dojo_files.js
+++ app/assets/javascripts/cyber-dojo_files.js
@@ -352,7 +352,7 @@ var cyberDojo = (function(cd, $) {
         hi.push(filename);
       }
     });
-    hi.sort();
+    hi.sort(highlightSorter);
     hi = hi.filter(filename => !cd.isOutputFile(filename));
     return hi;
   };
