a80a3b1fa1e32406a279af1838fce9cbaebf0b8c
Jon Jagger <jon@jaggersoft.com>
2020-01-15 13:07:17 +0000

    Refactoring; simplify http_json/responder

--- app/services/http_json/responder.rb
+++ app/services/http_json/responder.rb
@@ -16,8 +16,6 @@ module HttpJson
     def get(path, args)
       response = @requester.get(path, args)
       unpacked(response.body, path.to_s)
-    rescue => error
-      fail @exception_class, error.message
     end
 
     # - - - - - - - - - - - - - - - - - - - - -
@@ -25,8 +23,6 @@ module HttpJson
     def post(path, args)
       response = @requester.post(path, args)
       unpacked(response.body, path.to_s)
-    rescue => error
-      fail @exception_class, error.message
     end
 
     private
@@ -34,13 +30,13 @@ module HttpJson
     def unpacked(body, path)
       json = json_parse(body)
       unless json.is_a?(Hash)
-        fail error_msg(body, 'is not JSON Hash')
+        throw error_msg(body, 'is not JSON Hash')
       end
       if json.has_key?('exception')
-        fail JSON.pretty_generate(json['exception'])
+        throw JSON.pretty_generate(json['exception'])
       end
       unless json.has_key?(path)
-        fail error_msg(body, "has no key for '#{path}'")
+        throw error_msg(body, "has no key for '#{path}'")
       end
       json[path]
     end
@@ -50,13 +46,19 @@ module HttpJson
     def json_parse(body)
       JSON.parse(body)
     rescue JSON::ParserError
-      fail error_msg(body, 'is not JSON')
+      throw error_msg(body, 'is not JSON')
     end
 
     # - - - - - - - - - - - - - - - - - - - - -
 
     def error_msg(body, text)
       "http response.body #{text}:#{body}"
+    end
+
+    # - - - - - - - - - - - - - - - - - - - - -
+
+    def throw(message)
+      fail @exception_class, message
     end
 
   end
