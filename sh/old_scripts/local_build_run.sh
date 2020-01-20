f497257453c4b036e6165a5f07738c649efcb5e2
Jon Jagger <jon@jaggersoft.com>
2020-01-20 11:31:43 +0000

    Delete dead scripts

--- sh/old_scripts/local_build_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
-
-"${ROOT_DIR}/sh/build_docker_images.sh"
-WEB_SHA=$(printf '9%.0s' {1..40})
-docker tag cyberdojo/web:latest cyberdojo/web:${WEB_SHA:0:7}
-${ROOT_DIR}/../commander/cyber-dojo up
