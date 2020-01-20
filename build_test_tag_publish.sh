9eee42c5a38142c7c9b77a0aa5def54abfd2183a
Jon Jagger <jon@jaggersoft.com>
2020-01-20 11:37:52 +0000

    Update .gitignore for new build script name

---
+++ build_test_tag_publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -Eeu
+
+readonly SH_DIR="$( cd "$( dirname "${0}" )/sh" && pwd )"
+
+source ${SH_DIR}/versioner_env_vars.sh
+export $(versioner_env_vars)
+
+"${SH_DIR}/docker_containers_down.sh"
+"${SH_DIR}/build_docker_images.sh"
+"${SH_DIR}/tag_image.sh"
+"${SH_DIR}/docker_containers_up.sh"
+"${SH_DIR}/copy_in_saver_test_data.sh" 2> /dev/null
+"${SH_DIR}/run_tests_in_container.sh" "$@"
+"${SH_DIR}/docker_containers_down.sh"
+"${SH_DIR}/on_ci_publish_tagged_images.sh"
