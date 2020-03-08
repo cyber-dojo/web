c3f63ff234d7403662d15cc8ce35837a207eec15
Jon Jagger <jon@jaggersoft.com>
2020-03-08 08:12:50 +0000

    Render sha/index using browser-side ajax calls

--- app/controllers/sha_controller.rb
+++ app/controllers/sha_controller.rb
@@ -2,46 +2,12 @@
 class ShaController < ApplicationController
 
   def index
-    @services = {
-      'creator'                => info('creator', creator.sha),
-      'custom-chooser'         => info('custom-chooser', custom_chooser.sha),
-      'custom-start-points'    => info('custom-start-points', custom_start_points.sha),
-      'exercises-start-points' => info('exercises-start-points', exercises_start_points.sha),
-      'languages-start-points' => info('languages-start-points', languages_start_points.sha),
-      'avatars'   => info('avatars', avatars.sha),
-      'differ'    => info('differ', differ.sha),
-      'nginx'     => info('nginx', nginx_sha),
-      'runner'    => info('runner', runner.sha['sha']),
-      'saver'     => info('saver', saver.sha),
-      'web'       => info('web', web_sha),
-      # 'zipper'    => info('zipper', zipper.sha)
-    }
-  end
-
-  private
-
-  def web_sha
-    ENV['SHA']
-  end
-
-  def nginx_sha
-    '' # nginx is upstream of web, client has to fill this in
-  end
-
-  def info(name, sha, repo_name = name)
-    { 'repo_name' => repo_name,
-      'sha' => sha,
-      'github_url' => github_url(repo_name, sha),
-      'dockerhub_url' => dockerhub_url(repo_name)
-    }
-  end
-
-  def github_url(repo_name, sha)
-    "https://github.com/cyber-dojo/#{repo_name}/tree/#{sha}"
-  end
-
-  def dockerhub_url(repo_name)
-    "https://hub.docker.com/r/cyberdojo/#{repo_name}/tags"
+    @names = %w(
+      creator custom-chooser
+      custom-start-points exercises-start-points languages-start-points
+      avatar differ runner saver
+    )
+    @web_sha = ENV['SHA']
   end
 
 end
