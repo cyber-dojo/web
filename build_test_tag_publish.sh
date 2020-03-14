c26d91c5cd1176e906de63836812a774e941c0a0
Jon Jagger <jon@jaggersoft.com>
2020-03-14 17:16:21 +0000

    Add build-only option to main script

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -8,6 +8,9 @@ export $(versioner_env_vars)
 "${SH_DIR}/docker_containers_down.sh"
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/tag_image.sh"
+if [ "${1}" == '--no-test' ]; then
+  exit 0
+fi
 "${SH_DIR}/docker_containers_up.sh"
 "${SH_DIR}/copy_in_saver_test_data.sh" 2> /dev/null
 "${SH_DIR}/run_tests_in_container.sh" "$@"
