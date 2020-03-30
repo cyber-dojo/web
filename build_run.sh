8d88c0b2675f2bbe51cc2836fe14f94dfb35f75c
Jon Jagger <jon@jaggersoft.com>
2020-03-30 22:13:28 +0100

    [ci skip] Add comment to build_run.sh re it not working yet

--- build_run.sh
+++ build_run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -Eeu
 
+# Script to build a web image whose Dockerfile does *not*
+# COPY web's source; instead web's source is volume-mounted
+# into the container. This allows for a faster feedback loop
+# when working on, eg CSS.
+# However, it's not finished yet. The server starts
+# $ rails server --environment=production
+# and in production mode it does not see changed files.
+# It needs to be development|test, but either they're
+# not configured, or nginx is caching.
+
 readonly ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source ${ROOT_DIR}/sh/versioner_env_vars.sh
@@ -29,5 +39,6 @@ run()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
+echo "WARNING: This is not working yet..."
 build
 run
