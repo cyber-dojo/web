45554a5ba9640d04d25e4fcfb45f60084d580637
Jon Jagger <jon@jaggersoft.com>
2020-03-30 19:46:27 +0100

    Rework dockerfile to allow dual-build; one build to copy web source, one build to volume-mount it

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -10,7 +10,8 @@ build_images()
   echo
   docker-compose \
     --file "${ROOT_DIR}/docker-compose.yml" \
-    build
+    build \
+    --build-arg BUILD_ENV=copy    
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
