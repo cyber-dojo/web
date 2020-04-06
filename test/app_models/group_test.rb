258833691e63b70857786ae7f7e4fad00d3c73dc
Jon Jagger <jon@jaggersoft.com>
2020-04-06 08:45:59 +0100

    Refactoring; simplify group-join test

--- test/app_models/group_test.rb
+++ test/app_models/group_test.rb
@@ -129,14 +129,12 @@ class GroupTest < AppModelsTestBase
   you can join 64 times and then the group is full
   ) do
     in_new_group do |group|
-      indexes = (0..63).to_a.shuffle
       64.times do |size|
-        kata = group.join(indexes)
+        kata = group.join
         refute_nil kata
         assert_equal size+1, group.size
-        indexes.rotate!
       end
-      kata = group.join(indexes)
+      kata = group.join
       assert_nil kata
     end
   end
