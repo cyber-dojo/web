8a51fae7aeb620792ce9b403bb9d75371b2b617b
Jon Jagger <jon@jaggersoft.com>
2020-04-13 17:56:07 +0100

    Add repler to sha/index page; fix sha/index it so it shows web/sha and nginx/sha

--- app/controllers/sha_controller.rb
+++ app/controllers/sha_controller.rb
@@ -3,9 +3,9 @@ class ShaController < ApplicationController
 
   def index
     @names = %w(
-      creator custom-chooser exercises-chooser languages-chooser
+      custom-chooser exercises-chooser languages-chooser
       custom-start-points exercises-start-points languages-start-points
-      avatars differ runner saver
+      avatars creator differ repler runner saver
     )
     @web_sha = ENV['SHA']
   end
