1ab0db136f9c63d1fe90ed6718cedb4e251cf67d
Jon Jagger <jon@jaggersoft.com>
2020-04-14 14:11:26 +0100

    Refactoring; rename scripts to names used in other repos

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -5,13 +5,13 @@ readonly SH_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/sh" && pwd)"
 source ${SH_DIR}/versioner_env_vars.sh
 export $(versioner_env_vars)
 
-"${SH_DIR}/docker_containers_down.sh"
-"${SH_DIR}/build_docker_images.sh"
+"${SH_DIR}/containers_down.sh"
+"${SH_DIR}/build_images.sh"
 "${SH_DIR}/tag_image.sh"
 if [ "${1:-}" == '--no-test' ]; then
   exit 0
 fi
-"${SH_DIR}/docker_containers_up.sh"
+"${SH_DIR}/containers_up.sh"
 "${SH_DIR}/run_tests_in_container.sh" "$@"
-"${SH_DIR}/docker_containers_down.sh"
+"${SH_DIR}/containers_down.sh"
 "${SH_DIR}/on_ci_publish_tagged_images.sh"
