83173aaa2b7878e7a3a1cb91fca24b02413c1b7d
Jon Jagger <jon@jaggersoft.com>
2020-05-08 16:03:43 +0100

    Refactoring; move filenames code into editor

---
+++ app/assets/javascripts/editor.js
@@ -0,0 +1,45 @@
+/*global jQuery,cyberDojo*/
+'use strict';
+var cyberDojo = ((cd, $) => {
+
+  cd.kata.editor = new Editor();
+
+  function Editor() {
+  };
+
+  Editor.prototype.createFiles = function(files) {
+    files.forEach(file => {
+      this.createFile(file.name, file);
+    });
+  };
+
+  Editor.prototype.createFile = function(filename, file) {
+    const newFile = makeNewFile(filename, file);
+    $('#visible-files-box').append(newFile);
+    cd.switchEditorToCodeMirror(filename);
+  };
+
+  // - - - - - - - - - - - - - - - - - - - - - -
+
+  const makeNewFile = (filename, file) => {
+    const div = $('<div>', {
+        class: 'filename_div',
+           id: `${filename}_div`
+    });
+    const text = $('<textarea>', {
+      class: 'file_content',
+      name: `file_content[${filename}]`,
+      id: `file_content_for_${filename}`,
+      'spellcheck': 'false',
+      'data-filename': filename,
+      text: file['content']
+    });
+    div.append(text);
+    return div;
+  };
+
+  // - - - - - - - - - - - - - - - - - - - - - -
+
+  return cd;
+
+})(cyberDojo || {}, jQuery);
