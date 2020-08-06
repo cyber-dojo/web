34f727c03e038a5de0f99fa293f361c74385f55c
Jon Jagger <jon@jaggersoft.com>
2020-08-06 19:35:42 +0100

    Add revert diff-traffic-light helper test

--- test/app_helpers/traffic_light_test.rb
+++ test/app_helpers/traffic_light_test.rb
@@ -14,11 +14,11 @@ class TrafficLightTest < AppHelpersTestBase
   test '67C',
   'traffic_light_count' do
     lights = [
-      event(0, 'red'  , 'none'),
-      event(1, 'red'  , 'none'),
-      event(2, 'amber', 'none'),
-      event(3, 'green', 'none'),
-      event(4, 'amber', 'none'),
+      event(0, colour:'red'  , predicted:'none'),
+      event(1, colour:'red'  , predicted:'none'),
+      event(2, colour:'amber', predicted:'none'),
+      event(3, colour:'green', predicted:'none'),
+      event(4, colour:'amber', predicted:'none'),
     ]
     expected =
       "<div class='traffic-light-count-wrapper'>" +
@@ -42,27 +42,27 @@ class TrafficLightTest < AppHelpersTestBase
   prediction for red|amber|green
   but not for faulty|timed_out
   ) do
-    red = event(0, colour='red')
+    red = event(0, colour:'red')
     expected_red = "<img src='/traffic-light/image/red_predicted_none.png'" +
                    " alt='red traffic-light'/>"
     assert_equal expected_red, traffic_light_image(red)
 
-    amber = event(1, colour='amber')
+    amber = event(1, colour:'amber')
     expected_amber = "<img src='/traffic-light/image/amber_predicted_none.png'" +
                      " alt='amber traffic-light'/>"
     assert_equal expected_amber, traffic_light_image(amber)
 
-    green = event(2, colour='green')
+    green = event(2, colour:'green')
     expected_green = "<img src='/traffic-light/image/green_predicted_none.png'" +
                      " alt='green traffic-light'/>"
     assert_equal expected_green, traffic_light_image(green)
 
-    faulty = event(3, colour='faulty')
+    faulty = event(3, colour:'faulty')
     expected_faulty = "<img src='/traffic-light/image/faulty.png'" +
                       " alt='faulty traffic-light'/>"
     assert_equal expected_faulty, traffic_light_image(faulty)
 
-    timed_out = event(4, colour='timed_out')
+    timed_out = event(4, colour:'timed_out')
     expected_timed_out = "<img src='/traffic-light/image/timed_out.png'" +
                          " alt='timed_out traffic-light'/>"
     assert_equal expected_timed_out, traffic_light_image(timed_out)
@@ -74,7 +74,7 @@ class TrafficLightTest < AppHelpersTestBase
   'diff_traffic_light' do
     id = 'a4r9YN'
     kata = OpenStruct.new(id:id)
-    light = event(index=14, colour='green', predicted='none', kata)
+    light = event(index=14, colour:'green', predicted:'none', kata:kata)
     number = 15
     avatar_index = 37
     expected = '' +
@@ -83,7 +83,7 @@ class TrafficLightTest < AppHelpersTestBase
         " data-index='#{index}'" +
         " data-number='#{number}'" +
         " data-avatar-index='#{avatar_index}'" +
-        " data-colour='#{colour}'>" +
+        " data-colour='green'>" +
         "<img src='/traffic-light/image/green_predicted_none.png'" +
            " alt='green traffic-light'/>" +
       '</div>'
@@ -91,14 +91,41 @@ class TrafficLightTest < AppHelpersTestBase
     assert_equal expected, actual
   end
 
+  #- - - - - - - - - - - - - - - -
+
+  test '444',
+  'reverted diff_traffic_light' do
+    id = 'Lr5ebj'
+    kata = OpenStruct.new(id:id)
+    light = event(index=14, colour:'red', predicted:'none', kata:kata, revert:6)
+    number = 15
+    avatar_index = 37
+    expected = '' +
+      "<div class='diff-traffic-light'" +
+        " data-id='#{id}'" +
+        " data-index='#{index}'" +
+        " data-number='#{number}'" +
+        " data-avatar-index='#{avatar_index}'" +
+        " data-colour='red'>" +
+        "<img src='/traffic-light/image/red_revert.png'" +
+           " alt='red traffic-light'/>" +
+      '</div>'
+    actual = diff_traffic_light(light, avatar_index, number)
+    assert_equal expected, actual
+  end
+
   private
 
-  def event(index, colour, predicted='none', kata=nil)
-    Event.new(kata, {
+  def event(index, args)
+    summary = {
       'index' => index,
-      'colour' => colour,
-      'predicted' => predicted,
-    })
+      'colour' => args[:colour],
+      'predicted' => (args[:predicted] || 'none')
+    }
+    if args[:revert]
+      summary['revert'] = args[:revert]
+    end
+    Event.new(args[:kata], summary)
   end
 
 end
