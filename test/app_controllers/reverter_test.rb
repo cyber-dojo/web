d82b48330ba0b56e786574b3470aae1788d71015
Jon Jagger <jon@jaggersoft.com>
2020-08-07 11:53:59 +0100

    Add controller test for reverting to a different avatars traffic-light

--- test/app_controllers/reverter_test.rb
+++ test/app_controllers/reverter_test.rb
@@ -8,8 +8,9 @@ class ReverterControllerTest  < AppControllerTestBase
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '276',
-  'in individual kata, revert back to our own previous traffic-light' do
+  test '276', %w(
+  in individual kata, revert back to our own previous traffic-light
+  ) do
     in_kata {
       filename = 'hiker.rb'
       change_file(filename, old_content='the_answer')
@@ -20,11 +21,11 @@ class ReverterControllerTest  < AppControllerTestBase
       assert_equal new_content, kata.files[filename]['content']
 
       post '/reverter/revert', params: { # 3
-        'format' => 'json',
         'src_id' => kata.id,
         'src_index' => 1,
         'id'     => kata.id,
         'index'  => 3,
+        'format' => 'json'
       }
       assert_response :success
 
@@ -38,6 +39,41 @@ class ReverterControllerTest  < AppControllerTestBase
       assert_equal old_content, event.files[filename]['content']
       assert_equal [kata.id,1], event.revert
     }
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '277', %w(
+  in group kata, revert back to a different avatar's traffic-light
+  ) do
+    new_content = 'and now for something_different'
+
+    in_kata do |lion|
+      filename = 'hiker.rb'
+      change_file(filename, new_content)
+      post_run_tests # 1
+
+      in_kata do |hippo|
+        post '/reverter/revert', params: { # 1
+          'src_id' => lion.id,
+          'src_index' => 1,
+          'id'     => hippo.id,
+          'index'  => 1,
+          'format' => 'json'
+        }
+        assert_response :success
+
+        files = json['files']
+        refute_nil files
+        refute_nil files[filename]
+        assert_equal new_content, files[filename]
+
+        assert_equal 2, hippo.events.size
+        revert_event = hippo.events.last
+        assert_equal new_content, revert_event.files[filename]['content']
+        assert_equal [lion.id,1], revert_event.revert
+      end
+    end
   end
 
 end
