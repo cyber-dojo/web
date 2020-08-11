6faaf44dd0f56dd2bc5845403c449ea97374ec2f
Jon Jagger <jon@jaggersoft.com>
2020-08-11 11:37:30 +0100

    Start adding tick/cross/revert circle images before traffic-light on kata page

--- app/helpers/traffic_light_image_path_helper.rb
+++ app/helpers/traffic_light_image_path_helper.rb
@@ -3,24 +3,7 @@
 module TrafficLightImagePathHelper # mix-in
 
   def traffic_light_image_path(light)
-    "/traffic-light/image/#{light.colour}#{imgSuffix(light)}.png"
-  end
-
-  module_function
-
-  def imgSuffix(light)
-    if !rag?(light.colour)
-      ''
-    elsif light.revert
-      '_revert'
-    else
-      predicted = light.predicted || 'none'
-      "_predicted_#{predicted}"
-    end
-  end
-
-  def rag?(colour)
-    %w( red amber green ).include?(colour.to_s)
+    "/traffic-light/image/#{light.colour}.png"
   end
 
 end
