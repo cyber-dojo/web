8c3d88340bbde1540c73ce2ba9e9d58fa4645bc6
Jon Jagger <jon@jaggersoft.com>
2020-08-12 14:18:41 +0100

    Update app-helper traffic-light tests

--- test/app_helpers/traffic_light_test.rb
+++ test/app_helpers/traffic_light_test.rb
@@ -37,23 +37,46 @@ class TrafficLightTest < AppHelpersTestBase
 
   #- - - - - - - - - - - - - - - -
 
+  test '443',
+  'diff for traffic-light with no prediction and no revert' do
+    id = 'a4r9YN'
+    kata = OpenStruct.new(id:id)
+    light = event(index=14, colour:'green', predicted:'none', kata:kata)
+    number = 15
+    avatar_index = 37
+    expected = '' +
+      "<div class='diff-traffic-light'" +
+        " data-id='#{id}'" +
+        " data-index='#{index}'" +
+        " data-number='#{number}'" +
+        " data-avatar-index='#{avatar_index}'" +
+        " data-colour='green'>" +
+        "<img src='/traffic-light/image/green.png'" +
+           " alt='green traffic-light'/>" +
+      '</div>'
+    actual = diff_traffic_light(light, avatar_index, number)
+    assert_equal expected, actual
+  end
+
+  #- - - - - - - - - - - - - - - -
+=begin
   test 'E41', %w(
   traffic_light_image has
   prediction for red|amber|green
   but not for faulty|timed_out
   ) do
     red = event(0, colour:'red')
-    expected_red = "<img src='/traffic-light/image/red_predicted_none.png'" +
+    expected_red = "<img src='/traffic-light/image/red.png'" +
                    " alt='red traffic-light'/>"
     assert_equal expected_red, traffic_light_image(red)
 
     amber = event(1, colour:'amber')
-    expected_amber = "<img src='/traffic-light/image/amber_predicted_none.png'" +
+    expected_amber = "<img src='/traffic-light/image/amber.png'" +
                      " alt='amber traffic-light'/>"
     assert_equal expected_amber, traffic_light_image(amber)
 
     green = event(2, colour:'green')
-    expected_green = "<img src='/traffic-light/image/green_predicted_none.png'" +
+    expected_green = "<img src='/traffic-light/image/green.png'" +
                      " alt='green traffic-light'/>"
     assert_equal expected_green, traffic_light_image(green)
 
@@ -67,24 +90,25 @@ class TrafficLightTest < AppHelpersTestBase
                          " alt='timed_out traffic-light'/>"
     assert_equal expected_timed_out, traffic_light_image(timed_out)
   end
-
+=end
   #- - - - - - - - - - - - - - - -
 
-  test '443',
-  'diff_traffic_light' do
-    id = 'a4r9YN'
+  test '444',
+  'predicted correctly traffic-light' do
+    id = 'Lr5ebj'
     kata = OpenStruct.new(id:id)
-    light = event(index=14, colour:'green', predicted:'none', kata:kata)
+    light = event(index=14, colour:'green', predicted:'green', kata:kata)
     number = 15
     avatar_index = 37
     expected = '' +
+      '<img class="tick" src="/traffic-light/image/circle-tick.png">' +
       "<div class='diff-traffic-light'" +
         " data-id='#{id}'" +
         " data-index='#{index}'" +
         " data-number='#{number}'" +
         " data-avatar-index='#{avatar_index}'" +
         " data-colour='green'>" +
-        "<img src='/traffic-light/image/green_predicted_none.png'" +
+        "<img src='/traffic-light/image/green.png'" +
            " alt='green traffic-light'/>" +
       '</div>'
     actual = diff_traffic_light(light, avatar_index, number)
@@ -93,21 +117,46 @@ class TrafficLightTest < AppHelpersTestBase
 
   #- - - - - - - - - - - - - - - -
 
-  test '444',
-  'reverted diff_traffic_light' do
+  test '445',
+  'predicted incorrectly traffic-light' do
     id = 'Lr5ebj'
     kata = OpenStruct.new(id:id)
-    light = event(index=14, colour:'red', predicted:'none', kata:kata, revert:6)
-    number = 15
-    avatar_index = 37
+    light = event(index=14, colour:'red', predicted:'green', kata:kata)
+    number = 16
+    avatar_index = 38
     expected = '' +
+      '<img class="cross" src="/traffic-light/image/circle-cross.png">' +
       "<div class='diff-traffic-light'" +
         " data-id='#{id}'" +
         " data-index='#{index}'" +
         " data-number='#{number}'" +
         " data-avatar-index='#{avatar_index}'" +
         " data-colour='red'>" +
-        "<img src='/traffic-light/image/red_revert.png'" +
+        "<img src='/traffic-light/image/red.png'" +
+           " alt='red traffic-light'/>" +
+      '</div>'
+    actual = diff_traffic_light(light, avatar_index, number)
+    assert_equal expected, actual
+  end
+
+  #- - - - - - - - - - - - - - - -
+
+  test '446',
+  'reverted traffic-light' do
+    id = 'Lr5ebj'
+    kata = OpenStruct.new(id:id)
+    light = event(index=14, colour:'red', kata:kata, revert:[id,6])
+    number = 19
+    avatar_index = 41
+    expected = '' +
+      '<img class="revert" src="/traffic-light/image/circle-revert.png">' +
+      "<div class='diff-traffic-light'" +
+        " data-id='#{id}'" +
+        " data-index='#{index}'" +
+        " data-number='#{number}'" +
+        " data-avatar-index='#{avatar_index}'" +
+        " data-colour='red'>" +
+        "<img src='/traffic-light/image/red.png'" +
            " alt='red traffic-light'/>" +
       '</div>'
     actual = diff_traffic_light(light, avatar_index, number)
