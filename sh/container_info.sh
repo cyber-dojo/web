c79cbc0430476ba258addbeecd3414f76178e7ac
Jon Jagger <jon@jaggersoft.com>
2020-08-19 13:17:40 +0100

    Refactoring; remove custom-chooser service (now part of creator service)

--- sh/container_info.sh
+++ sh/container_info.sh
@@ -31,15 +31,14 @@ service_port()
 : <<'COMMENT'
 
 $ name_port_ls
-cyber-dojo_custom-chooser_1 0.0.0.0:4536->4536/tcp
 cyber-dojo_creator_1 0.0.0.0:4523->4523/tcp
 cyber-dojo_saver_1 0.0.0.0:4537->4537/tcp
 cyber-dojo_languages-start-points_1 0.0.0.0:4524->4524/tcp
 cyber-dojo_custom-start-points_1 0.0.0.0:4526->4526/tcp
 cyber-dojo_exercises-start-points_1 0.0.0.0:4525->4525/tcp
 
-1.To get the name of the container running a port 4536
-  $ name_port_ls | grep 4536 | cut -f 1 -d " "
+1.To get the name of the container running a port 4523
+  $ name_port_ls | grep 4523 | cut -f 1 -d " "
 
 2.To get port for container called xxx
   $ name_port_ls | grep xxx | cut -f 2 -d '>' | cut -f 1 -d '/'
