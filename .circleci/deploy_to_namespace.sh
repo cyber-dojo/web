43e8aa2c6b3dfa057ebcca30b818e3b9ae448690
Jon Jagger <jon@jaggersoft.com>
2020-10-14 14:54:30 +0100

    Helm error suggests yml for live/ready probes has new httpGet. name

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -10,6 +10,37 @@ export $(curl "${VERSIONER_URL}/app/.env")
 readonly CYBER_DOJO_WEB_TAG="${CIRCLE_SHA1:0:7}"
 readonly YAML_VALUES_FILE="${MY_DIR}/k8s-general-values.yml"
 
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+modified_helm_upgrade_probe_yes_prometheus_no()
+{
+  local -r namespace="${1}"
+  local -r repo="${2}"
+  local -r image="${3}"
+  local -r tag="${4}"
+  local -r port="${5}"
+  local -r general_values="${6}"
+  if [ -z "${7:-}" ]; then
+    local -r specific_values=""
+  else
+    local -r specific_values="--values ${7}"
+  fi
+
+  helm upgrade \
+    --install \
+    --namespace=${namespace} \
+    --set-string containers[0].image=${image} \
+    --set-string containers[0].tag=${tag} \
+    --set service.port=${port} \
+    --set containers[0].livenessProbe.httpGet.port=${port} \
+    --set containers[0].readinessProbe.httpGet.port=${port} \
+    --values ${general_values} \
+    ${specific_values} \
+    ${namespace}-${repo} \
+    ${HELM_CHART_REPO} \
+    --version ${HELM_CHART_VERSION}
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 gcloud_init
 helm_init
 
@@ -18,7 +49,7 @@ echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 cat "${YAML_VALUES_FILE}"
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
-helm_upgrade_probe_yes_prometheus_no \
+modified_helm_upgrade_probe_yes_prometheus_no \
    "${NAMESPACE}" \
    "web" \
    "${CYBER_DOJO_WEB_IMAGE}" \
