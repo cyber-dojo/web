44662086eec9bdb9b6b5c39467fab1aba651bb0f
Jon Jagger <jon@jaggersoft.com>
2020-10-21 08:02:16 +0100

    Switch all traffic-light tips to use differ directly

--- app/assets/javascripts/cyber-dojo_hover_tips.js
+++ app/assets/javascripts/cyber-dojo_hover_tips.js
@@ -2,7 +2,70 @@
 'use strict';
 var cyberDojo = (function(cd, $) {
 
-  cd.setupTrafficLightTip = ($light, version, id, avatarIndex, wasIndex, nowIndex, number) => {
+  cd.setupTrafficLightTip = ($light, kataId, wasIndex, nowIndex) => {
+    cd.setTip($light, () => {
+      const args = { id:kataId, was_index:wasIndex, now_index:nowIndex };
+      $.post('/differ/diff_summary', JSON.stringify(args), (data) => {
+        cd.showHoverTip($light, tipHtml($light, data.diff_summary));
+      });
+    });
+  };
+
+  // - - - - - - - - - - - - - - - - - - - -
+
+  const tipHtml = ($light, diffSummary) => {
+    const avatarIndex = $light.data('avatar-index');
+    const colour = $light.data('colour');
+    const number = $light.data('number');
+    return `<table>
+             <tr>
+               <td>${avatarImage(avatarIndex)}</td>
+               <td><span class="traffic-light-count ${colour}">${number}</span></td>
+               <td><img src="/images/traffic-light/${colour}.png"
+                      class="traffic-light-diff-tip-traffic-light-image"></td>
+             </tr>
+           </table>
+           ${diffLinesHtmlTable(diffSummary)}`;    
+  };
+
+  // - - - - - - - - - - - - - - - - - - - -
+
+  const avatarImage = (avatarIndex) => {
+    if (avatarIndex === '') {
+      return '';
+    } else {
+      return `<img src="/images/avatars/${avatarIndex}.jpg"
+                 class="traffic-light-diff-tip-avatar-image">`;
+    }
+  };
+
+  // - - - - - - - - - - - - - - - - - - - -
+
+  const diffLinesHtmlTable = (files) => {
+    const chunks = $('<table>');
+    Object.keys(files).forEach(function(filename) {
+      chunks.append(
+        `<tr>
+          <td>
+            <div class="traffic-light-diff-tip-line-count-deleted some button">
+              ${files[filename].deleted}
+            </div>
+          </td>
+          <td>
+            <div class="traffic-light-diff-tip-line-count-added some button">
+              ${files[filename].added}
+            </div>
+          </td>
+          <td>&nbsp;${filename}</td>
+        </tr>`
+      ); //append
+    }); // forEach
+    return chunks.get(0).outerHTML;
+  };
+
+  // - - - - - - - - - - - - - - - - - - - -
+
+  cd.OLD_setupTrafficLightTip = ($light, version, id, avatarIndex, wasIndex, nowIndex, number) => {
     const args = {
            version:version,
                 id:id,
