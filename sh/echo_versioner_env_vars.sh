37fdf7faf6046c6c44b4a14b852f6baa2a315396
Jon Jagger <jon@jaggersoft.com>
2020-10-29 12:16:18 +0000

    Deleted temporary image tags used for local intergration testing

--- sh/echo_versioner_env_vars.sh
+++ sh/echo_versioner_env_vars.sh
@@ -6,12 +6,6 @@ echo_versioner_env_vars()
   docker run --rm cyberdojo/versioner:latest
   echo CYBER_DOJO_WEB_SHA="$(image_sha)"
   echo CYBER_DOJO_WEB_TAG="$(image_tag)"
-
-  echo CYBER_DOJO_NGINX_SHA=a3aa9ac5bef66c71f6b94a168097f21a7ae5d038
-  echo CYBER_DOJO_NGINX_TAG=a3aa9ac
-  
-  echo CYBER_DOJO_DIFFER_SHA=69077bdac11c854425bf267ce4527e98042fd3ac
-  echo CYBER_DOJO_DIFFER_TAG=69077bd
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
