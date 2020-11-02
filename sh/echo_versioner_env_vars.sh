fe8240478a84382c48aa355e425807523166037f
Jon Jagger <jon@jaggersoft.com>
2020-11-02 20:12:08 +0000

    Set local differ env-vars for testing

--- sh/echo_versioner_env_vars.sh
+++ sh/echo_versioner_env_vars.sh
@@ -7,9 +7,9 @@ echo_versioner_env_vars()
   echo CYBER_DOJO_WEB_SHA="$(image_sha)"
   echo CYBER_DOJO_WEB_TAG="$(image_tag)"
 
-  echo CYBER_DOJO_DIFFER_SHA=52a2cab52bbbe5f418d066aa6e7c32ced1cf65e2
-  echo CYBER_DOJO_DIFFER_TAG=52a2cab
-    
+  echo CYBER_DOJO_DIFFER_SHA=cddbc35b93e5effb05b0d45014ec7973c714d9af
+  echo CYBER_DOJO_DIFFER_TAG=cddbc35
+
   echo CYBER_DOJO_NGINX_SHA=a3aa9ac5bef66c71f6b94a168097f21a7ae5d038
   echo CYBER_DOJO_NGINX_TAG=a3aa9ac
 }
