fbc829a83a9f6354195fda0be43ef3080de18327
Jon Jagger <jon@jaggersoft.com>
2020-11-03 08:34:04 +0000

    Added new tests for new diff_view() function

--- test/app_lib/diff_view_test.rb
+++ test/app_lib/diff_view_test.rb
@@ -17,8 +17,124 @@ class DiffViewTest < AppLibTestBase
 
   # - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '292',
-  'simple example going from red to green' do
+  test '291', %w(
+  simple example going from red to green
+  view2 which will replace view1
+  ) do
+    diffs =
+    [
+      {
+        'type' => 'changed',
+        'filename' => 'hiker.rb',
+        'old_filename' => 'hiker.rb',
+        'new_filename' => 'hiker.rb',
+        'line_counts' => {
+          'added' => 1,
+          'deleted' => 1,
+          'same' => 3
+        },
+        'lines' => [
+          { 'line' => '',           'type' => 'same',    'number' => 1 },
+          { 'line' => 'def answer', 'type' => 'same',    'number' => 2 },
+          { 'type' => 'section', 'index' => 0 },
+          { 'line' => '  6 * 9',    'type' => 'deleted', 'number' => 3 },
+          { 'line' => '  6 * 7',    'type' => 'added',   'number' => 3 },
+          { 'line' => 'end',        'type' => 'same',    'number' => 4 },
+        ]
+      }
+    ]
+    view = diff_view2(diffs)
+
+    expected_view =
+    [
+      {
+        :id => "id_0",
+        :type => "changed",
+        :filename => "hiker.rb",
+        :old_filename => "hiker.rb",
+        :new_filename => "hiker.rb",
+        :section_count => 1,
+        :deleted_line_count => 1,
+        :added_line_count => 1,
+        :same_line_count => 3,
+        :content =>
+          "<same>&nbsp;</same>" +
+          "<same>def answer</same>" +
+          "<span id='id_0_section_0'></span>" +
+          "<deleted>  6 * 9</deleted>" +
+          "<added>  6 * 7</added>" +
+          "<same>end</same>",
+        :line_numbers =>
+          "<same><ln>1</ln></same>" +
+          "<same><ln>2</ln></same>" +
+          "<deleted><ln>3</ln></deleted>" +
+          "<added><ln>3</ln></added>" +
+          "<same><ln>4</ln></same>"
+      }
+    ]
+    assert_equal expected_view, view
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '292', %w(
+  simple example of deleted file
+  view2 which will replace view1
+  ) do
+    diffs =
+    [
+      {
+        'type' => 'deleted',
+        'filename' => 'hiker.rb',
+        'old_filename' => 'hiker.rb',
+        'new_filename' => nil,
+        'line_counts' => {
+          'added' => 0,
+          'deleted' => 3,
+          'same' => 0
+        },
+        'lines' => [
+          { 'type' => 'section', 'index' => 0 },
+          { 'line' => 'def answer', 'type' => 'deleted', 'number' => 1 },
+          { 'line' => '  6 * 7',    'type' => 'deleted', 'number' => 2 },
+          { 'line' => 'end',        'type' => 'deleted', 'number' => 3 },
+        ]
+      }
+    ]
+    view = diff_view2(diffs)
+
+    expected_view =
+    [
+      {
+        :id => "id_0",
+        :type => "deleted",
+        :filename => "hiker.rb",
+        :old_filename => "hiker.rb",
+        :new_filename => nil,
+        :section_count => 1,
+        :deleted_line_count => 3,
+        :added_line_count => 0,
+        :same_line_count => 0,
+        :content =>
+          "<span id='id_0_section_0'></span>" +
+          "<deleted>def answer</deleted>" +
+          "<deleted>  6 * 7</deleted>" +
+          "<deleted>end</deleted>",
+        :line_numbers =>
+          "<deleted><ln>1</ln></deleted>" +
+          "<deleted><ln>2</ln></deleted>" +
+          "<deleted><ln>3</ln></deleted>"
+      }
+    ]
+    assert_equal expected_view, view
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '293', %w(
+  simple example going from red to green
+  view1 which is deprecated to view2
+  ) do
     diffs =
     {
       'hiker.rb' =>
@@ -31,7 +147,7 @@ class DiffViewTest < AppLibTestBase
         { 'line' => 'end',        'type' => 'same',    'number' => 4 },
       ]
     }
-    view = diff_view(diffs)
+    view = diff_view1(diffs)
 
     expected_view =
     [
