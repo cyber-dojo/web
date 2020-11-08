6b973041cf49008bec0360067ce316ae3c40de60
Jon Jagger <jon@jaggersoft.com>
2020-11-08 06:17:23 +0000

    Removed env-vars set for integration testing.

--- sh/echo_versioner_env_vars.sh
+++ sh/echo_versioner_env_vars.sh
@@ -6,9 +6,6 @@ echo_versioner_env_vars()
   docker run --rm cyberdojo/versioner:latest
   echo CYBER_DOJO_WEB_SHA="$(image_sha)"
   echo CYBER_DOJO_WEB_TAG="$(image_tag)"
-
-  #echo CYBER_DOJO_DIFFER_SHA=66df74ba1d9a0684de5caea523b2e8a6295bd027
-  #echo CYBER_DOJO_DIFFER_TAG=66df74b
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
