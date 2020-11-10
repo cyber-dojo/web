538adc22993eb62ba15df593d906ad7b73f7a99a
Jon Jagger <jon@jaggersoft.com>
2020-11-10 13:27:41 +0000

    Set env-vars used for local integration testing.

--- sh/echo_versioner_env_vars.sh
+++ sh/echo_versioner_env_vars.sh
@@ -6,6 +6,12 @@ echo_versioner_env_vars()
   docker run --rm cyberdojo/versioner:latest
   echo CYBER_DOJO_WEB_SHA="$(image_sha)"
   echo CYBER_DOJO_WEB_TAG="$(image_tag)"
+
+  echo CYBER_DOJO_NGINX_SHA=279fa4b86e4ba744af5cb3064c2597c20bdeedf6
+  echo CYBER_DOJO_NGINX_TAG=279fa4b
+
+  echo CYBER_DOJO_MODEL_SHA=9b7d3118482160458ed18f2da5eeb0e2006345d6
+  echo CYBER_DOJO_MODEL_TAG=9b7d311
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
