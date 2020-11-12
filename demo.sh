d40acfeb347ee8010414d9929d1b71df2ba89807
Jon Jagger <jon@jaggersoft.com>
2020-11-12 11:04:17 +0000

    [ci skip] Added docker/docker-compose installed checks to demo.sh

--- demo.sh
+++ demo.sh
@@ -9,6 +9,25 @@ source "${ROOT_DIR}/sh/copy_in_saver_test_data.sh"
 source "${ROOT_DIR}/sh/ip_address.sh"
 export $(echo_versioner_env_vars)
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+exit_non_zero_unless_installed()
+{
+  if ! installed "${1}" ; then
+    stderr 'ERROR: ${1} is not installed!'
+    exit 42
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+installed()
+{
+  if hash "${1}" 2> /dev/null; then
+    true
+  else
+    false
+  fi
+}
+
 # - - - - - - - - - - - - - - - - - - - - - - -
 remove()
 {
@@ -46,10 +65,12 @@ up_nginx()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
-remove 3000 #web
+exit_non_zero_unless_installed docker
+exit_non_zero_unless_installed docker-compose
+remove 3000 # web
 web_build
 remove 80 # nginx
 up_nginx
-copy_in_saver_test_data
+copy_in_saver_test_data # eg 5U2J18
 sleep 2
 open "http://$(ip_address):80/kata/edit/5U2J18"
