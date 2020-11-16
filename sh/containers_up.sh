8854783b3cc50ebaa4c269a644977a67fdc20856
Jon Jagger <jon@jaggersoft.com>
2020-11-16 13:20:04 +0000

    Removed differ from docker-compose yml and build script.

--- sh/containers_up.sh
+++ sh/containers_up.sh
@@ -34,7 +34,6 @@ containers_up()
 
   wait_until_ready model     4528
   wait_until_ready runner    4597
-  wait_until_ready differ    4567
   wait_until_ready saver     4537
 
   wait_until_running test_web
