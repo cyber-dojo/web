792acae007423ee54b9d07e2e2c1fbeb5b412b38
Jon Jagger <jon@jaggersoft.com>
2020-11-17 08:54:47 +0000

    Updated build script to copy saver-test data cleanly.

--- sh/copy_in_saver_test_data.sh
+++ sh/copy_in_saver_test_data.sh
@@ -1,13 +1,23 @@
-#!/bin/bash -Eeu
 
 # - - - - - - - - - - - - - - - - - - -
 copy_in_saver_test_data()
 {
+  local -r SAVER_CID="$(saver_cid)"
   local -r SRC_PATH=${ROOT_DIR}/test/data/cyber-dojo
-  local -r SAVER_CID=$(docker ps --filter status=running --format '{{.Names}}' | grep "saver")
   local -r DEST_PATH=/cyber-dojo
+  # Empty the /cyber-dojo dir ready for tar-pipe
+  docker exec "${SAVER_CID}" bash -c 'rm -rf /cyber-dojo/groups/*'
+  docker exec "${SAVER_CID}" bash -c 'rm -rf /cyber-dojo/katas/*'
   # You cannot docker cp to a tmpfs, so tar-piping instead...
   cd ${SRC_PATH} \
     && tar -c . \
     | docker exec -i ${SAVER_CID} tar x -C ${DEST_PATH}
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
+saver_cid()
+{
+  # locally, the container name is model_saver_1
+  # on CI the contains name is, eg, project_saver_1_1bebf84ac62f
+  docker ps --filter status=running --format '{{.Names}}' | grep "saver"
 }
