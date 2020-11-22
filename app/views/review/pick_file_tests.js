e985fc45de73ee9c3a28d207f988c76dc8bf39f8
Jon Jagger <jon@jaggersoft.com>
2020-11-22 16:54:10 +0000

    Updated review file-picker to prefer even empty created/delete non .txt files to changed .txt files.

--- app/views/review/pick_file_tests.js
+++ app/views/review/pick_file_tests.js
@@ -3,80 +3,115 @@
 const pickFile = require('./pick_file');
 
 describe('pickFile cascading rules...', () => {
-  it('Rule 1: pick the current filename when it has at least one diff', () =>
+  it('Rule 1: pick previous-filename with diff', () =>
   {
-    const currentFilename = 'readme.txt';
+    const previousFilename = 'readme.txt';
     const diffs = [];
     diffs.push(cyber_dojo_sh(0));
-    diffs.push(  changed(1, 'readme.txt',  3, 7));
+    diffs.push(  changed(1, 'readme.txt',  3, 7)); // <-----
     diffs.push(  changed(2, 'hiker.c'   , 13,27));
-    const picked = pickFile(diffs, currentFilename);
+    const picked = pickFile(diffs, previousFilename);
     expect(picked.id).toEqual(1);
   });
   // otherwise
-  it('Rule 2: pick the largest diff of non .txt file', () =>
+  it('Rule 2: pick largest non .txt, with diff', () =>
   {
-    const currentFilename = 'hiker.h';
+    const previousFilename = 'hiker.h';
     const diffs = [];
     diffs.push(cyber_dojo_sh(0));
     diffs.push(unchanged(1, 'hiker.h', 321));
     diffs.push(  changed(2, 'readme.txt', 34, 18));
-    diffs.push(  changed(3, 'data.json',  4, 9));
+    diffs.push(  changed(3, 'data.json',  4, 9)); // <-----
     diffs.push(  changed(4, 'hiker.test.c',  3, 9));
-    const picked = pickFile(diffs, currentFilename);
+    const picked = pickFile(diffs, previousFilename);
     expect(picked.id).toEqual(3);
   });
-
   // otherwise
-  it('Rule 3: pick the largest diff of .txt file', () =>
-  {
-    const currentFilename = 'hiker.h';
+  it('Rule 3: pick largest non .txt, identical rename', () => {
+    const previousFilename = 'hiker.h';
+    const diffs = [];
+    diffs.push(cyber_dojo_sh(0));
+    diffs.push(unchanged(1, 'hiker.h', 52));
+    diffs.push(  created(2, 'readme.txt', 683));
+    diffs.push(  renamed(3, 'makefile', 0, 0, 7));
+    diffs.push(  renamed(4, 'Makefile', 0, 0, 8)); // <-----
+    const picked = pickFile(diffs, previousFilename);
+    expect(picked.id).toEqual(4);
+  });
+  // otherwise
+  it('Rule 4: pick any diff of non .txt, empty, created file', () => {
+    const previousFilename = 'hiker.h';
     const diffs = [];
     diffs.push(cyber_dojo_sh(0));
     diffs.push(unchanged(1, 'hiker.h', 321));
-    diffs.push(  changed(2, 'readme.txt', 4, 8));
-    diffs.push(unchanged(3, 'data.json',  21));
-    diffs.push(unchanged(4, 'hiker.test.c', 56));
-    const picked = pickFile(diffs, currentFilename);
+    diffs.push(  created(2, 'hiker.c', 0)); // <-----
+    diffs.push(unchanged(3, 'data.json',  4, 9));
+    diffs.push(  changed(4, 'readme.txt', 3, 9));
+    const picked = pickFile(diffs, previousFilename);
     expect(picked.id).toEqual(2);
   });
-
   // otherwise
-  it('Rule 4: pick the largest 100% identical rename', () =>
+  it('Rule 5: pick any diff of non .txt, empty, deleted file', () => {
+    const previousFilename = 'hiker.h';
+    const diffs = [];
+    diffs.push(cyber_dojo_sh(0));
+    diffs.push(unchanged(1, 'hiker.h', 321));
+    diffs.push(  deleted(2, 'hiker.c', 0)); // <-----
+    diffs.push(unchanged(3, 'data.json',  4, 9));
+    diffs.push(  changed(4, 'readme.txt', 3, 9));
+    const picked = pickFile(diffs, previousFilename);
+    expect(picked.id).toEqual(2);
+  });
+  // otherwise
+  it('Rule 6: pick largest, .txt file diff', () =>
   {
-    const currentFilename = 'hiker.h';
+    const previousFilename = 'hiker.h';
+    const diffs = [];
+    diffs.push(cyber_dojo_sh(0));
+    diffs.push(unchanged(1, 'hiker.h', 321));
+    diffs.push(  created(2, 'readme.txt', 8));
+    diffs.push(  created(3, 'info.txt', 9)); // <-----
+    diffs.push(unchanged(4, 'data.json',  21));
+    diffs.push(unchanged(5, 'hiker.test.c', 56));
+    const picked = pickFile(diffs, previousFilename);
+    expect(picked.id).toEqual(3);
+  });
+  // otherwise
+  it('Rule 7: pick largest .txt file 100% identical rename', () =>
+  {
+    const previousFilename = 'hiker.h';
     const diffs = [];
     diffs.push(cyber_dojo_sh(0));
     diffs.push(unchanged(1, 'hiker.h', 52));
     diffs.push(unchanged(2, 'hiker.c', 683));
-    diffs.push(  renamed(3, 'makefile', 0, 0, 7));
-    diffs.push(  renamed(4, 'Makefile', 0, 0, 8));
-    const picked = pickFile(diffs, currentFilename);
+    diffs.push(  renamed(3, 'readme.txt', 0, 0, 7));
+    diffs.push(  renamed(4, 'info.txt', 0, 0, 8)); // <-----
+    const picked = pickFile(diffs, previousFilename);
     expect(picked.id).toEqual(4);
   });
   // otherwise
-  it('Rule 5: pick the current filename', () =>
+  it('Rule 8: pick the previous-filename', () =>
   {
-    const currentFilename = 'hiker.h';
+    const previousFilename = 'hiker.h';
     const diffs = [];
     diffs.push(cyber_dojo_sh(0));
     diffs.push(unchanged(1, 'hiker.c', 34));
-    diffs.push(unchanged(2, 'hiker.h', 44 ));
+    diffs.push(unchanged(2, 'hiker.h', 44 )); // <-----
     diffs.push(unchanged(3, 'makefile', 66));
     diffs.push(unchanged(4, 'Makefile', 12));
-    const picked = pickFile(diffs, currentFilename);
+    const picked = pickFile(diffs, previousFilename);
     expect(picked.id).toEqual(2);
   });
   // otherwise
-  it('Rule 6: pick cyber-dojo.sh (which can never be deleted)', () =>
+  it('Rule 9: pick cyber-dojo.sh (which can never be deleted)', () =>
   {
-    const currentFilename = 'hiker.h';
+    const previousFilename = 'hiker.h';
     const diffs = [];
-    diffs.push(cyber_dojo_sh(0));
+    diffs.push(cyber_dojo_sh(0)); // <-----
     diffs.push(unchanged(1, 'hiker.c', 23));
     diffs.push(unchanged(2, 'makefile', 55));
     diffs.push(unchanged(3, 'hiker.tests.c', 78));
-    const picked = pickFile(diffs, currentFilename);
+    const picked = pickFile(diffs, previousFilename);
     expect(picked.id).toEqual(0);
   });
 });
