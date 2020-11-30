8b763642d388ee835a70433e21465802b500b753
Jon Jagger <jon@jaggersoft.com>
2020-11-30 12:10:23 +0000

    Build script tidying.

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -3,17 +3,20 @@
 export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export SH_DIR="${ROOT_DIR}/sh"
 
-source "${SH_DIR}/containers_down.sh"
 source "${SH_DIR}/build_tagged_images.sh"
-source "${SH_DIR}/exit_zero_if_build_only.sh"
-source "${SH_DIR}/setup_dependent_images.sh"
+source "${SH_DIR}/containers_down.sh"
 source "${SH_DIR}/containers_up.sh"
-source "${SH_DIR}/run_tests_in_container.sh"
+source "${SH_DIR}/exit_zero_if_build_only.sh"
 source "${SH_DIR}/on_ci_publish_tagged_images.sh"
+source "${SH_DIR}/setup_dependent_images.sh"
+source "${SH_DIR}/remove_old_images.sh"
+source "${SH_DIR}/run_tests_in_container.sh"
+
 source "${SH_DIR}/echo_versioner_env_vars.sh"
 export $(echo_versioner_env_vars)
 
 containers_down
+remove_old_images
 build_tagged_images
 exit_zero_if_build_only
 #setup_dependent_images
---
+++ sh/remove_old_images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -Eeu
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+remove_old_images()
+{
+  local -r dil=$(docker image ls --format "{{.Repository}}:{{.Tag}}")
+  remove_all_web_images_except_latest "${dil}" "${CYBER_DOJO_WEB_IMAGE}"
+}
+
+# - - - - - - - - - - - - - - - - - - - - - -
+remove_all_web_images_except_latest()
+{
+  local -r docker_image_ls="${1}"
+  local -r name="${2}"
+  for image_name in `echo "${docker_image_ls}" | grep "${name}:"`
+  do
+    if [ "${image_name}" != "${name}:latest" ]; then
+      if [ "${image_name}" != "${name}:<none>" ]; then
+        docker image rm "${image_name}"
+      fi
+    fi
+  done
+}
