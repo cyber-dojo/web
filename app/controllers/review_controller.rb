b9766717bd4375f0c4855879878e34b13f93b83f
Jon Jagger <jon@jaggersoft.com>
2020-12-01 12:08:32 +0000

    Removed dead code.

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -2,22 +2,14 @@
 class ReviewController < ApplicationController
 
   def show
-    # Once you are on the review dialog/page
-    # all interaction with the web server is via the
-    # differ-controller.
     set_bar_info
-    @version = kata.schema.version
     if kata.group.id.nil?
       @title = "review:#{kata.id}"
     else
       @title = "review:#{kata.group.id}"
     end
-    @avatar_index = kata.avatar_index
     @was_index = was_index
     @now_index = now_index
-    @filename = params['filename']
-    @filename_extension = kata.manifest.filename_extension
-    @highlight_filenames = kata.manifest.highlight_filenames
   end
 
   private
