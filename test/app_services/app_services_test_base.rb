5a6a7283a294ad23a7e541de30593664a4ff6b73
Jon Jagger <jon@jaggersoft.com>
2020-12-05 09:06:09 +0000

    Removed dead test code.

--- test/app_services/app_services_test_base.rb
+++ test/app_services/app_services_test_base.rb
@@ -2,58 +2,6 @@ require_relative '../all'
 
 class AppServicesTestBase < TestBase
 
-  def creation_time
-    [ 2016,12,5, 17,44,23 ]
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  def tag0
-    {
-      'event'  => 'created',
-      'time'   => creation_time,
-      'index'  => 0
-    }
-  end
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  def assert_sha(sha)
-    assert_equal(40, sha.size)
-    sha.chars.all? { |ch| assert is_hex?(ch), ch }
-  end
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  def is_hex?(ch)
-    '0123456789abcdef'.include?(ch)
-  end
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  def assert_starts_with(visible_files, filename, content)
-    actual = visible_files[filename]['content']
-    diagnostic = [
-      "filename:#{filename}",
-      "expected:#{content}:",
-      "--actual:#{actual.split[0]}:"
-    ].join("\n")
-    assert actual.start_with?(content), diagnostic
-  end
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  def assert_includes(visible_files, filename, content)
-    actual = visible_files[filename]['content']
-    diagnostic = [
-      "filename:#{filename}",
-      "!include:#{content}:"
-    ].join("\n")
-    assert actual.include?(content), diagnostic
-  end
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - -
-
   def set_http(klass)
     @http = klass
   end
