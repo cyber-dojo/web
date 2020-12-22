4ab5669fb1e6993e26b07b0c4a006b9659d7bed3
Jon Jagger <jon@jaggersoft.com>
2020-12-22 16:45:22 +0000

    Don't switch to review mode on revert; add more detail to revert hover-tip.

--- sh/echo_versioner_env_vars.sh
+++ sh/echo_versioner_env_vars.sh
@@ -6,9 +6,6 @@ echo_versioner_env_vars()
   docker run --rm cyberdojo/versioner:latest
   echo CYBER_DOJO_WEB_SHA="$(image_sha)"
   echo CYBER_DOJO_WEB_TAG="$(image_tag)"
-
-  echo CYBER_DOJO_MODEL_SHA=54d7008e64c41a84a3c55688df07bbb6642d78cc
-  echo CYBER_DOJO_MODEL_TAG=54d7008
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
