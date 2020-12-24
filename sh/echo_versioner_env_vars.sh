b1cb888efef7cd982bfe4816bb0f18e2a05d8686
Jon Jagger <jon@jaggersoft.com>
2020-12-24 11:20:02 +0000

    Restore two lines accidentally delted from bash script.

--- sh/echo_versioner_env_vars.sh
+++ sh/echo_versioner_env_vars.sh
@@ -4,6 +4,9 @@
 echo_versioner_env_vars()
 {
   docker run --rm cyberdojo/versioner:latest
+  #
+  echo CYBER_DOJO_WEB_SHA="$(image_sha)"
+  echo CYBER_DOJO_WEB_TAG="$(image_tag)"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
