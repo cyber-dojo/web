7cff2f0510692b1ea7b7e353de8620ef30aaf029
Jon Jagger <jon@jaggersoft.com>
2021-01-04 10:11:55 +0000

    Added env-vars to k8s yml file rather than concatentating them in the deployment script.

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -Eeu
 
 readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 readonly NAMESPACE="${1}" # beta|prod
 readonly K8S_URL=https://raw.githubusercontent.com/cyber-dojo/k8s-install/master
 readonly VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
@@ -10,14 +9,8 @@ export $(curl "${VERSIONER_URL}/app/.env")
 readonly CYBER_DOJO_WEB_TAG="${CIRCLE_SHA1:0:7}"
 readonly YAML_VALUES_FILE="${MY_DIR}/k8s-general-values.yml"
 
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 gcloud_init
 helm_init
-
-cat "${MY_DIR}/env-var-values.yml" >> "${YAML_VALUES_FILE}"
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-cat "${YAML_VALUES_FILE}"
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 helm_upgrade_probe_yes_prometheus_no \
    "${NAMESPACE}" \
