438d94905afa35742f69d30f1e997f0e7828a665
Jon Jagger <jon@jaggersoft.com>
2021-06-20 12:03:23 +0100

    Delete dead comment

--- sh/echo_versioner_env_vars.sh
+++ sh/echo_versioner_env_vars.sh
@@ -4,9 +4,6 @@
 echo_versioner_env_vars()
 {
   docker run --rm cyberdojo/versioner:latest
-  #
-  #echo CYBER_DOJO_SAVER_SHA=29f0936e9a44deaa58526f06ddd7924310cbd3dc
-  #echo CYBER_DOJO_SAVER_TAG=29f0936
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
