5e18f5ecef196f838071fe13a0a53ffbc9dd3a86
Jon Jagger <jon@jaggersoft.com>
2022-08-13 16:01:07 +0100

    Put 404/500 error-handling routes into same file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,16 @@ class ApplicationController < ActionController::Base
     rescue_from Exception, with: :render_500
   end
 
+  def render_404
+    respond_to do |format|
+      format.html { render template: 'error/404', layout: 'layouts/error', status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
+  end
+
   def render_500(exception)
     respond_to do |format|
-      format.html { render template: 'error/500', layout: 'layouts/raw', status: 500 }
+      format.html { render template: 'error/500', layout: 'layouts/error', status: 500 }
       format.all { render nothing: true, status: 500}
     end
   end
