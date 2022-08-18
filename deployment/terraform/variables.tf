3d25a68e192bc105ee8842f61253db0768439a43
zhelezovartem <Zhelezovartem@gmail.com>
2022-08-18 17:09:27 +0300

    ci: add parameters for the deployment logging

--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -35,7 +35,7 @@ variable "TAGGED_IMAGE" {
 variable "app_env_vars" {
   type = map(any)
   default = {
-    CYBER_DOJO_PROMETHEUS      = "false"
+    CYBER_DOJO_PROMETHEUS      = "true"
     CYBER_DOJO_SAVER_PORT      = "4537"
     CYBER_DOJO_WEB_PORT        = "3000"
     CYBER_DOJO_RUNNER_PORT     = "4597"
