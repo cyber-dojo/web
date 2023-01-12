12ad8f68d57b321033632d61f31b1f15f7c48c1d
Jon Jagger <jon@jaggersoft.com>
2023-01-12 16:31:23 +0000

    Add special case handling to spike looking into new traffic-light colours

--- sh/echo_env_vars.sh
+++ sh/echo_env_vars.sh
@@ -2,7 +2,7 @@
 echo_env_vars()
 {
   echo
-  echo "echo CYBER_DOJO_WEB_SHA=$(image_sha)"
-  echo "echo CYBER_DOJO_WEB_TAG=$(image_tag)"
+  echo "CYBER_DOJO_WEB_SHA=$(image_sha)"
+  echo "CYBER_DOJO_WEB_TAG=$(image_tag)"
   echo
 }
