884663460a5d0e1cb9067f4d947425834b66fe2f
zhelezovartem <zhelezovartem@gmail.com>
2024-03-13 20:18:33 +0500

    use beta registry for prod

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -1,5 +1,5 @@
 module "ecs-service" {
-  source                    = "s3::https://s3-eu-central-1.amazonaws.com/terraform-modules-9d7e951c290ec5bbe6506e0ddb064808764bc636/terraform-modules.zip//ecs-service/v3"
+  source                    = "s3::https://s3-eu-central-1.amazonaws.com/terraform-modules-9d7e951c290ec5bbe6506e0ddb064808764bc636/terraform-modules.zip//ecs-service/v4"
   service_name              = var.service_name
   TAGGED_IMAGE              = var.TAGGED_IMAGE
   enable_execute_command    = "true"
@@ -9,8 +9,6 @@ module "ecs-service" {
   mem_reservation           = var.mem_reservation
   mem_limit                 = var.mem_limit
   app_env_vars              = local.app_env_vars
-  ecr_replication_targets   = var.ecr_replication_targets
-  ecr_replication_origin    = var.ecr_replication_origin
   ecs_wait_for_steady_state = true
   tags                      = module.tags.result
 }
