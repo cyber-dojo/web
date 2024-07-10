0e49b7670882e11ecd552f07df36d46353e61f09
JonJagger <jon@kosli.com>
2024-07-10 10:50:14 +0100

    Fix docker healthcheck command

--- healthcheck.sh
+++ healthcheck.sh
@@ -15,6 +15,8 @@ readonly PORT="${CYBER_DOJO_WEB_PORT}"
 readonly READY_LOG_FILENAME=/tmp/ready.log
 
 wget http://0.0.0.0:${PORT}/ready -q -O - >> "${READY_LOG_FILENAME}" 2>&1
+echo >> "${READY_LOG_FILENAME}"  # add a newline
 
 # keep only most recent 500 lines
-sed -i '501,$ d' "${READY_LOG_FILENAME}"
+tail -n500 "${READY_LOG_FILENAME}" > "${READY_LOG_FILENAME}.tmp"
+mv "${READY_LOG_FILENAME}.tmp" "${READY_LOG_FILENAME}"
