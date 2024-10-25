57f492cc5a2ad9ba08254a71e72729a06f8c84e7
zhelezovartem <zhelezovartem@gmail.com>
2024-10-25 10:51:35 +0300

    tf fmt

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -1,15 +1,15 @@
 module "ecs-service" {
-  source                    = "s3::https://s3-eu-central-1.amazonaws.com/terraform-modules-9d7e951c290ec5bbe6506e0ddb064808764bc636/terraform-modules.zip//ecs-service/v4"
-  service_name              = var.service_name
-  TAGGED_IMAGE              = var.TAGGED_IMAGE
-  enable_execute_command    = "true"
-  app_port                  = var.app_port
-  desired_count             = var.desired_count
-  cpu_limit                 = var.cpu_limit
-  mem_reservation           = var.mem_reservation
-  mem_limit                 = var.mem_limit
-  app_env_vars              = local.app_env_vars
-  ecs_wait_for_steady_state = true
+  source                     = "s3::https://s3-eu-central-1.amazonaws.com/terraform-modules-9d7e951c290ec5bbe6506e0ddb064808764bc636/terraform-modules.zip//ecs-service/v4"
+  service_name               = var.service_name
+  TAGGED_IMAGE               = var.TAGGED_IMAGE
+  enable_execute_command     = "true"
+  app_port                   = var.app_port
+  desired_count              = var.desired_count
+  cpu_limit                  = var.cpu_limit
+  mem_reservation            = var.mem_reservation
+  mem_limit                  = var.mem_limit
+  app_env_vars               = local.app_env_vars
+  ecs_wait_for_steady_state  = true
   ecs_service_update_timeout = "10m"
-  tags                      = module.tags.result
+  tags                       = module.tags.result
 }
