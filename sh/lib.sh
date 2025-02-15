bd6c299cc86d8e1b1a14c5b5fc8f1bb3b0c8e9aa
JonJagger <jon@kosli.com>
2025-02-15 09:06:58 +0000

    Tidy scripts

--- sh/lib.sh
+++ sh/lib.sh
@@ -8,9 +8,9 @@ exit_non_zero_if_bad_base_image()
   regex=":[a-z0-9]{7}@sha256:[a-z0-9]{64}$"
   if ! [[ ${base_image} =~ $regex ]]; then
     stderr "BASE_IMAGE must have a 7-digit short-sha tag and a full 64-digit digest, Eg"
-    stderr " base_image_name  : cyberdojo/web-base"
-    stderr " base_image_tag   : 559d354"
-    stderr " base_image_digest: ddab9080cd0bbd8e976a18bdd01b37b66e47fe83b0db396e65dc3014bad17fd3"
+    stderr " name  : cyberdojo/web-base"
+    stderr " tag   : 559d354"
+    stderr " digest: ddab9080cd0bbd8e976a18bdd01b37b66e47fe83b0db396e65dc3014bad17fd3"
     exit 42
   fi
 }
