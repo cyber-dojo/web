beea138146360e39f3b980955f10fdefb0a7fab9
JonJagger <jon@kosli.com>
2025-12-23 17:47:45 +0000

    create_v2_kata now create red/amber/green traffic-lights

--- source/script/create_v2_kata.rb
+++ source/script/create_v2_kata.rb
@@ -12,11 +12,56 @@ def create_v2_kata()
   v0_kid = '5U2J18'
   manifest = http.get('kata_manifest', {id: v0_kid})
   manifest['version'] = 2
-  v2_kid = http.post('kata_create', {manifest: manifest})
-  puts(v2_kid)
-  files = http.get('kata_event', {id:v2_kid, index:0 })['files']
+  
+  id = http.post('kata_create', {manifest: manifest})
+  files = http.get('kata_event', {id:id, index:0 })['files']
   # [ bats_help.txt cyber-dojo.sh hiker.sh readme.txt test_hiker.sh ]
   hiker_sh = files['hiker.sh']['content'] # includes '6 * 9'
+
+  stdout = {'content' => 'expected [42] but was [54]', 'truncated' => false}
+  stderr = {'content' => '', 'truncated' => false}
+  status = 1
+  http.post('kata_ran_tests', {
+    id: id,
+    index: 1,
+    files: files,
+    stdout: stdout,
+    stderr: stderr,
+    status: status,
+    summary: {'colour' => 'red', 'predicted' => 'none'}
+  })
+
+  files['hiker.sh']['content'] = hiker_sh.sub('6 * 9', '6 * 9s')
+  stdout = {'content' => 'expected [42] but was []', 'truncated' => false}
+  stderr = {'content' => 'value too great for base (error token is "9s")', 'truncated' => false}
+  status = 1
+  http.post('kata_ran_tests', {
+    id: id,
+    index: 2,
+    files: files,
+    stdout: stdout,
+    stderr: stderr,
+    status: status,
+    summary: {'colour' => 'amber', 'predicted' => 'none'}
+  })
+
+  files['hiker.sh']['content'] = hiker_sh.sub('6 * 9', '6 * 7')
+  stdout = {'content' => 'Overall result: SUCCESS', 'truncated' => false}
+  stderr = {'content' => '', 'truncated' => false}
+  status = 0
+  http.post('kata_ran_tests', {
+    id: id,
+    index: 3,
+    files: files,
+    stdout: stdout,
+    stderr: stderr,
+    status: status,
+    summary: {'colour' => 'green', 'predicted' => 'none'}
+  })
+
+  events = http.get('kata_events', {id:id})
+  puts(events)
+  puts(id)
 
 end
 
