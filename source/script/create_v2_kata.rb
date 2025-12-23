2a138b470c891a451db17884680a919d42b53678
JonJagger <jon@kosli.com>
2025-12-23 17:28:29 +0000

    Add few statements to create_v2_kata script

--- source/script/create_v2_kata.rb
+++ source/script/create_v2_kata.rb
@@ -6,16 +6,18 @@ end
 require_source 'services/externals'
 include Externals
 
-def create_v2_dashboard()
-  puts("create_v2_dashboard")
-  
+def create_v2_kata()
+  puts("create_v2_kata")  
   http = saver.instance_variable_get(:@http)
   v0_kid = '5U2J18'
   manifest = http.get('kata_manifest', {id: v0_kid})
   manifest['version'] = 2
   v2_kid = http.post('kata_create', {manifest: manifest})
   puts(v2_kid)
+  files = http.get('kata_event', {id:v2_kid, index:0 })['files']
+  # [ bats_help.txt cyber-dojo.sh hiker.sh readme.txt test_hiker.sh ]
+  hiker_sh = files['hiker.sh']['content'] # includes '6 * 9'
 
 end
 
-create_v2_dashboard
+create_v2_kata
