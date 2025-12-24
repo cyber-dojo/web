a0bbfeead793a6427e0f2f1a4802935965dba221
JonJagger <jon@kosli.com>
2025-12-24 07:20:29 +0000

    Tidy script

--- source/script/create_v2_kata.rb
+++ source/script/create_v2_kata.rb
@@ -6,61 +6,75 @@ end
 require_source 'services/externals'
 include Externals
 
+$http = saver.instance_variable_get(:@http)
+
 def create_v2_kata()
-  http = saver.instance_variable_get(:@http)
-  v0_kid = '5U2J18'
-  manifest = http.get('kata_manifest', {id: v0_kid})
+  v0_id = '5U2J18'
+  manifest = $http.get('kata_manifest', {id: v0_id})
   manifest['version'] = 2
   manifest.delete('group_id')
   manifest.delete('group_index')
   
-  id = http.post('kata_create', {manifest: manifest})
-  files = http.get('kata_event', {id:id, index:0 })['files']
+  id = $http.post('kata_create', {manifest: manifest})
+  files = $http.get('kata_event', {id:id, index:0 })['files']
   # [ bats_help.txt cyber-dojo.sh hiker.sh readme.txt test_hiker.sh ]
   hiker_sh = files['hiker.sh']['content'] # includes '6 * 9'
+  index = 1
 
+  index = red_traffic_light(id, index, files)
+
+  files['hiker.sh']['content'] = hiker_sh.sub('6 * 9', '6 * 9s')
+  index = amber_traffic_light(id, index, files)
+
+  files['hiker.sh']['content'] = hiker_sh.sub('6 * 9', '6 * 7')
+  index = green_traffic_light(id, index, files)
+
+  print(id)
+end
+
+def red_traffic_light(id, index, files)
   stdout = {'content' => 'expected [42] but was [54]', 'truncated' => false}
   stderr = {'content' => '', 'truncated' => false}
   status = 1
-  http.post('kata_ran_tests', {
+  $http.post('kata_ran_tests', {
     id: id,
-    index: 1,
+    index: index,
     files: files,
     stdout: stdout,
     stderr: stderr,
     status: status,
     summary: {'colour' => 'red', 'predicted' => 'none'}
   })
+end
 
-  files['hiker.sh']['content'] = hiker_sh.sub('6 * 9', '6 * 9s')
+def amber_traffic_light(id, index, files)
   stdout = {'content' => 'expected [42] but was []', 'truncated' => false}
   stderr = {'content' => 'value too great for base (error token is "9s")', 'truncated' => false}
   status = 1
-  http.post('kata_ran_tests', {
+  $http.post('kata_ran_tests', {
     id: id,
-    index: 2,
+    index: index,
     files: files,
     stdout: stdout,
     stderr: stderr,
     status: status,
     summary: {'colour' => 'amber', 'predicted' => 'none'}
   })
+end
 
-  files['hiker.sh']['content'] = hiker_sh.sub('6 * 9', '6 * 7')
+def green_traffic_light(id, index, files)
   stdout = {'content' => 'Overall result: SUCCESS', 'truncated' => false}
   stderr = {'content' => '', 'truncated' => false}
   status = 0
-  http.post('kata_ran_tests', {
+  $http.post('kata_ran_tests', {
     id: id,
-    index: 3,
+    index: index,
     files: files,
     stdout: stdout,
     stderr: stderr,
     status: status,
     summary: {'colour' => 'green', 'predicted' => 'none'}
   })
-
-  print(id)
 end
 
 create_v2_kata
