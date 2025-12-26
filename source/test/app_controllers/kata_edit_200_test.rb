724c7f74a80a5734625da5c6f9bcc859f859b097
JonJagger <jon@kosli.com>
2025-12-26 11:45:51 +0000

    Use RunnerStub in kata_edit_200_test.rb

--- source/test/app_controllers/kata_edit_200_test.rb
+++ source/test/app_controllers/kata_edit_200_test.rb
@@ -7,7 +7,7 @@ class KataEdit200Test  < AppControllerTestBase
   end
 
   test '9B9', %w( edit landing page ) do
-    with_runner_class('RunnerService') do
+    with_runner_class('RunnerStub') do
       in_kata do |kata|
         get "/kata/edit/#{kata.id}"
         assert_response :success
