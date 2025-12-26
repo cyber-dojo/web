fa8bc59205d09a8cd37cdeb7b609369d33cdec04
JonJagger <jon@kosli.com>
2025-12-26 12:42:38 +0000

    Add test for traffic-light predictions

---
+++ source/test/app_controllers/predicted_test.rb
@@ -0,0 +1,35 @@
+require_relative 'app_controller_test_base'
+
+class PredictedTest  < AppControllerTestBase
+
+  def self.hex_prefix
+    '1D3'
+  end
+
+  test '5b7', %w( predicted right ) do
+    with_runner_class('RunnerStub') do
+      in_kata do |kata|
+        runner.stub_run({outcome: 'red'})
+        post_run_tests({'predicted'=>'red'})
+        last = kata.event(-1)
+        assert_equal 'red', last['predicted']
+        assert_equal 'red', last['colour']
+      end
+    end
+  end
+
+  # - - - - - - - - - - - - - - - - - - - -
+
+  test '5b8', %w( predicted wrong ) do
+    with_runner_class('RunnerStub') do
+      in_kata do |kata|
+        runner.stub_run({outcome: 'amber'})
+        post_run_tests({'predicted'=>'red'})
+        last = kata.event(-1)
+        assert_equal 'red', last['predicted']
+        assert_equal 'amber', last['colour']
+      end
+    end
+  end
+
+end
